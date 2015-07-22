157e324953926791fe60c68bd3590f7fd2f96b89
Chris Malone <cmalone@lanl.gov>
2015-07-22 09:48:52 -0600

    simplify initial mesh and move creation to main.cc

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -15,19 +15,6 @@
 #include "MeshFactory.hh"
 
 
-Driver::Driver()
-{
-  // Create some default meshes to test with
-  // 2d quad mesh from (0,0) to (1,1) with 61x61 zones
-  inputMesh_ = Jali::MeshFactory::create(0.0, 0.0,
-					 1.0, 1.0,
-					 61, 61);
-  // 2d quad mesh from (0.11, 0.17) to (0.71, 0.67) with 23x41 zones
-  targetMesh_ = Jali::MeshFactory::create(0.11, 0.17,
-					  0.71, 0.67,
-					  23, 41);
-} // Driver::Driver
-
 void Driver::run()
 {
     std::printf("in Driver::run()...\n");
