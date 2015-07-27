87b543ca923dfb4e26d0bac1118b3aca50724d1c
Rao Garimella <rao@ml-fey1.lanl.gov>
2015-07-27 12:52:09 -0600

    initial implementation of a class that can be used to represent the state data on a mesh

--- src/state/state.h
+++ src/state/state.h
@@ -17,8 +17,7 @@
 #include "state_vector.h"
 
 
-namespace NGC {
-namespace Remap {
+namespace Portage {
     
 class State {
  public:
@@ -39,9 +38,11 @@ class State {
   
   ~State() {}
   
-  // Add state vector
+  // Add state vector - returns 1 if successfully added, 0 otherwise
   
-  void add(StateVector const & vector);
+  int add(std::string const name, Jali::Entity_kind const on_what, 
+          double const * const data);
+  int add(StateVector const & vector);  // discouraged - copies data
   
   
   // Iterators for going through all the state vectors
@@ -79,9 +80,88 @@ class State {
                                            // for each kind of entity
 
 };
+
+
+
+///// ONCE CINCH IS ABLE TO BUILD TESTS WITH LIBRARY FILES AND MULTIPLE SOURCE FILES, WE CAN PUT THESE ROUTINES INTO state.cc
+
+// add a vector to the state object
+
+inline
+int State::add(std::string const name, Jali::Entity_kind const on_what,
+               double const * const data) {
+
+  if (find(name,on_what) == cend()) {
+    // a search of the state vectors by name and kind of entity turned up
+    // empty, so add the vector to the list; if not, warn about duplicate
+    // state data
+    
+    // does this syntax cause copying of data from temporary StateVector
+    // to the object that is created in state_vectors_ ? If it does, we
+    // can use the C++11 emplace to construct in place
+
+    state_vectors_.push_back(StateVector(name,on_what,mymesh_,data));
+    return 1;
+  }
+  else {      
+    // found a state vector by same name living on the same kind of entity
+    
+    std::cerr << "Attempted to add duplicate state vector. Ignoring\n" << std::endl;
+    return 0;
+  }
+  
+  return 0;
+}
+
+
+// add a vector to the state object from an already defined vector -
+// discouraged as it involves a copying of data
+
+inline
+int State::add(StateVector const & vector) {
+
+  if (mymesh_ != vector.mesh()) {
+    std::cerr << "State and StateVector not defined on the same mesh" << std::endl;
+    return 0;
+  }
+  
+  if (find(vector.name(),vector.on_what()) == cend()) {
+    // a search of the state vectors by name and kind of entity turned up
+    // empty, so add the vector to the list; if not, warn about duplicate
+    // state data
+    
+    state_vectors_.push_back(vector);
+    return 1;
+  }
+  else {      
+    // found a state vector by same name living on the same kind of entity
+    
+    std::cerr << "Attempted to add duplicate state vector. Ignoring\n" << std::endl;
+    return 0;
+  }
+  
+  return 0;
+}
+
+
+// find a state vector by name and what type of entity it is on
+
+inline
+State::const_iterator State::find(std::string const name, Jali::Entity_kind const on_what) const {
+  
+  std::vector<StateVector>::const_iterator it = state_vectors_.cbegin();
+  while (it != state_vectors_.cend()) {
+    StateVector const & vector = *it;
+    if (vector.name() == name && vector.on_what() == on_what)
+      break;
+    else
+      ++it;
+  }
+  return it;
+  
+}
   
 
-} // end namespace Remap
-} // end namespace NGC
+} // namespace Portage
   
 #endif
