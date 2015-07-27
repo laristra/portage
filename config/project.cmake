660f74b6851200307f78ecf3bacd150a257fe030
Rao Garimella <rao@ml-fey1.lanl.gov>
2015-07-27 14:10:20 -0600

    made some experimental changes to be able to define libraries from individual subdirs but commented them out as cinch is not able to interpret them correctly

--- config/project.cmake
+++ config/project.cmake
@@ -8,6 +8,22 @@ project(portage)
 cinch_add_application_directory(app)
 cinch_add_library_target(portage src)
 
+# Create some subcomponent libraries for the purposes of unit testing
+#
+# NOT ABLE TO DO EITHER OF THESE
+
+if (ENABLE_UNIT_TESTS)
+#  cinch_add_library_target(search src/search)
+#  cinch_add_library_target(intersect src/intersect)
+#  cinch_add_library_target(remap src/remap)
+#  cinch_add_library_target(state src/state)
+
+#  cinch_add_library_target(portage_search src/search)
+#  cinch_add_library_target(portage_intersect src/intersect)
+#  cinch_add_library_target(portage_remap src/remap)
+#  cinch_add_library_target(portage_state src/state)
+endif()
+
 #~---------------------------------------------------------------------------~-#
 # Formatting options for emacs and vim.
 # vim: set tabstop=4 shiftwidth=4 expandtab :
