9f48243c21a2292a4a84ee1bba8e53507c90b839
Rao Garimella <rao@ml-fey1.lanl.gov>
2015-07-27 12:56:33 -0600

    tests for the state vector class

--- src/state/test/test_state_vector.cc
+++ src/state/test/test_state_vector.cc
@@ -3,44 +3,52 @@
  * All rights reserved.
  *---------------------------------------------------------------------------~*/
 
+#include "../state_vector.h"
+
 #include "gtest/gtest.h"
 #include "mpi.h"
 
 #include "Mesh.hh"
 #include "MeshFactory.hh"
 
-#include "../state_vector.h"
+TEST(StateVector, DefineVectorOnCells) {
 
-TEST(STATE_VECTOR, DEFINE_VECTOR_ON_CELLS) {
-
-  int mpi_initialized;
-  MPI_Initialized(&mpi_initialized);
-
-      
-  int mpi_initialized_here = false;
-  if (!mpi_initialized) {
-    mpi_initialized_here = true;
-    MPI_Init(NULL, NULL);
-  }
+  MPI_Init(NULL, NULL);
 
   Jali::MeshFactory mf(MPI_COMM_WORLD);
   Jali::Mesh *mesh = mf(0.0,0.0,1.0,1.0,2,2);
 
   ASSERT_TRUE(mesh != NULL);
 
-  // purposely define data vector that is smaller than the number of cells 
-  // so that we can test initialization
-
-  std::vector<double> data1 = {1.0,3.0,2.5}; 
-  NGC::Remap::StateVector myvec1("var1",Jali::CELL,mesh,&(data1[0]));
+  std::vector<double> data1 = {1.0,3.0,2.5,4.5}; 
+  Portage::StateVector myvec1("var1",Jali::CELL,mesh,&(data1[0]));
 
   int ncells = mesh->num_entities(Jali::CELL,Jali::ALL);
   ASSERT_EQ(ncells,myvec1.size());
   ASSERT_EQ(data1[0],myvec1[0]);
   ASSERT_EQ(data1[1],myvec1[1]);
   ASSERT_EQ(data1[2],myvec1[2]);
-  ASSERT_EQ(0.0,myvec1[3]);
-  
-  if (mpi_initialized_here)
-    MPI_Finalize();
+  ASSERT_EQ(data1[3],myvec1[3]);
+
 }
+
+
+TEST(StateVector, DefineVectorOnNodes) {
+
+  Jali::MeshFactory mf(MPI_COMM_WORLD);
+  Jali::Mesh *mesh = mf(0.0,0.0,1.0,1.0,2,2);
+
+  ASSERT_TRUE(mesh != NULL);
+
+  std::vector<double> data1 = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0}; 
+  Portage::StateVector myvec1("var1",Jali::NODE,mesh,&(data1[0]));
+
+  int nnodes = mesh->num_entities(Jali::NODE,Jali::ALL);
+  ASSERT_EQ(nnodes,myvec1.size());
+  for (int i = 0; i < nnodes; ++i)
+    ASSERT_EQ(data1[i],myvec1[i]);
+  
+  MPI_Finalize();
+}
+
+
