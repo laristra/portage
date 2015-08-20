2db923db07c8b01f8acd654dc8ce20207bec2b24
Chris Malone <cmalone@lanl.gov>
2015-08-20 08:45:28 -0600

    start of driver update

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -6,6 +6,10 @@
 #include "driver.h"
 
 #include <cstdio>
+#include <vector>
+//#include <utility>
+#include <numeric>
+#include <algorithm>
 
 #include "portage/search/search.h"
 #include "portage/intersect/intersect.h"
@@ -21,51 +25,65 @@ void Driver::run()
 {
     std::printf("in Driver::run()...\n");
 
+	const Search s(&sourceMesh_, &targetMesh_);
+	const Intersect i(sourceMesh_, targetMesh_);
+	const Remap r(sourceMesh_, sourceState_, targetMesh_, targetState_);
+
     int numTargetCells = targetMesh_.num_entities(Jali::CELL, Jali::ALL);
 
-    double* newField = new double[numTargetCells];
-    for (unsigned int i=0; i<numTargetCells; i++)
-    {
-        // Search for possible intersections.
-        Search s;
-        Jali::Entity_ID_List candidates;
-        s.search(&sourceMesh_, &targetMesh_, i, &candidates);
+	std::vector<int> cellIndices(numTargetCells);
+	// populates cellIndices with a sequence of ints starting at 0...
+	std::iota(cellIndices.begin(), cellIndices.end(), 0);
+
+	//    double* newField = new double[numTargetCells];
+	std::vector<double> newField(numTargetCells);
+
+	// this will populate newField with the doubles returned from the final remap
+	std::transform(cellIndices.begin(), cellIndices.end(),
+				   newField.begin(),
+				   composerFunctor<Search, Intersect, Remap>(&s, &i, &r));
+//     for (unsigned int i=0; i<numTargetCells; i++)
+//     {
+//         // Search for possible intersections.
+//         Search s;
+//         Jali::Entity_ID_List candidates;
+//         s.search(&sourceMesh_, &targetMesh_, i, &candidates);
  
-#ifdef DEBUG_OUTPUT
-        std::cout << std::endl << "Candidates for cell " << i << std::endl;
-        for (unsigned int j=0; j<candidates.size(); j++)
-            std::cout << candidates[j] << " ";
-        std::cout << std::endl;
-#endif
+// #ifdef DEBUG_OUTPUT
+//         std::cout << std::endl << "Candidates for cell " << i << std::endl;
+//         for (unsigned int j=0; j<candidates.size(); j++)
+//             std::cout << candidates[j] << " ";
+//         std::cout << std::endl;
+// #endif
 
-        // Calculate the overlap of actual intersections.
-        std::vector<float> moments;
-        Intersect isect(sourceMesh_, targetMesh_);
-        isect.intersect(i, &candidates, &moments);
+//         // Calculate the overlap of actual intersections.
+//         std::vector<float> moments;
+//         Intersect isect(sourceMesh_, targetMesh_);
+//         isect.intersect(i, &candidates, &moments);
 
-#ifdef DEBUG_OUTPUT
-        std::cout << std::endl << "Moments for cell " << i << std::endl;
-        for (unsigned int j=0; j<moments.size(); j++)
-            std::cout << moments[j] << " ";
-        std::cout << std::endl;
-#endif
+// #ifdef DEBUG_OUTPUT
+//         std::cout << std::endl << "Moments for cell " << i << std::endl;
+//         for (unsigned int j=0; j<moments.size(); j++)
+//             std::cout << moments[j] << " ";
+//         std::cout << std::endl;
+// #endif
 
-        // Remap from sourceMesh_ to targetMesh_
-        Remap r(sourceMesh_, sourceState_, targetMesh_, targetState_);
-        newField[i] = r.remap(remap_var_names_[0], i, candidates, moments);
-    }
+//         // Remap from sourceMesh_ to targetMesh_
+//         Remap r(sourceMesh_, sourceState_, targetMesh_, targetState_);
+//         newField[i] = r.remap(remap_var_names_[0], i, candidates, moments);
+//     }
 
-    targetState_.add("remapped_data", Jali::CELL, newField);
+//     targetState_.add("remapped_data", Jali::CELL, newField);
 
-#ifdef DEBUG_OUTPUT
-    std::vector<StateVector>::const_iterator field = targetState_.find("remapped_data", Jali::CELL);
-    Portage::StateVector stateVector =  *field;
-    for (unsigned int i=0; i<numTargetCells; i++)
-    {
-        double x = *(stateVector.begin() + i);
-        std::cout << "Remapped field for cell " << i << ": " << x << std::endl; 
-    }
-#endif
+// #ifdef DEBUG_OUTPUT
+//     std::vector<StateVector>::const_iterator field = targetState_.find("remapped_data", Jali::CELL);
+//     Portage::StateVector stateVector =  *field;
+//     for (unsigned int i=0; i<numTargetCells; i++)
+//     {
+//         double x = *(stateVector.begin() + i);
+//         std::cout << "Remapped field for cell " << i << ": " << x << std::endl; 
+//     }
+// #endif
 
 } // Driver::run
 
--- src/driver/driver.h
+++ src/driver/driver.h
@@ -6,6 +6,9 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
+#include<algorithm>
+#include<vector>
+
 #include "Mesh.hh"
 #include "portage/state/state.h"
 
@@ -18,51 +21,94 @@ namespace Portage {
 
 class Driver
 {
- public:
+public:
   
-  //! Constructor uses established meshes for remap
-  Driver(Jali::Mesh const & sourceMesh,State const & sourceState,
-         Jali::Mesh const & targetMesh, State & targetState) 
-      : sourceMesh_(sourceMesh), sourceState_(sourceState),
-        targetMesh_(targetMesh), targetState_(targetState) {}
+	//! Constructor uses established meshes for remap
+	Driver(Jali::Mesh const & sourceMesh,State const & sourceState,
+		   Jali::Mesh const & targetMesh, State & targetState) 
+		: sourceMesh_(sourceMesh), sourceState_(sourceState),
+		  targetMesh_(targetMesh), targetState_(targetState) {}
   
-  //! Copy constructor (disabled)
-  Driver(const Driver &) = delete;
+	//! Copy constructor (disabled)
+	Driver(const Driver &) = delete;
   
-  //! Assignment operator (disabled)
-  Driver & operator = (const Driver &) = delete;
+	//! Assignment operator (disabled)
+	Driver & operator = (const Driver &) = delete;
   
-  //! Destructor
-  ~Driver() {}
+	//! Destructor
+	~Driver() {}
   
   
-  // Specify the names of the variables to be remapped
+	// Specify the names of the variables to be remapped
 
-  void set_remap_var_names(std::vector<std::string> &remap_var_names_in) {
-    remap_var_names_ = remap_var_names_in;
-  }
+	void set_remap_var_names(std::vector<std::string> &remap_var_names_in) {
+		remap_var_names_ = remap_var_names_in;
+	}
 
-  // Get the names of the variables to be remapped
+	// Get the names of the variables to be remapped
 
-  std::vector<std::string> remap_var_names() {
-    return remap_var_names_;
-  }
+	std::vector<std::string> remap_var_names() {
+		return remap_var_names_;
+	}
 
-  /*!
-    \brief This method calls the search, intersect, and remap routines
-    needed to map one mesh to another.
-  */
-  void run();
+	/*!
+	  \brief This method calls the search, intersect, and remap routines
+	  needed to map one mesh to another.
+	*/
+	void run();
+
+	template <typename SearchType, typename IsectType, typename RemapType>
+	struct composerFunctor
+	{
+		const SearchType* s_;
+		const IsectType* i_;
+		const RemapType* r_;
+		composerFunctor(const SearchType* s, const IsectType* i, const RemapType* r)
+			: s_(s), i_(i), r_(r) { }
+
+		
+		{
+			
+		}
+
+		double operator()(Jali::Entity_ID const targetCellIndex)
+		{
+			// Search for candidates and return their cell indices
+			Jali::Entity_ID_List* candidates;
+			s_->search(targetCellIndex, candidates);
+
+			// Intersect wants a vector of pairs of (x,y) coordinates for
+			// each candidate.
+			std::vector<std::vector<std::pair<double,double> > > 
+				candidatesNodesCoords(candidates->size());
+		    std::transform(candidates->begin(), candidates->end(),
+			    		   candidatesNodesCoords.begin(),
+						   [&](const Jali::Entity_ID cellID) -> std::vector<std::pair<double,double> >
+						   {
+							   std::vector<JaliGeometry::Point> cellNodes;
+							   targetMesh_->cell_get_coordinates(cellID, &cellNodes);
+							   std::vector<std::pair<double,double> > cellNodeCoords(cellNodes.size());
+							   std::transform(cellNodes.begin(), cellNodes.end(),
+											  cellNodeCoords.begin(),
+											  [](const JaliGeometry::Point node)
+											  {
+												  return std::make_pair(node.x(), node.y());
+											  });
+							   return cellNodeCoords;});
+	}
+
+
+
+private:
   
- private:
-  
-  Jali::Mesh const & sourceMesh_;
-  Jali::Mesh const & targetMesh_;
-  State const & sourceState_;
-  State & targetState_;
-  std::vector<std::string> remap_var_names_;
+	Jali::Mesh const & sourceMesh_;
+	Jali::Mesh const & targetMesh_;
+	State const & sourceState_;
+	State & targetState_;
+	std::vector<std::string> remap_var_names_;
 
 }; // class Driver
+
 
 } // namespace Portage
 
