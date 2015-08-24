28c04964ec48429fc5a24b85440de86bb9400efb
Chris Malone <cmalone@lanl.gov>
2015-08-24 10:58:24 -0600

    more cosmetics

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -24,38 +24,39 @@ void Driver::run()
     std::printf("in Driver::run()...\n");
 
 	const SearchSimple search(&sourceMesh_, &targetMesh_);
-	// Use Angela's new Clipper-based intersector
 	const IntersectClipper intersect;
 	const Remap remap(sourceMesh_, sourceState_, targetMesh_, targetState_);
 
     int numTargetCells = targetMesh_.num_entities(Jali::CELL, Jali::ALL);
 
-	std::cout << "Number of target cells in target mesh " << numTargetCells << std::endl;
+	std::cout << "Number of target cells in target mesh "
+			  << numTargetCells << std::endl;
 
 	std::vector<int> cellIndices(numTargetCells);
 	// populates cellIndices with a sequence of ints starting at 0...
 	std::iota(cellIndices.begin(), cellIndices.end(), 0);
 
 	std::vector<double> newField(numTargetCells);
-	// this will populate newField with the doubles returned from the final remap
+	composerFunctor<SearchSimple, IntersectClipper, Remap> 
+		composer(&search, &intersect, &remap,
+				 &sourceMesh_, &targetMesh_,
+				 remap_var_names_[0]);
+	// this populates newField with the doubles returned from the final remap
 	std::transform(cellIndices.begin(), cellIndices.end(),
 				   newField.begin(),
-				   composerFunctor<SearchSimple, IntersectClipper, Remap>(&search, 
-																		  &intersect, 
-																		  &remap, 
-																	&sourceMesh_, &targetMesh_,
-																	remap_var_names_[0]));
+				   composer);
 	// Add it to the new state
-	// CMM: is there a less hacky way of getting vector to array?
 	targetState_.add("remapped_data", Jali::CELL, &newField[0]);
 
 #ifdef DEBUG_OUTPUT
-    std::vector<StateVector>::const_iterator field = targetState_.find("remapped_data", Jali::CELL);
+    std::vector<StateVector>::const_iterator 
+		field = targetState_.find("remapped_data", Jali::CELL);
     Portage::StateVector stateVector =  *field;
     for (unsigned int i=0; i<numTargetCells; i++)
     {
         double x = *(stateVector.begin() + i);
-        std::cout << "Remapped field for cell " << i << ": " << x << std::endl; 
+        std::cout << "Remapped field for cell " << i 
+				  << ": " << x << std::endl; 
     }
 #endif
 
