73632330fce5889d24162b309dd12c07ae4690fb
Ondřej Čertík <ondrej.certik@gmail.com>
2015-11-12 14:02:18 -0700

    Install Jali on Jenkins each time
    Now a specific version of Jali is checked out, installed and used to build
    Portage.

--- jenkins/build_pr.sh
+++ jenkins/build_pr.sh
@@ -10,6 +10,13 @@ set -e
 # Echo each command
 set -x
 
+# Tag or git commit hash of Jali version to build and use for this PR:
+JALI_VERSION=v0.6.0
+
+# Where to find Jali's TPLs:
+TPL_INSTALL_PREFIX=/usr/local/codes/ngc/private/jali-0.6.0-tpl-intel
+
+
 git config user.email ""
 git config user.name "Jenkins"
 git merge origin/master
@@ -23,7 +30,36 @@ module load openmpi/1.6.5
 
 echo $WORKSPACE
 cd $WORKSPACE
-rm -rf build
+git clean -dfx
+
+# Build Jali
+
+git clone ssh://git@xcp-stash.lanl.gov:7999/ngc/jali.git jali-repo
+cd jali-repo
+git checkout $JALI_VERSION
+JALI_INSTALL_PREFIX=`pwd`/jali-inst
+mkdir build
+cd build
+
+cmake \
+  -C $TPL_INSTALL_PREFIX/share/cmake/Jali-tpl-config.cmake \
+  -D CMAKE_BUILD_TYPE=Debug \
+  -D CMAKE_CXX_FLAGS='-std=c++11' \
+  -D CMAKE_INSTALL_PREFIX:FILEPATH=$JALI_INSTALL_PREFIX \
+  -D HDF5_NO_SYSTEM_PATHS:BOOL=TRUE \
+  -D BOOST_ROOT:FILEPATH=$TPL_INSTALL_PREFIX \
+  -D ENABLE_MSTK_Mesh:BOOL=TRUE \
+  -D ENABLE_STK_Mesh:BOOL=FALSE \
+  -D ENABLE_MOAB_Mesh:BOOL=FALSE \
+  ..
+make -j16
+ctest -j16 --output-on-failure
+make install
+
+
+# Build Portage
+
+cd $WORKSPACE
 mkdir build
 cd build
 
@@ -35,7 +71,7 @@ cmake \
   -D ENABLE_MPI=True \
   -D ENABLE_MPI_CXX_BINDINGS=True \
   -D ENABLE_JENKINS_OUTPUT=True \
-  -D Jali_DIR:FILEPATH=/usr/local/codes/ngc/private/jali-0.6.0-intel/lib \
+  -D Jali_DIR:FILEPATH=$JALI_INSTALL_PREFIX/lib \
   ..
-make
+make -j16
 ctest --output-on-failure
