978559ba6b9626ef40eb298999d1cca747220af6
Rao Garimella <rao@lanl.gov>
2015-12-01 16:05:31 -0700

    updated Jali TPLs and Jali versions

--- jenkins/build_pr.sh
+++ jenkins/build_pr.sh
@@ -11,10 +11,10 @@ set -e
 set -x
 
 # Tag or git commit hash of Jali version to build and use for this PR:
-JALI_VERSION=v0.6.1
+JALI_VERSION=v0.6.3
 
 # Where to find Jali's TPLs:
-TPL_INSTALL_PREFIX=/usr/local/codes/ngc/private/jali-0.6.0-tpl-intel
+TPL_INSTALL_PREFIX=/usr/local/codes/ngc/private/jali-1.0.2-tpl-intel
 
 
 git config user.email ""
