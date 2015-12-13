e43882c4589d94fc040a6c3d5bc4a250c9f99624
Christopher Sewell <csewell@lanl.gov>
2015-12-12 20:56:19 -0700

    Getting first-order cell-centered and node-centered remaps to work with OpenMP Thrust

--- config/packages.cmake
+++ config/packages.cmake
@@ -93,11 +93,21 @@ if(ENABLE_THRUST)
   # Use THRUST_DIR directly if specified, otherwise try to build from NGC
   set(THRUST_DIR "${NGC_INCLUDE_DIR}" CACHE PATH "Thrust directory")
   message(STATUS "Using THRUST_DIR=${THRUST_DIR}")
+  
   # Allow for swapping backends - should this be in CACHE?
   set(THRUST_BACKEND "THRUST_DEVICE_SYSTEM_OMP" CACHE STRING "Thrust backend")
   message(STATUS "Using ${THRUST_BACKEND} as Thrust backend.")
   include_directories(${THRUST_DIR})
-#  add_definitions(-DTHRUST)
-  add_definitions(-DTHRUST 
-                  -DTHRUST_DEVICE_SYSTEM=${THRUST_BACKEND})
+  add_definitions(-DTHRUST)
+  add_definitions(-DTHRUST_DEVICE_SYSTEM=${THRUST_BACKEND})
+
+  if("${THRUST_BACKEND}" STREQUAL "THRUST_DEVICE_SYSTEM_OMP")
+    FIND_PACKAGE( OpenMP REQUIRED)
+    if(OPENMP_FOUND)
+      set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+      set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+    endif()
+  endif ()
+
 endif(ENABLE_THRUST)
--- src/driver/driver.h
+++ src/driver/driver.h
@@ -116,7 +116,7 @@ class Driver
     void run()
     {
         std::printf("in Driver::run()...\n");
-        
+       
         // Get an instance of the desired search algorithm type
         const SearchSimple<Mesh_Wrapper,Mesh_Wrapper>
                 search(source_mesh_, target_mesh_);
@@ -140,23 +140,22 @@ class Driver
         // it is added. This logic needs to be reversed. The find function
         // should add it if it is not found (if so requested).
 
-        std::vector<double> dummyvals(numTargetCells,0);
-        double *target_field = NULL;
-        target_state_.get_data(remap_entity_,remap_var_names_[0],&target_field);
+        double *target_field_raw = NULL;
+        target_state_.get_data(remap_entity_,remap_var_names_[0],&target_field_raw);
+        Portage::pointer<double> target_field(target_field_raw);
 
         // Create a cellIndices vector and populates with a sequence of
         // ints starting at 0.  
-
         composerFunctor<SearchSimple<Mesh_Wrapper,Mesh_Wrapper>,
             IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>,
             Remap_1stOrder<Mesh_Wrapper,Jali_State_Wrapper,Jali::Entity_kind> >
                 composer(&search, &intersect, &remap, remap_var_names_[0]);
 
         // This populates targetField with the doubles returned from the final remap
-	Portage::transform(target_mesh_.begin(Jali::CELL),
-		    target_mesh_.end(Jali::CELL),target_field,
+        Portage::transform(target_mesh_.begin(Jali::CELL),
+		    target_mesh_.end(Jali::CELL),
+                    target_field, 
 		    composer);
-
     }
 
 
@@ -183,15 +182,15 @@ struct composerFunctor
     const RemapType* remap_;
     const std::string remap_var_name_;
     //----------------------------------------
-
+   
     composerFunctor(const SearchType* searcher, const IsectType* intersecter, 
                     const RemapType* remapper, const std::string remap_var_name)
 			: search_(searcher), intersect_(intersecter), remap_(remapper), 
               remap_var_name_(remap_var_name) { }
 
-
     double operator()(int const targetCellIndex)
     {
+
         // Search for candidates and return their cells indices
         std::vector<int> candidates;
         search_->search(targetCellIndex, &candidates);
@@ -200,14 +199,8 @@ struct composerFunctor
         // moments of intersection
         std::vector<std::vector<std::vector<double> > > 
                 moments(candidates.size());
-        std::cout << "Candidates size: " << candidates.size() << std::endl;
         for (int i=0;i<candidates.size();i++)
-        {
             moments[i] = (*intersect_)(candidates[i], targetCellIndex);
-            std::cout << targetCellIndex << " " << i << " " << candidates[i] << " ";
-            if (moments[i].size() > 0) std::cout << moments[i][0][0] << std::endl;
-            else std::cout << "None" << std::endl;
-        }
 
         // Compute new value on target cell based on source mesh
         // values and intersection moments
@@ -245,6 +238,7 @@ struct composerFunctor
         double remappedValue = (*remap_)(source_cells_and_weights);
         
         return remappedValue;
+
     }
 };  // struct composerFunctor
 
