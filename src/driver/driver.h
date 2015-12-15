a1c157c3a1ae465c03d386e955379b351a4d0190
Rao Garimella <rao@lanl.gov>
2015-12-15 12:41:30 -0700

    added method to set order of remap - also modified code to call 1st or 2nd order remap based on the requested order

--- src/driver/driver.h
+++ src/driver/driver.h
@@ -16,6 +16,7 @@
 #include "portage/search/search_kdtree.h"
 #include "portage/intersect/intersectClipper.h"
 #include "portage/remap/remap_1st_order.h"
+#include "portage/remap/remap_2nd_order.h"
 
 namespace Portage {
 
@@ -24,30 +25,47 @@ namespace Portage {
     \brief Wrapper for dual mesh
  */
 
-class MeshWrapperDual {
-public:
-    MeshWrapperDual(const Jali_Mesh_Wrapper &w) : w_(w) {}
-    int num_owned_cells() const { return w_.num_owned_nodes(); }
-    int num_ghost_cells() const { return w_.num_ghost_nodes(); }
-    void cell_get_coordinates(int const cellid,
-            std::vector<std::pair<double,double> > *xylist) const {
-        w_.dual_cell_get_coordinates(cellid, xylist);
-    }
-
+class MeshWrapperDual { // cellid is the dual cell (i.e. node) id
+ public:
+  MeshWrapperDual(const Jali_Mesh_Wrapper &w) : w_(w) {}
+  int space_dimension() const { return w_.space_dimension(); }
+  int num_owned_cells() const { return w_.num_owned_nodes(); }
+  int num_ghost_cells() const { return w_.num_ghost_nodes(); }
+  void cell_get_coordinates(int const dualcellid,
+                            std::vector<std::pair<double,double> > *xylist) const {
+    w_.dual_cell_get_coordinates(dualcellid, xylist);
+  }
+  
   counting_iterator begin(Entity_kind const entity) const {
-        if (entity == NODE) return w_.begin(CELL);
-        return w_.begin(NODE);
-    }
-
+    if (entity == NODE) return w_.begin(CELL);
+    return w_.begin(NODE);
+  }
+  
   counting_iterator end(Entity_kind const entity) const {
-        if (entity == NODE) return w_.end(CELL);
-        return w_.end(NODE);
-    }
-    std::vector<std::pair<double, double> > cellToXY(int cellID) const{
-        std::vector<std::pair<double, double> > cellPoints;
-        cell_get_coordinates(cellID, &cellPoints);
-        return cellPoints;
-    }
+    if (entity == NODE) return w_.end(CELL);
+    return w_.end(NODE);
+  }
+  std::vector<std::pair<double, double> > cellToXY(int dualcellID) const{
+    std::vector<std::pair<double, double> > cellPoints;
+    cell_get_coordinates(dualcellID, &cellPoints);
+    return cellPoints;
+  }
+  void cell_get_node_adj_cells(int const dualcellID,
+                               Parallel_type const ptype,
+                               std::vector<int> *adjcells) const {
+    w_.node_get_cell_adj_nodes(dualcellID,ptype,adjcells);
+  }
+  void dual_cell_get_node_adj_cells(int const dualnodeID,
+                                    Parallel_type const ptype,
+                                    std::vector<int> *adjnodes) const {
+    w_.cell_get_node_adj_cells(dualnodeID,ptype,adjnodes);
+  }
+  void cell_centroid(int const dualcellID, std::vector<double> *centroid) const {
+    w_.dual_cell_centroid(dualcellID, centroid); 
+  }
+  void dual_cell_centroid(int const dualnodeID, std::vector<double> *centroid) const {
+    w_.cell_centroid(dualnodeID, centroid);
+  }
 
 private:
     const Jali_Mesh_Wrapper &w_;
@@ -67,97 +85,156 @@ struct composerFunctor;
 template <class Mesh_Wrapper>
 class Driver
 {
-  public:
+ public:
   
-    //! Constructor - takes in wrapper classes for source/target mesh and state
+  //! Constructor - takes in wrapper classes for source/target mesh and state
+  
+  Driver(Entity_kind remapEntity, 
+         Mesh_Wrapper const & sourceMesh, 
+         Jali_State_Wrapper const & sourceState,           
+         Mesh_Wrapper const & targetMesh,
+         Jali_State_Wrapper & targetState) 
+      : remap_entity_(remapEntity), 
+        source_mesh_(sourceMesh), source_state_(sourceState),
+        target_mesh_(targetMesh), target_state_(targetState),
+        remap_order_(1)
+  {}
+  
+  //! Copy constructor (disabled)
+  Driver(const Driver &) = delete;
+  
+  //! Assignment operator (disabled)
+  Driver & operator = (const Driver &) = delete;
+  
+  //! Destructor
+  ~Driver() {}
+  
+  
+  // Specify the names of the variables to be remapped
+  
+  void set_remap_var_names(std::vector<std::string> &remap_var_names_in) {
+    remap_var_names_ = remap_var_names_in;
+  }
+  
+  // Get the names of the variables to be remapped
+  
+  std::vector<std::string> remap_var_names() {
+    return remap_var_names_;
+  }
+  
+  // Set the order of accuracy of remap
+  
+  void set_remap_order(unsigned int const order) {
+    remap_order_ = order;
+  }
+  
+  // Get the order of accuracy of remap
+  
+  unsigned int remap_order() {
+    return remap_order_;
+  }
+  
+  /*!
+    \brief This method calls the search, intersect, and remap routines
+    needed to map one mesh to another.
+  */
+  void run()
+  {
+    std::printf("in Driver::run()...\n");
+    
+    // Get an instance of the desired search algorithm type
+    const SearchKDTree<Mesh_Wrapper,Mesh_Wrapper>
+        search(source_mesh_, target_mesh_);
+    
+    // Get an instance of the desired intersect algorithm type
+    const IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>
+        intersect{source_mesh_, target_mesh_};
+    
+    int numTargetCells = target_mesh_.num_owned_cells();
+    std::cout << "Number of target cells in target mesh "
+              << numTargetCells << std::endl;
+    
+    // Ask for a StateVector with the name remap_var_names_[0] to be
+    // added to the targetState_. If it is already present, the
+    // existing StateVector reference is returned. If its not present,
+    // it is added. This logic needs to be reversed. The find function
+    // should add it if it is not found (if so requested).
+    
+    double *target_field_raw = NULL;
+    target_state_.get_data(remap_entity_,remap_var_names_[0],&target_field_raw);
+    Portage::pointer<double> target_field(target_field_raw);
 
-    Driver(Entity_kind remapEntity, 
-           Mesh_Wrapper const & sourceMesh, 
-           Jali_State_Wrapper const & sourceState,           
-           Mesh_Wrapper const & targetMesh,
-           Jali_State_Wrapper & targetState) 
-            : remap_entity_(remapEntity), 
-              source_mesh_(sourceMesh), source_state_(sourceState),
-              target_mesh_(targetMesh), target_state_(targetState) 
-    {}
-  
-    //! Copy constructor (disabled)
-    Driver(const Driver &) = delete;
-  
-    //! Assignment operator (disabled)
-    Driver & operator = (const Driver &) = delete;
-  
-    //! Destructor
-    ~Driver() {}
-  
-  
-    // Specify the names of the variables to be remapped
+    // Check order of accuracy of remap and do the appropriate thing
 
-    void set_remap_var_names(std::vector<std::string> &remap_var_names_in) {
-        remap_var_names_ = remap_var_names_in;
+    if (remap_order() == 1) {
+
+      std::cout << "Remapping variable " << remap_var_names_[0] << 
+          " using a 1st order accurate algorithm" << std::endl;
+
+      // Eventually put this in a loop over remap variable names as well
+      // Assume for now that we are only doing cell-based remap
+      const Remap_1stOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind> 
+          remap(source_mesh_, source_state_, remap_entity_, remap_var_names_[0]);
+      
+          
+      // Create a cellIndices vector and populates with a sequence of
+      // ints starting at 0.  
+      composerFunctor<SearchKDTree<Mesh_Wrapper,Mesh_Wrapper>,
+                      IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>,
+                      Remap_1stOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind> >
+          composer(&search, &intersect, &remap, remap_var_names_[0]);
+
+      // This populates targetField with the doubles returned from
+      // the final remap For some reason (perhaps some code outside
+      // portage is pulling in a "using thrust" command), the
+      // compiler is not able to disambiguate Portage::transform and
+      // thrust::transform here. So, be explicit that we want
+      // Portage::transform
+      
+      Portage::transform((counting_iterator)target_mesh_.begin(CELL),
+                         (counting_iterator)target_mesh_.end(CELL),
+                         target_field,composer);
+      
+    }
+    else {
+      
+      if (remap_order() != 2)
+        std::cerr << 
+            "Remap order can be 1 or 2 only. Doing 2nd order remap" << 
+            std::endl;
+
+
+      std::cout << "Remapping variable " << remap_var_names_[0] << 
+          " using a 2nd order accurate algorithm" << std::endl;
+      
+      // Eventually put this in a loop over remap variable names as well
+      // Assume for now that we are only doing cell-based remap
+      const Remap_2ndOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind> 
+          remap(source_mesh_, source_state_, remap_entity_, remap_var_names_[0],
+                NOLIMITER);
+      
+          
+      // Create a cellIndices vector and populates with a sequence of
+      // ints starting at 0.  
+      composerFunctor<SearchKDTree<Mesh_Wrapper,Mesh_Wrapper>,
+                      IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>,
+                      Remap_2ndOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind> >
+          composer(&search, &intersect, &remap, remap_var_names_[0]);
+      
+      // This populates targetField with the doubles returned from
+      // the final remap For some reason (perhaps some code outside
+      // portage is pulling in a "using thrust" command), the
+      // compiler is not able to disambiguate Portage::transform and
+      // thrust::transform here. So, be explicit that we want
+      // Portage::transform
+      
+      Portage::transform((counting_iterator)target_mesh_.begin(CELL),
+                         (counting_iterator)target_mesh_.end(CELL),
+                         target_field,composer);
+      
     }
 
-    // Get the names of the variables to be remapped
-
-    std::vector<std::string> remap_var_names() {
-        return remap_var_names_;
-    }
-
-    /*!
-      \brief This method calls the search, intersect, and remap routines
-      needed to map one mesh to another.
-    */
-    void run()
-    {
-        std::printf("in Driver::run()...\n");
-       
-        // Get an instance of the desired search algorithm type
-        const SearchKDTree<Mesh_Wrapper,Mesh_Wrapper>
-                search(source_mesh_, target_mesh_);
-        
-        // Get an instance of the desired intersect algorithm type
-        const IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>
-                intersect{source_mesh_, target_mesh_};
-        
-        // Eventually put this in a loop over remap variable names as well
-        // Assume for now that we are only doing cell-based remap
-        const Remap_1stOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind>
-                remap(source_mesh_, source_state_, remap_entity_, remap_var_names_[0]);
-        
-        int numTargetCells = target_mesh_.num_owned_cells();
-        std::cout << "Number of target cells in target mesh "
-                  << numTargetCells << std::endl;
-
-        // Ask for a StateVector with the name remap_var_names_[0] to be
-        // added to the targetState_. If it is already present, the
-        // existing StateVector reference is returned. If its not present,
-        // it is added. This logic needs to be reversed. The find function
-        // should add it if it is not found (if so requested).
-
-        double *target_field_raw = NULL;
-        target_state_.get_data(remap_entity_,remap_var_names_[0],&target_field_raw);
-        Portage::pointer<double> target_field(target_field_raw);
-
-        // Create a cellIndices vector and populates with a sequence of
-        // ints starting at 0.  
-        composerFunctor<SearchKDTree<Mesh_Wrapper,Mesh_Wrapper>,
-            IntersectClipper<Mesh_Wrapper, Mesh_Wrapper>,
-            Remap_1stOrder<Mesh_Wrapper,Jali_State_Wrapper,Entity_kind> >
-                composer(&search, &intersect, &remap, remap_var_names_[0]);
-
-        // This populates targetField with the doubles returned from
-        // the final remap For some reason (perhaps some code outside
-        // portage is pulling in a "using thrust" command), the
-        // compiler is not able to disambiguate Portage::transform and
-        // thrust::transform here. So, be explicit that we want
-        // Portage::transform
-
-        Portage::transform((counting_iterator)target_mesh_.begin(CELL),
-                  (counting_iterator)target_mesh_.end(CELL),
-                  target_field,composer);
-
-    }
-
+  }
 
 private:
   
@@ -167,12 +244,11 @@ private:
     Jali_State_Wrapper & target_state_;
     std::vector<std::string> remap_var_names_;
     Entity_kind remap_entity_;
+  unsigned int remap_order_;
 
 }; // class Driver
 
 // This functor is used inside a std::transform inside Driver::run
-
-// Should we move composerFunctor into the .cc file? - RVG
 
 template <typename SearchType, typename IsectType, typename RemapType>
 struct composerFunctor
