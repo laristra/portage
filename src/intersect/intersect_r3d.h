d830a30c8163e847a32358a4931e21881739166e
Ondřej Čertík <ondrej.certik@gmail.com>
2016-01-12 09:40:39 -0700

    Add intersect_r3d.h and a test

---
+++ src/intersect/intersect_r3d.h
@@ -0,0 +1,67 @@
+/*---------------------------------------------------------------------------~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *---------------------------------------------------------------------------~*/
+
+#ifndef INTERSECT_R3D_H
+#define INTERSECT_R3D_H
+
+#include "search.h"
+#include "clipper.hpp"
+#include <iostream>
+#include <cmath>
+#include <cfloat>
+#include <algorithm>
+
+namespace Portage {
+
+/*!
+ * \class IntersectR3D <typename C> 3-D intersection algorithm
+ */
+
+template <typename SourceMeshType, typename TargetMeshType=SourceMeshType>
+class IntersectR3D {
+public:
+  typedef std::tuple<double, double, double> Point;
+  typedef std::vector<Point> Poly;
+  //Provide volume and centroid
+  typedef std::pair<double, Point> Moment;
+
+  IntersectR3D(const SourceMeshType &s, const TargetMeshType &t)
+    : sourceMeshWrapper(s), targetMeshWrapper(t) {}
+
+  /*! \brief Intersect two cells and return the first two moments.
+   * \param[in] cellA first cell index to intersect
+   * \param[in] cellB second cell index to intersect
+   * \return list of moments; ret[0] == 0th moment; ret[1] == first moment
+   */
+
+  std::vector<std::vector<double>> operator() (const int cellA,
+            const int cellB) const {
+    Poly elA = sourceMeshWrapper.cellToXYZ(cellA);
+    Poly elB = targetMeshWrapper.cellToXYZ(cellB);
+
+    // TODO: Convert elA and elB to tets
+    //
+    // TODO: call r3D here for those tets
+    std::vector<std::vector<double>> moments;
+    moments = {{1, 1.5, 1.5}};
+    return moments;
+  }
+
+  IntersectR3D() {}
+
+  //! Copy constructor (disabled)
+  IntersectR3D(const IntersectR3D &) = delete;
+
+  //! Assignment operator (disabled)
+  IntersectR3D & operator = (const IntersectR3D &) = delete;
+private:
+  const SourceMeshType &sourceMeshWrapper;
+  const TargetMeshType &targetMeshWrapper;
+}; // class IntersectR3D
+
+
+} // namespace Portage
+
+#endif // INTERSECT_R3D_H
---
+++ src/intersect/intersect_r3d_test.cc
@@ -0,0 +1,27 @@
+#include "intersectClipper.h" // TODO: remove this include
+#include "intersect_r3d.h"
+#include "gtest/gtest.h"
+#include "Mesh.hh"
+#include "../driver/driver.h"
+#include "portage/wrappers/mesh/jali/jali_mesh_wrapper.h"
+#include "MeshFactory.hh"
+
+TEST(intersectClipper, simple){
+  Jali::MeshFactory mf(MPI_COMM_WORLD);
+  Jali::Mesh* sm = mf(0,0,0, 2,2,2, 1,1,1);
+  Jali::Mesh* tm = mf(1,1,1, 2,2,2, 1,1,1);
+  Portage::Jali_Mesh_Wrapper s(*sm);
+  Portage::Jali_Mesh_Wrapper t(*tm);
+
+  Portage::IntersectR3D<Portage::Jali_Mesh_Wrapper> isect{s , t};
+  std::vector<std::vector<double> > moments = isect(0, 0);
+  for(int i=0;i<moments.size();i++){
+    for(int j=0;j<moments[i].size();j++){
+      std::cout << "i, j, m " << i << ", " << j << ", " << moments[i][j] << std::endl;
+    }
+  }
+
+  ASSERT_EQ(moments[0][0], 1);
+  ASSERT_EQ(moments[0][1], 1.5);
+  ASSERT_EQ(moments[0][2], 1.5);
+}
