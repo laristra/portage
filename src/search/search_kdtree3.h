6fc10f49aa06fc0e97df5ba9757e81cca92531c2
Charles Ferenbaugh <cferenba@lanl.gov>
2016-01-12 16:50:36 -0700

    Add 3D search structure and tests

---
+++ src/search/search_kdtree3.h
@@ -0,0 +1,127 @@
+/*---------------------------------------------------------------------------~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *---------------------------------------------------------------------------~*/
+
+#ifndef SEARCH_KDTREE3_H
+#define SEARCH_KDTREE3_H
+
+/*!
+    \class SearchKDTree3 search_kdtree.h
+    \brief SearchKDTree3 provides...
+ */
+
+#include <vector>
+#include <tuple>
+
+#include "Point.h"
+#include "BoundBox.h"
+#include "kdtree.h"
+
+
+
+namespace Portage {
+
+template <typename SourceMeshType, typename TargetMeshType>
+class SearchKDTree3 {
+  public:
+
+    //! Default constructor (disabled)
+    SearchKDTree3() = delete;
+    
+    //! Constructor with Meshes
+    /*!
+      \brief Builds the kd-tree for finding intersection
+      
+      \param source_mesh   pointer to wrapper for getting the source mesh info
+      \param target_mesh   pointer to wrapper for getting the target mesh info 
+      
+      Constructor for kd-tree for finding cells from a source
+      mesh that overlap the target mesh
+
+    */
+
+    SearchKDTree3(const SourceMeshType & source_mesh, 
+                 const TargetMeshType & target_mesh)
+            : sourceMesh_(source_mesh), targetMesh_(target_mesh)  {
+
+        int numCells = sourceMesh_.num_owned_cells() + 
+                sourceMesh_.num_ghost_cells();
+        std::vector<gk::IsotheticBBox<3>> bboxes;
+        bboxes.reserve(numCells);
+        
+        // find bounding boxes for all cells
+        for (int c = 0; c < numCells; ++c) {
+            std::vector<std::tuple<double,double,double>> cell_coord;
+            sourceMesh_.cell_get_coordinates(c, &cell_coord);
+            gk::IsotheticBBox<3> bb;
+            for (int n = 0; n < cell_coord.size(); ++n) {
+                bb.add(gk::Point<3>(std::get<0>(cell_coord[n]),
+                                    std::get<1>(cell_coord[n]),
+                                    std::get<2>(cell_coord[n])));
+            }
+            bboxes.emplace_back(bb);
+        }
+
+        // create the kd-tree
+        tree_ = gk::KDTreeCreate(bboxes);
+
+    } // SearchKDTree3::SearchKDTree3
+
+    //! Copy constructor (disabled)
+    SearchKDTree3(const SearchKDTree3 &) = delete;
+
+    //! Assignment operator (disabled)
+    SearchKDTree3 & operator = (const SearchKDTree3 &) = delete;
+
+    //! Destructor
+    ~SearchKDTree3() { if (tree_) delete tree_; }
+
+    /*!
+      \brief returns source mesh cells potentially overlapping given target cell
+
+      \param cellId the index of the target cell that I pass in...
+      \param candidates pointer to vector of potential candidate cells in sourceMesh
+    */
+    void search(const int cellId, std::vector<int> *candidates) const;
+
+  private:
+
+    // Aggregate data members
+    const SourceMeshType & sourceMesh_;
+    const TargetMeshType & targetMesh_;
+    gk::KDTree<3>* tree_;
+
+}; // class SearchKDTree3
+
+
+
+template<typename SourceMeshType, typename TargetMeshType>
+void SearchKDTree3<SourceMeshType,TargetMeshType>::
+search(const int cellId, std::vector<int> *candidates)
+const {
+    // find bounding box for target cell
+    std::vector<std::tuple<double,double,double>> cell_coord;
+    targetMesh_.cell_get_coordinates(cellId, &cell_coord);
+    gk::IsotheticBBox<3> bb;
+    for (int n = 0; n < cell_coord.size(); ++n) {
+        bb.add(gk::Point<3>(std::get<0>(cell_coord[n]),
+                            std::get<1>(cell_coord[n]),
+                            std::get<2>(cell_coord[n])));
+    }
+
+    // now see which sourceMesh cells have bounding boxes overlapping
+    // with target cell, using the kdtree
+    std::vector<long> lcandidates;
+    gk::Intersect(bb, tree_, lcandidates);
+    candidates->assign(lcandidates.begin(), lcandidates.end());
+
+} // SearchKDTree3::search
+
+
+
+
+} // namespace Portage
+
+#endif // SEARCH_KDTREE3_H
+
---
+++ src/search/search_kdtree3_test.cc
@@ -0,0 +1,50 @@
+/*--------------------------------------------------------------------------~~*
+ * Copyright (c) 2014 Los Alamos National Security, LLC
+ * All rights reserved.
+ *--------------------------------------------------------------------------~~*/
+
+#include "search_kdtree3.h"
+
+#include <algorithm>
+
+#include "gtest/gtest.h"
+
+#include "Mesh.hh"
+#include "MeshFactory.hh"
+
+#include "portage/wrappers/mesh/jali/jali_mesh_wrapper.h"
+
+TEST(search_kdtree3, case1)
+{
+    Jali::MeshFactory mf(MPI_COMM_WORLD);
+    Jali::Mesh *smesh = mf(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 3, 3, 3);
+    Jali::Mesh *tmesh = mf(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 2, 2, 2);
+    Portage::Jali_Mesh_Wrapper source_mesh_wrapper(*smesh);
+    Portage::Jali_Mesh_Wrapper target_mesh_wrapper(*tmesh);
+
+    Portage::SearchKDTree3<Portage::Jali_Mesh_Wrapper,Portage::Jali_Mesh_Wrapper> search(source_mesh_wrapper, target_mesh_wrapper);
+
+    for (int tc = 0; tc < 8; ++tc) {
+        std::vector<int> candidates;
+        search.search(tc, &candidates);
+
+        ASSERT_EQ(8, candidates.size());
+        int tx = tc % 2;
+        int ty = (tc / 2) % 2;
+        int tz = tc / 4;
+        int scbase = tx + ty * 3 + tz * 9;
+        // candidates might not be in order, so sort them
+        std::sort(candidates.begin(), candidates.end());
+        ASSERT_EQ(scbase,      candidates[0]);
+        ASSERT_EQ(scbase + 1,  candidates[1]);
+        ASSERT_EQ(scbase + 3,  candidates[2]);
+        ASSERT_EQ(scbase + 4,  candidates[3]);
+        ASSERT_EQ(scbase + 9,  candidates[4]);
+        ASSERT_EQ(scbase + 10, candidates[5]);
+        ASSERT_EQ(scbase + 12, candidates[6]);
+        ASSERT_EQ(scbase + 13, candidates[7]);
+    }
+
+} // TEST(search_kdtree3, case1)
+
+
