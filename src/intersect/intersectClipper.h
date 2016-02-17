cdf322587a69956a16c0c74860f2d26970514871
Jenkins <angelah@varan.lanl.gov>
2016-02-17 15:03:54 -0700

    fixed formatting/auto range loops/const

--- src/intersect/intersectClipper.h
+++ src/intersect/intersectClipper.h
@@ -19,26 +19,26 @@
   @returns std::vector<double>--area, mx, my
 */
 std::vector<double> areaAndMomentPolygon(const std::vector<std::pair<double,double> > poly){
-    double area = 0;
-    double cx = 0;
-    double cy = 0;
-    std::vector <double> ret;
-    for (int i=0;i<poly.size()-1;i++){
-        double a = (poly[i].first*poly[i+1].second-poly[i+1].first*poly[i].second);  
-        area+=a;
-        cx+= (poly[i].first+poly[i+1].first)*a;
-        cy+= (poly[i].second+poly[i+1].second)*a;
-    }
-    int lastIndex = poly.size()-1;
-    //close the polygon
-    double a = poly[lastIndex].first*poly[0].second - poly[0].first*poly[lastIndex].second;
-    area+= a;
-    cx+= (poly[lastIndex].first+poly[0].first)*a;
-    cy+= (poly[lastIndex].second+poly[0].second)*a;
-    ret.emplace_back(.5*area);
-    ret.emplace_back(cx/6.);
-    ret.emplace_back(cy/6.);
-    return ret;
+  double area = 0;
+  double cx = 0;
+  double cy = 0;
+  std::vector <double> ret;
+  for(int i=0;i<poly.size()-1;i++){
+    double a = (poly[i].first*poly[i+1].second-poly[i+1].first*poly[i].second);  
+    area+=a;
+    cx+= (poly[i].first+poly[i+1].first)*a;
+    cy+= (poly[i].second+poly[i+1].second)*a;
+  }
+  int lastIndex = poly.size()-1;
+  //close the polygon
+  double a = poly[lastIndex].first*poly[0].second - poly[0].first*poly[lastIndex].second;
+  area+= a;
+  cx+= (poly[lastIndex].first+poly[0].first)*a;
+  cy+= (poly[lastIndex].second+poly[0].second)*a;
+  ret.emplace_back(.5*area);
+  ret.emplace_back(cx/6.);
+  ret.emplace_back(cy/6.);
+  return ret;
 }
 
 /*!
@@ -55,133 +55,132 @@ template <typename SourceMeshType, typename TargetMeshType=SourceMeshType> class
 {
 
 public:
-    // @todo Consolidate the concept of 'Points' amongst the various code bits, e.g. this Point,
-    // JaliGeometry::Point, gk::Point, etc.
+// @todo Consolidate the concept of 'Points' amongst the various code bits, e.g. this Point,
+// JaliGeometry::Point, gk::Point, etc.
 
-    /// Alias for a pair of (x,y) coordinates
-    typedef std::pair<double, double> Point;
-    /// Alias for a collection of Points.
-    typedef std::vector<Point> Poly; 
-    /// Alias to provide volume and centroid
-    typedef std::pair<double, Point> Moment;
+/// Alias for a pair of (x,y) coordinates
+typedef std::pair<double, double> Point;
+/// Alias for a collection of Points.
+typedef std::vector<Point> Poly; 
+/// Alias to provide volume and centroid
+typedef std::pair<double, Point> Moment;
 
-     /// Constructor taking a source mesh @c s and a target mesh @c t.
-    IntersectClipper(const SourceMeshType &s, const TargetMeshType &t):sourceMeshWrapper(s), targetMeshWrapper(t){}
+/// Constructor taking a source mesh @c s and a target mesh @c t.
+IntersectClipper(const SourceMeshType &s, const TargetMeshType &t):sourceMeshWrapper(s), targetMeshWrapper(t){}
 
-     /*!
-       @brief Intersect two cells and return the first two moments.
-       @param[in] cellA first cell index to intersect
-       @param[in] cellB second cell index to intersect
-       @return list of moments; ret[0] == 0th moment; ret[1] == first moment
-     */
-    std::vector<std::vector<double> > operator() (const int cellA, const int cellB) const {      
-        const Poly polyA = sourceMeshWrapper.cellToXY(cellA);
-        const Poly polyB = targetMeshWrapper.cellToXY(cellB);
-        double max_size_poly = 0;
-        max_size_poly = IntersectClipper::updateMaxSize(polyA, max_size_poly);
-        max_size_poly = IntersectClipper::updateMaxSize(polyB, max_size_poly);
+/*!
+  @brief Intersect two cells and return the first two moments.
+  @param[in] cellA first cell index to intersect
+  @param[in] cellB second cell index to intersect
+  @return list of moments; ret[0] == 0th moment; ret[1] == first moment
+*/
+std::vector<std::vector<double> > operator() (const int cellA, const int cellB) const {      
+  const Poly polyA = sourceMeshWrapper.cellToXY(cellA);
+  const Poly polyB = targetMeshWrapper.cellToXY(cellB);
+  double max_size_poly = 0;
+  max_size_poly = IntersectClipper::updateMaxSize(polyA, max_size_poly);
+  max_size_poly = IntersectClipper::updateMaxSize(polyB, max_size_poly);
 
-        const ClipperLib::Path intPolyA = IntersectClipper::convertPoly2int(polyA, max_size_poly);
-        const ClipperLib::Path intPolyB = IntersectClipper::convertPoly2int(polyB, max_size_poly);
+  const ClipperLib::Path intPolyA = IntersectClipper::convertPoly2int(polyA, max_size_poly);
+  const ClipperLib::Path intPolyB = IntersectClipper::convertPoly2int(polyB, max_size_poly);
 
-        //Make clipper aware of polyA and polyB
-        ClipperLib::Clipper clpr;
-        clpr.AddPath(intPolyA, ClipperLib::ptSubject, true);
-        clpr.AddPath(intPolyB, ClipperLib::ptClip, true);
+  //Make clipper aware of polyA and polyB
+  ClipperLib::Clipper clpr;
+  clpr.AddPath(intPolyA, ClipperLib::ptSubject, true);
+  clpr.AddPath(intPolyB, ClipperLib::ptClip, true);
 
-        //Compute the intersection of all paths which have been added to clipper (in this case
-        //polyA and polyB; use the Even-Odd winding rule in case of self-intersecting polygons
-        //For more information on the winding rules see:
-        //http://www.angusj.com/delphi/clipper/documentation/Docs/Units/ClipperLib/Types/PolyFillType.htm
-        ClipperLib::Paths solution;
-        clpr.Execute(ClipperLib::ctIntersection, solution,
-		     ClipperLib::pftEvenOdd, ClipperLib::pftEvenOdd);
+  //Compute the intersection of all paths which have been added to clipper (in this case
+  //polyA and polyB; use the Even-Odd winding rule in case of self-intersecting polygons
+  //For more information on the winding rules see:
+  //http://www.angusj.com/delphi/clipper/documentation/Docs/Units/ClipperLib/Types/PolyFillType.htm
+  ClipperLib::Paths solution;
+  clpr.Execute(ClipperLib::ctIntersection, solution,
+               ClipperLib::pftEvenOdd, ClipperLib::pftEvenOdd);
 
-        std::vector< std::vector<std::pair<double, double>> > intersectionList;
-        for(int i=0;i<solution.size();i++){
-            std::vector<std::pair<double, double> > poly;
-            for(int j=0;j<solution[i].size();j++){
-                //Build list of intersections and convert back to doubles
-                poly.emplace_back(std::make_pair(integer2real(solution[i][j].X, max_size_poly), integer2real(solution[i][j].Y, max_size_poly)));
-            }
-            intersectionList.emplace_back(poly);
-        }
-        std::vector<std::vector<double>> moments;
-        for (int i=0;i<intersectionList.size();i++){
-            moments.emplace_back(areaAndMomentPolygon(intersectionList[i]));
-        }    
-        return moments;
+  std::vector< std::vector<std::pair<double, double>> > intersectionList;
+  for(auto const &i: solution){
+    std::vector<std::pair<double, double> > poly;
+    for(auto const &j: i){
+      //Build list of intersections and convert back to doubles
+      poly.emplace_back(std::make_pair(integer2real(j.X, max_size_poly), integer2real(j.Y, max_size_poly)));
     }
+    intersectionList.emplace_back(poly);
+  }
+  std::vector<std::vector<double>> moments;
+  for(auto const &i: intersectionList){
+    moments.emplace_back(areaAndMomentPolygon(i));
+  }    
+  return moments;
+}
 
-    /// Default constructor.
-    IntersectClipper() {}
+/// Default constructor.
+IntersectClipper() {}
 
-    /// Copy constructor (disabled)
-    IntersectClipper(const IntersectClipper &) = delete;
+/// Copy constructor (disabled)
+IntersectClipper(const IntersectClipper &) = delete;
 
-    /// Assignment operator (disabled)
-    IntersectClipper & operator = (const IntersectClipper &) = delete;
+/// Assignment operator (disabled)
+IntersectClipper & operator = (const IntersectClipper &) = delete;
 
 
 private:
 
-    //We must use the same max size for all the polygons, so the number we are looking for is the maximum value in the set--all the X and Y values will be converted using this value
-    static double updateMaxSize( const std::vector<std::pair<double, double> > poly, double max_size_poly){
-        for(auto i = poly.begin(), e=poly.end();i!=e;++i){
-            double m = std::max(std::abs(i->first), std::abs(i->second));
-            if (m > max_size_poly) max_size_poly = m;
-        }
-        return max_size_poly;
-    }
+//We must use the same max size for all the polygons, so the number we are looking for is the maximum value in the set--all the X and Y values will be converted using this value
+static double updateMaxSize( const std::vector<std::pair<double, double> > poly, double max_size_poly){
+  for(auto &i: poly){
+    double m = std::max(std::abs(i.first), std::abs(i.second));
+    if (m > max_size_poly) max_size_poly = m;
+  }
+  return max_size_poly;
+}
 
-    /*!
-       @brief Convert a double to an integer.
+/*!
+  @brief Convert a double to an integer.
        
-       @note Could do this by multiplying by a power of 10 large enough to preserve the 
-       number of digits in the original double; however it is more precise to
-       multiply by a power of 2.
+  @note Could do this by multiplying by a power of 10 large enough to preserve the 
+  number of digits in the original double; however it is more precise to
+  multiply by a power of 2.
        
-       @param[in] a number to be converted
-       @param[in] max_size
-    */
-    static long real2integer(double a, const double max_size){
-        int exp;
+  @param[in] a number to be converted
+  @param[in] max_size
+*/
+static long real2integer(double a, const double max_size){
+  int exp;
 
-        // We want to move the decimal point of the floating point number so that the last digit given is before the decimal point.
-        // The alogrithm is to find the exponent (in the floating point representation) of the largest number in the polygons (either X or Y coordinate).  
-        //Multiply by the power of two which is number of digits in mantissa (in this case, 53) - exponent
-        //lrint, then rounds to an integer
+  // We want to move the decimal point of the floating point number so that the last digit given is before the decimal point.
+  // The alogrithm is to find the exponent (in the floating point representation) of the largest number in the polygons (either X or Y coordinate).  
+  //Multiply by the power of two which is number of digits in mantissa (in this case, 53) - exponent
+  //lrint, then rounds to an integer
 
-        //Compute the exponent 
-        frexp(max_size, &exp);  
-        //Size of long must be >= size double
-        return lrint(ldexp(a, DBL_MANT_DIG-exp));
-    }
+  //Compute the exponent 
+  frexp(max_size, &exp);  
+  //Size of long must be >= size double
+  return lrint(ldexp(a, DBL_MANT_DIG-exp));
+}
 
-    /*! 
-      @brief Convert integer back to double given the max_size of the numbers within the problem
-      @param[in] a number to convert
-      @param[in] max_size 
-    */
-    static double integer2real(long a, const double max_size){
-        int exp;
-        frexp(max_size, &exp);  
-        return ldexp(a, exp-DBL_MANT_DIG);
-    }
+/*! 
+  @brief Convert integer back to double given the max_size of the numbers within the problem
+  @param[in] a number to convert
+  @param[in] max_size 
+*/
+static double integer2real(long a, const double max_size){
+  int exp;
+  frexp(max_size, &exp);  
+  return ldexp(a, exp-DBL_MANT_DIG);
+}
 
-    //Convert an entire polygon (specifiied as a std::vector<std::pair<double, double>) to a std::vector<IntPoint>
-    static std::vector<ClipperLib::IntPoint> convertPoly2int(  std::vector<std::pair<double, double> > poly, double max_size_poly){
-        std::vector<ClipperLib::IntPoint> intpoly(poly.size());
-        std::transform(poly.begin(), poly.end(), intpoly.begin(), [max_size_poly](std::pair<double, double> point){
-                return ClipperLib::IntPoint(real2integer(point.first, max_size_poly), real2integer(point.second, max_size_poly));}); 
-        return intpoly;
-    }
+//Convert an entire polygon (specifiied as a std::vector<std::pair<double, double>) to a std::vector<IntPoint>
+static std::vector<ClipperLib::IntPoint> convertPoly2int(  std::vector<std::pair<double, double> > poly, double max_size_poly){
+  std::vector<ClipperLib::IntPoint> intpoly(poly.size());
+  std::transform(poly.begin(), poly.end(), intpoly.begin(), [max_size_poly](std::pair<double, double> point){
+      return ClipperLib::IntPoint(real2integer(point.first, max_size_poly), real2integer(point.second, max_size_poly));}); 
+  return intpoly;
+}
   
 private:
-    const SourceMeshType &sourceMeshWrapper;
-    const TargetMeshType &targetMeshWrapper;
+const SourceMeshType &sourceMeshWrapper;
+const TargetMeshType &targetMeshWrapper;
 
 }; // class IntersectClipper
 
 #endif // INTERSECT_CLIPPER_H
-
--- src/intersect/intersectClipperTest.cc
+++ src/intersect/intersectClipperTest.cc
@@ -4,7 +4,13 @@
 #include "../driver/driver.h"
 #include "portage/wrappers/mesh/jali/jali_mesh_wrapper.h"
 #include "MeshFactory.hh"
-
+/*! 
+ * @brief Intersect two cells on two single cell meshes to compute moments.
+ * This exercises Clipper as well as the intersectClipper class.
+ * Intersect two cells contained in the mesh: 
+ * (0, 0) (2, 0) (2, 2) (0,2) with (1,1) (2,1) (2,2) (1,2)
+ * Results should be an area of 1 and a centroid of 1.5, 1.5.
+ */
 TEST(intersectClipper, simple){
   Jali::MeshFactory mf(MPI_COMM_WORLD);
   //Create mesh from 0, 0 to 2.4, 2 1x1
@@ -12,7 +18,6 @@ TEST(intersectClipper, simple){
   std::unique_ptr<Jali::Mesh> tm = std::unique_ptr<Jali::Mesh>(mf(1,1,2, 2, 1, 1));
   Portage::Jali_Mesh_Wrapper s(*sm);
   Portage::Jali_Mesh_Wrapper t(*tm);
-
   IntersectClipper<Portage::Jali_Mesh_Wrapper> isect{s , t};
   std::vector<std::vector<double> > moments = isect(0, 0); 
   for(int i=0;i<moments.size();i++){
@@ -26,32 +31,31 @@ TEST(intersectClipper, simple){
   ASSERT_EQ(moments[0][2], 1.5);
 }
 
-TEST(intersectClipper, convex){
-  std::vector<JaliGeometry::Point> cellA, cellB;
-  cellA.emplace_back(2,5);
-  cellA.emplace_back(10.5,5);
-  cellA.emplace_back(10.5, 3.5);
-  cellA.emplace_back(2,3.5);
+//@todo Figure out a way to convert this older test to an intersection test in the current framework
+// TEST(intersectClipper, convex){
+//   std::vector<JaliGeometry::Point> cellA, cellB;
+//   cellA.emplace_back(2,5);
+//   cellA.emplace_back(10.5,5);
+//   cellA.emplace_back(10.5, 3.5);
+//   cellA.emplace_back(2,3.5);
 
-  cellB.emplace_back(2.5,0);
-  cellB.emplace_back(8.5,0);
-  cellB.emplace_back(8.5,5);
-  cellB.emplace_back(5.5,2.5);
-  cellB.emplace_back(2.5,4);
-  
-  IntersectClipper<Portage::Jali_Mesh_Wrapper> isect{Jali_Mesh_Wrapper, Jali_Mesh_Wrapper};
-  std::vector<std::vector<double> > moments = isect(cellA, cellB); 
-  for(int i=0;i<moments.size();i++){
-    for(int j=0;j<moments[i].size();j++){
-      std::cout << "i, j, m " << i << ", " << j << ", " << moments[i][j] << std::endl;
-    }   
-  }
-  double eps = 1e-6;
-  EXPECT_NEAR(moments[0][0], 1.35, eps);
-  EXPECT_NEAR(moments[0][1], 10.665, eps);
-  EXPECT_NEAR(moments[0][2], 5.4, eps);
-  EXPECT_NEAR(moments[1][0], .25, eps);
-  EXPECT_NEAR(moments[1][1], .708333, eps);
-  EXPECT_NEAR(moments[1][2], .916667, eps);
-}
+//   cellB.emplace_back(2.5,0);
+//   cellB.emplace_back(8.5,0);
+//   cellB.emplace_back(8.5,5);
+//   cellB.emplace_back(5.5,2.5);
+//   cellB.emplace_back(2.5,4);
 
+//   std::vector<std::vector<double> > moments = isect(cellA, cellB); 
+//   for(int i=0;i<moments.size();i++){
+//     for(int j=0;j<moments[i].size();j++){
+//       std::cout << "i, j, m " << i << ", " << j << ", " << moments[i][j] << std::endl;
+//     }   
+//   }
+//   double eps = 1e-6;
+//   EXPECT_NEAR(moments[0][0], 1.35, eps);
+//   EXPECT_NEAR(moments[0][1], 10.665, eps);
+//   EXPECT_NEAR(moments[0][2], 5.4, eps);
+//   EXPECT_NEAR(moments[1][0], .25, eps);
+//   EXPECT_NEAR(moments[1][1], .708333, eps);
+//   EXPECT_NEAR(moments[1][2], .916667, eps);
+// }
