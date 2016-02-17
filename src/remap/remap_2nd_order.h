5e8ee6904cb82dac8c56d1d2ab87584e31143a4f
Rao Garimella <rao@lanl.gov>
2016-02-17 15:13:49 -0700

    avoid computation of contribution from a source cell if the volume of intersection with that cell is zero; also some comment restructuring

--- src/remap/remap_2nd_order.h
+++ src/remap/remap_2nd_order.h
@@ -20,7 +20,10 @@ namespace Portage {
 /*!
   @class Remap_2ndOrder remap_2nd_order.h
   @brief Remap_2ndOrder does a 2nd order remap of scalars
-
+  @tparam MeshType The type of the mesh wrapper used to access mesh info
+  @tparam StateType The type of the state manager used to access data.
+  @tparam OnWhatType The type of entity-based data we wish to remap; e.g. does
+  it live on nodes, cells, edges, etc.
 
   [1] Margolin, L.G. and Shashkov, M.J. "Second-order sign-preserving
   conservative interpolation (remapping) on general grids." Journal of
@@ -39,6 +42,14 @@ template<typename MeshType, typename StateType, Entity_kind on_what>
 class Remap_2ndOrder {
  public:
   
+  /*! 
+    @brief Constructor
+    @param[in] source_mesh The mesh wrapper used to query source mesh info
+    @param[in] source_state The state-manager wrapper used to query field info
+    @param[in] remap_var_name Name of the field to be remapped
+    @param[in] limiter_type Gradient limiter type (see gradient.h)
+  */
+
   Remap_2ndOrder(MeshType const & source_mesh, StateType const & source_state,
                  std::string const remap_var_name,
                  LimiterType const limiter_type) :
@@ -106,8 +117,8 @@ class Remap_2ndOrder {
   operator() (std::pair<std::vector<int> const &, 
               std::vector< std::vector<double> > const &> sources_and_weights) const {
     // not implemented for all types - see specialization for cells and nodes
-  throw 
-      std::runtime_error("Remap operator not implemented for this entity type");
+    
+    std::cerr << "Remap operator not implemented for this entity type\n";
   }
 
  private:
@@ -184,15 +195,17 @@ class Remap_2ndOrder<MeshType,StateType,CELL> {
   /*! 
     @brief Functor to do the actual remap calculation
     @param[in] cells_and_weights A pair of two vectors
-    @c sources_and_weights.first() is the vector of source entity indices 
-    in the source mesh that will contribute to the current target mesh entity.
-    @c sources_and_weights.second() is the vector of vector weights for each
-    of the source mesh entities in @c sources_and_weights.first().  Each element
-    of the weights vector is a moment of the source data over the target
-    entity; for first order remap, only the first element (or zero'th moment)
-    of the weights vector (i.e. the volume of intersection) is used. Source 
-    entities may be repeated in the list if the intersection of a target entity
-    and a source entity consists of two or more disjoint pieces
+    @c sources_and_weights.first() is the vector of source entity
+    indices in the source mesh that will contribute to the current
+    target mesh entity.  @c sources_and_weights.second() is the vector
+    of vector weights for each of the source mesh entities in @c
+    sources_and_weights.first().  Each element of the weights vector
+    is a moment of the source data over the target entity; for first
+    order remap, only the first element (or zero'th moment) of the
+    weights vector (i.e. the volume of intersection) is used. Source
+    entities may be repeated in the list if the intersection of a
+    target entity and a source entity consists of two or more disjoint
+    pieces
     
     @todo Cleanup the datatype for sources_and_weights - it is somewhat confusing.
 
@@ -256,6 +269,9 @@ double Remap_2ndOrder<MeshType,StateType,CELL> :: operator()
     int srccell = source_cells[j];
     std::vector<double> xsect_weights = weights[j];
     double xsect_volume = xsect_weights[0];
+
+    double eps = 1e-30;
+    if (xsect_volume <= eps) continue; // no intersection
     
     std::vector<double> srccell_centroid(spdim);
     source_mesh_.cell_centroid(srccell,&srccell_centroid);
@@ -410,6 +426,9 @@ double Remap_2ndOrder<MeshType,StateType,NODE> :: operator()
     int srccell = source_cells[j];
     std::vector<double> xsect_weights = weights[j];
     double xsect_volume = xsect_weights[0];
+
+    double eps = 1e-30;
+    if (xsect_volume <= eps) continue; // no intersection
     
     // note: here we are getting the node coord, not the centroid of the dual cell
     std::vector<double> srccell_coord(spdim); 
