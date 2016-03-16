945bae89e847d23c61f6b261c3a2acc81848f4c6
Chris Malone <cmalone@lanl.gov>
2016-03-16 10:55:16 -0600

    cleanup

--- app/main_flecsi.cc
+++ app/main_flecsi.cc
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iterator>
 
+#include "portage/support/Point.h"
 #include "portage/wrappers/mesh/flecsi/flecsi_mesh_wrapper.h"
 #include "portage/wrappers/state/flecsi/flecsi_state_wrapper.h"
 #include "portage/driver/driver.h"
@@ -14,9 +15,7 @@
 
 using mesh_t = flecsi::burton_mesh_t;
 
-using vertex_t = flecsi::mesh_t::vertex_t;
 using real_t = flecsi::mesh_t::real_t;
-//using vector_t = flecsi::mesh_t::vector_t;
 
 /*!
   @file main_flecsi.cc
@@ -54,8 +53,8 @@ int main(int argc, char** argv) {
 
   // Setup the meshes
   mesh_t inputMesh, targetMesh;
-  // This isn't working because it resets inputMesh to the same as targetMesh...
   Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx, ny, inputMesh);
+  // Example of how to get information directly from mesh object
   // std::cout << "input mesh before target mesh creattion" << std::endl;
   // for (auto v : inputMesh.vertices()) {
   //   std::cout << "coords: " << v->coordinates() << std::endl;
@@ -67,6 +66,7 @@ int main(int argc, char** argv) {
   auto someCellIndex = 5;
   auto thisCell = inputMesh.cells()[someCellIndex];
 
+  // Examples of getting data from mesh object or wrapper
   std::vector<int> nodeIDs;
   inputMeshWrapper.cell_get_nodes(someCellIndex, &nodeIDs);
   std::cout << "node IDs from wrapper: ";
@@ -87,188 +87,25 @@ int main(int argc, char** argv) {
   std::cout << "centroid from mesh:    [ ";
   std::cout << thisCell->centroid() << std::endl;
 
-  std::vector<std::pair<double,double>> nodeCoords;
+  //  std::vector<std::pair<double,double>> nodeCoords;
+  std::vector<Portage::Point2> nodeCoords;
   inputMeshWrapper.cell_get_coordinates(someCellIndex, &nodeCoords);
   std::cout << "node coordinates from wrapper:" << std::endl;
   for (auto nc : nodeCoords)
-    std::cout << "[ " << nc.first << " " << nc.second << " ]" << std::endl;
+    std::cout << nc << std::endl;
+    // std::cout << "[ " << nc.first << " " << nc.second << " ]" << std::endl;
   std::cout << "node coordinates from mesh:" << std::endl;
   for (auto n : inputMesh.vertices(thisCell))
     std::cout << n->coordinates() << std::endl;
 
   return 0;
 
+  // This is broken because of the singleton issue in FleCSI state manager.
   // Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx+1, ny+1, targetMesh);
   // std::cout << "input mesh after target mesh creattion" << std::endl;
   // for (auto v : inputMesh.vertices()) {
   //   std::cout << "coords: " << v->coordinates() << std::endl;
   // }
-
-
-
-
-
-
-
-
-  // Do this by hand...
-  // auto dx = (xmax - xmin) / real_t(nx);
-  // auto dy = (ymax - ymin) / real_t(ny);
-  // std::cout << "dx dy " << dx << " " << dy << std::endl;
-  // auto num_verts = (nx+1)*(ny+1);
-  // inputMesh.init_parameters(num_verts);
-  // std::vector<vertex_t*> verts;
-  // for (auto j = 0; j < ny+1; ++j) {
-  //   for (auto i = 0; i < nx+1; ++i) {
-  //     auto vert = inputMesh.create_vertex(
-  //         {xmin + dx*real_t(i), ymin + dy*real_t(j)});
-  //     verts.push_back(vert);
-  //   }
-  // }
-  // auto nx1 = nx + 1;
-  // for (auto j = 0; j < ny; ++j) {
-  //   for (auto i = 0; i < nx; ++i) {
-  //     auto c = inputMesh.create_cell({verts[i + j*nx1],
-  //             verts[i + 1 + j*nx1],
-  //             verts[i + 1 + (j + 1)*nx1],
-  //             verts[i + (j + 1)*nx1]});
-  //   }
-  // }
-  // inputMesh.init();
-  // std::cout << "input mesh " << std::endl;
-  // for (auto v : inputMesh.vertices()) {
-  //   std::cout << "coords: " << v->coordinates() << std::endl;
-  // }
-
-  // // target mesh
-  // ++nx;
-  // ++ny;
-  // dx = (xmax - xmin) / real_t(nx);
-  // dy = (ymax - ymin) / real_t(ny);
-  // std::cout << "dx dy " << dx << " " << dy << std::endl;
-  // num_verts = (nx+1)*(ny+1);
-  // targetMesh.init_parameters(num_verts);
-  // verts.clear();
-  // for (auto j = 0; j < ny+1; ++j) {
-  //   for (auto i = 0; i < nx+1; ++i) {
-  //     auto vert = targetMesh.create_vertex(
-  //         {xmin + dx*real_t(i), ymin + dy*real_t(j)});
-  //     verts.push_back(vert);
-  //   }
-  // }
-  // nx1 = nx + 1;
-  // for (auto j = 0; j < ny; ++j) {
-  //   for (auto i = 0; i < nx; ++i) {
-  //     auto c = targetMesh.create_cell({verts[i + j*nx1],
-  //             verts[i + 1 + j*nx1],
-  //             verts[i + 1 + (j + 1)*nx1],
-  //             verts[i + (j + 1)*nx1]});
-  //   }
-  // }
-  // targetMesh.init();
-
-  // std::cout << "input mesh " << std::endl;
-  // for (auto v : inputMesh.vertices()) {
-  //   std::cout << "coords: " << v->coordinates() << std::endl;
-  // }
-  // // mesh_t targetMesh;
-  // // Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx+1, ny+1, targetMesh);
-  // // std::cout << "input mesh " << std::endl;
-  // // for (auto v : inputMesh.vertices()) {
-  // //   std::cout << "coords: " << v->coordinates() << std::endl;
-  // // }
-
-  // std::cout << "target mesh " << std::endl;
-  // for (auto v : targetMesh.vertices()) {
-  //   std::cout << "coords: " << v->coordinates() << std::endl;
-  // }
-
-
-
-  // // Setup the wrappers
-  // Portage::Flecsi_Mesh_Wrapper inputMeshWrapper(inputMesh);
-  // Portage::Flecsi_Mesh_Wrapper targetMeshWrapper(targetMesh);
-
-  // // Register the state data with the meshes; this just lets the statemanager
-  // // know that there will be data
-  // // NOTES: register_state is a macro defined in burton.h
-  // //        cells is an enum in burton_mesh_traits.h
-  // register_state(inputMesh, "celldata", cells, real_t, flecsi::persistent);
-  // // NOTE:
-  // // There is a naming conflict here for some reason - must change variable name
-  // //  register_state(targetMesh, "celldata", cells, real_t, flecsi::persistent);
-  // register_state(targetMesh, "celldata_tar", cells, real_t, flecsi::persistent);
-
-  // // Setup the iniital data
-  // // NOTE: access_state is a macro defined in burton.h
-  // auto cd = access_state(inputMesh, "celldata", real_t);
-  // // linear function
-  // for (auto c : inputMesh.cells()) {
-  //   auto cen = c->centroid();
-  //   cd[c] = cen[0] + cen[1];
-  // }
-
-  // // Make the state wrappers
-  // Portage::Flecsi_State_Wrapper sourceStateWrapper(inputMesh);
-  // Portage::Flecsi_State_Wrapper targetStateWrapper(targetMesh);
-
-  // // Build the main driver data for this remap
-  // Portage::Driver<Portage::Flecsi_Mesh_Wrapper,
-  //                 Portage::Flecsi_State_Wrapper> d(Portage::CELL,
-  //                                                  inputMeshWrapper,
-  //                                                  sourceStateWrapper,
-  //                                                  targetMeshWrapper,
-  //                                                  targetStateWrapper);
-
-  // // Register the variables to be remapped
-  // std::vector<std::string> src_remap_fields, tar_remap_fields;
-  // src_remap_fields.push_back("celldata");
-  // tar_remap_fields.push_back("celldata_tar");
-  // d.set_remap_var_names(src_remap_fields, tar_remap_fields);
-
-  // d.set_interpolation_order(1);
-
-  // d.run();
-
-  // for (auto c: inputMesh.cells()) {
-  //   auto vol = c->area();
-  //   //    auto coords = c->coordinates();
-  //   //    std::cout << "area: " << vol << " " << coords << std::endl;
-  //   std::cout << "area: " << vol << std::endl;
-  // }
-
-  // auto cell5 = inputMesh.cells()[4];
-  // auto vert5 = inputMesh.vertices()[4];
-
-  // // std::cout << "cell 5 " << &cell5 << std::endl;
-  // // for (auto v : inputMesh.vertices(cell5)) {
-  // //     std::cout << "coords: " << v->coordinates() << std::endl;
-  // // }
-
-  // std::cout << "testing..." << std::endl;
-  // // inputMesh.cells(cell5);   Fails
-  // inputMesh.vertices(cell5);
-  // inputMesh.cells(vert5);
-  // inputMesh.wedges(cell5);
-  // std::cout << "done testing..." << std::endl;
-
-  // for (auto vi : inputMesh.cells(cell5))
-  //   std::cout << "ids: " << vi << std::endl;
-
-
-
-  // std::cout << "meshwrapper dim: " << inputMeshWrapper.space_dimension()
-  //           << std::endl;
-
-  // std::vector<std::pair<double,double>> vert_coords;
-  // inputMeshWrapper.cell_get_coordinates(4, &vert_coords);
-  // for (auto c : vert_coords)
-  //   std::cout << c.first << " " << c.second << std::endl;
-  // std::vector<int> vertIds;
-  // inputMeshWrapper.cell_get_nodes(4, &vertIds);
-  // for (auto vi : vertIds)
-  //   std::cout << "id: " << vi << std::endl;
-
 
   return 0;
 }
