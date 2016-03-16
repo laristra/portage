2fb5b567606fe5bc1120bb32d1d3fbe2f7752a82
Chris Malone <cmalone@lanl.gov>
2016-03-16 11:01:12 -0600

    swap to Portage::Point and cleanup; note this is a minimal implementation

--- src/wrappers/state/flecsi/flecsi_state_wrapper.h
+++ src/wrappers/state/flecsi/flecsi_state_wrapper.h
@@ -16,11 +16,7 @@
 
 using mesh_t = flecsi::burton_mesh_t;
 
-using cell_t = flecsi::mesh_t::cell_t;
-using vertex_t = flecsi::mesh_t::vertex_t;
-
 using real_t = flecsi::mesh_t::real_t;
-
 using const_string_t = flecsi::const_string_t;
 
 /*!
@@ -51,7 +47,7 @@ class Flecsi_State_Wrapper {
       : flecsi_mesh_(state.flecsi_mesh_) {}
 
   /*!
-    @brief Assignment operator (disabled) - don't know how to implement (RVG)
+    @brief Assignment operator (disabled)
    */
   Flecsi_State_Wrapper & operator=(Flecsi_State_Wrapper const &) = delete;
 
@@ -59,16 +55,6 @@ class Flecsi_State_Wrapper {
     @brief Empty destructor
    */
   ~Flecsi_State_Wrapper() {};
-
-  /* /\*! */
-  /*   @brief Initialize fields from mesh file */
-  /*  *\/ */
-  /* void init_from_mesh() { flecsi_mesh_.init_from_mesh(); } */
-
-  /* /\*! */
-  /*   @brief Export fields to mesh file */
-  /*  *\/ */
-  /* void export_to_mesh() {flecsi_mesh_.export_to_mesh(); } */
 
   /*!
     @brief Get pointer to scalar data
@@ -86,79 +72,11 @@ class Flecsi_State_Wrapper {
     // I added to the case where it can create from a char*
     const const_string_t flecsiname(var_name.c_str());
     auto dat = access_state(flecsi_mesh_,
-                            //                            flecsiname,
                             std::forward<const const_string_t>(flecsiname),
                             real_t);
-    // NOTE: I added this method
+    // NOTE: I added this method - should check with Ben and co. about it
     *data = dat.data();
   }
-
-  /* /\*! */
-  /*   @brief Get the entity type on which the given field is defined */
-  /*   @param[in] var_name The string name of the data field */
-  /*   @return The Entity_kind enum for the entity type on which the field is defined */
-  /*  *\/ */
-  /* Entity_kind get_entity(const std::string var_name) const { */
-
-  /*   std::shared_ptr<Flecsi::BaseStateVector> vector =  */
-  /*       *(flecsi_mesh_.find(var_name, Flecsi::ANY_KIND)); */
-  /*   if (vector != 0) return (Portage::Entity_kind) vector->on_what(); */
-
-  /*   return Portage::UNKNOWN_KIND; */
-
-  /* } */
-
-  /* /\*! */
-  /*   @brief Get the data type of the given field */
-  /*   @param[in] var_name The string name of the data field */
-  /*   @return A reference to the type_info struct for the field's data type */
-  /*  *\/  */
-  /* const std::type_info& get_type(const std::string var_name) const { */
-    
-  /*   std::shared_ptr<Flecsi::BaseStateVector> vector =  */
-  /*       *(flecsi_mesh_.find(var_name,Flecsi::ANY_KIND)); */
-  /*   if (vector != 0) return vector->get_type(); */
-  /*   return typeid(0); */
-
-  /* } */
-
-  /* /\*! */
-  /*   @brief Begin iterator on vector names */
-  /*   @return Begin iterator on vector of strings */
-  /*  *\/ */
-  /* std::vector<std::string>::iterator names_begin() const {  */
-  /*   return flecsi_mesh_.names_begin();  */
-  /* } */
-
-  /* /\*! */
-  /*   @brief End iterator on vector names */
-  /*   @return End iterator on vector of strings */
-  /*  *\/ */
-  /* std::vector<std::string>::iterator names_end() const {  */
-  /*   return flecsi_mesh_.names_end();  */
-  /* } */
-
-  /* /\*! */
-  /*   @brief Typedef for permutation iterator on vector of strings */
-  /*  *\/ */
-  /* typedef Flecsi::State::string_permutation string_permutation; */
-
-  /* /\*! */
-  /*   @brief Begin iterator on vector names of specific entity type */
-  /*   @param[in] on_what The desired entity type */
-  /*   @return Permutation iterator to start of string vector */
-  /*  *\/ */
-  /* string_permutation names_entity_begin(Entity_kind const on_what) const {  */
-  /*   return flecsi_mesh_.names_entity_begin((Flecsi::Entity_kind)on_what);  */
-  /* } */
-
-  /* /\*! */
-  /*   @brief End iterator on vector of names of specific entity type */
-  /*   @param[in] on_what The desired entity type */
-  /*  *\/ */
-  /* string_permutation names_entity_end(Entity_kind const on_what) const {  */
-  /*   return flecsi_mesh_.names_entity_end((Flecsi::Entity_kind)on_what);  */
-  /* } */
 
  private:
 
