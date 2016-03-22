2b73271f532006b409ed798b090a65e8cbfc6a84
Angela M. Herring <angelah@lanl.gov>
2016-03-22 08:46:22 -0600

    Added README.md parser.

---
+++ jenkins/parseREADME.py
@@ -0,0 +1,32 @@
+import subprocess, sys
+#This script parses the README file which describes how to build portage
+#It looks for code blocks enclosed in ``` and requires that the first 
+#line of a block be a followed by a hostname.  The script will then 
+#execute all commands specified after that point on the host machine
+#until the block is closed with a ```.
+#Args: Arg 1 is file to parse, Arg 2 is the workspace
+code = False
+scripts = {}
+for line in open(sys.argv[1]):    
+    if line.startswith("```"):
+        code = not code
+        currentScript = None
+    elif code:
+        if currentScript is None:
+            assert line.startswith("#")
+            currentScript = ["set -e\n pwd \n cd "+ sys.argv[2] + "\n rm -Rf build \n"]
+            scripts[line.lstrip("#").strip()] = currentScript
+        else: currentScript.append(line)
+
+retcode = 0
+for host in scripts:
+    if(host=='varan'):
+        print "HOST " + host
+        scripts[host] += 'cd ..;rm -Rf build;'
+        print scripts[host]
+        proc = subprocess.Popen(['ssh', '-T', "jenksrvngc@" + host], stdin = subprocess.PIPE)
+        print "opened process"
+        proc.communicate("".join(scripts[host]))
+        if proc.returncode:
+            retcode = proc.returncode
+sys.exit(retcode)
