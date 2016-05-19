9b6508a0dfa796e83f8ec32044e807d6f6415eca
Chris Malone <cmalone@lanl.gov>
2016-05-19 16:46:30 -0600

    flecsi-to-flecsi remap of linear func

--- app/main_flecsi.cc
+++ app/main_flecsi.cc
@@ -1,5 +1,4 @@
 #include <vector>
-// note that flecsi is missing this in some files - e.g. data.h
 #include <iostream>
 #include <cstdio>
 #include <memory>
@@ -12,6 +11,8 @@
 #include "portage/driver/driver.h"
 
 #include "flecsi/specializations/burton/burton.h"
+#include "flecsi/io/io.h"
+#include "flecsi/specializations/burton/burton_io_exodus.h"
 
 using mesh_t = flecsi::burton_mesh_t;
 
@@ -22,19 +23,18 @@ using real_t = flecsi::mesh_t::real_t;
   @brief A simple application that drives our remap routines on a FleCSI mesh.
 
   The program is used to showcase our capabilities with various types of remap
-  operations on a 2d FleCSI mesh with simple linear or quadratic data.For the
+  operations on a 2d FleCSI mesh with simple linear.  For the
   cases of remapping linear data with a second-order interpolator, the L2 norm
   output at the end should be identically zero.
  */
 
-/// @TODO add multiple examples
 
 void print_usage() {
-  std::printf("usage: portageapp_flecsi ncellsx ncellsy\n");
+  std::printf("usage: portage_flecsiapp ncellsx ncellsy [order=1]\n");  // [y];
+  //  std::printf("    if 'y' is specified, then dump the meshes to Exodus\n");
 }
 
 int main(int argc, char** argv) {
-
   if (argc <= 2) {
     print_usage();
     return 0;
@@ -43,68 +43,81 @@ int main(int argc, char** argv) {
   // number of CELLS in x and y for input mesh
   auto nx = atoi(argv[1]);
   auto ny = atoi(argv[2]);
+  auto order = (argc > 3) ? atoi(argv[3]) : 1;
+  // bool dump_output = (argc > 4) ?
+  //     (std::string(argv[4]) == "y") ? true : false
+  //     : false;
+
   // dimensions of meshes
   real_t xmin = 0.0, xmax = 1.0;
   real_t ymin = 0.0, ymax = 1.0;
 
-  std::printf("starting portageapp_flecsi...\n");
-
-  ///@TODO swap to a smart pointer
+  std::printf("starting portage_flecsiapp...\n");
 
   // Setup the meshes
   mesh_t inputMesh, targetMesh;
   Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx, ny, inputMesh);
-  // Example of how to get information directly from mesh object
-  // std::cout << "input mesh before target mesh creattion" << std::endl;
-  // for (auto v : inputMesh.vertices()) {
-  //   std::cout << "coords: " << v->coordinates() << std::endl;
-  // }
+  Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx+1, ny+1, targetMesh);
 
+  // Setup the wrappers
   Portage::Flecsi_Mesh_Wrapper inputMeshWrapper(inputMesh);
+  Portage::Flecsi_Mesh_Wrapper targetMeshWrapper(targetMesh);
 
-  // Compare data from wrapper to Mesh
-  auto someCellIndex = 5;
-  auto thisCell = inputMesh.cells()[someCellIndex];
+  // Register the state with FleCSI
+  // register_state is a macro from burton.h
+  // we keep the inputState accessor for filling of data
+  auto inputState = register_state(inputMesh, "celldata", cells, real_t,
+                                   flecsi::persistent);
+  register_state(targetMesh, "celldata", cells, real_t, flecsi::persistent);
 
-  // Examples of getting data from mesh object or wrapper
-  std::vector<int> nodeIDs;
-  inputMeshWrapper.cell_get_nodes(someCellIndex, &nodeIDs);
-  std::cout << "node IDs from wrapper: ";
-  std::copy(nodeIDs.begin(), nodeIDs.end(),
-            std::ostream_iterator<int>(std::cout, " "));
-  std::cout << std::endl;
-  std::cout << "node IDs from mesh:    ";
-  for (auto n : inputMesh.vertices(thisCell))
-    std::cout << n.global_id() << " ";
-  std::cout << std::endl;
+  // Fill with linear function
+  for (auto c : inputMesh.cells()) {
+    auto cen = c->centroid();
+    inputState[c] = cen[0] + cen[1];
+  }
 
-  std::vector<double> centroid;
-  inputMeshWrapper.cell_centroid(someCellIndex, &centroid);
-  std::cout << "centroid from wrapper: [ ";
-  std::copy(centroid.begin(), centroid.end(),
-            std::ostream_iterator<double>(std::cout, " "));
-  std::cout << " ]" << std::endl;
-  std::cout << "centroid from mesh:    [ ";
-  std::cout << thisCell->centroid() << std::endl;
+  // Build the state wrappers
+  Portage::Flecsi_State_Wrapper inputStateWrapper(inputMesh);
+  Portage::Flecsi_State_Wrapper targetStateWrapper(targetMesh);
 
-  //  std::vector<std::pair<double,double>> nodeCoords;
-  std::vector<Portage::Point2> nodeCoords;
-  inputMeshWrapper.cell_get_coordinates(someCellIndex, &nodeCoords);
-  std::cout << "node coordinates from wrapper:" << std::endl;
-  for (auto nc : nodeCoords)
-    std::cout << nc << std::endl;
-    // std::cout << "[ " << nc.first << " " << nc.second << " ]" << std::endl;
-  std::cout << "node coordinates from mesh:" << std::endl;
-  for (auto n : inputMesh.vertices(thisCell))
-    std::cout << n->coordinates() << std::endl;
+  // Setup the main driver for this mesh type
+  Portage::Driver<Portage::Flecsi_Mesh_Wrapper,
+                  Portage::Flecsi_State_Wrapper> d(Portage::CELL,
+                                                   inputMeshWrapper,
+                                                   inputStateWrapper,
+                                                   targetMeshWrapper,
+                                                   targetStateWrapper);
 
-  return 0;
+  // Declare which variables are remapped
+  std::vector<std::string> varnames(1, "celldata");
+  d.set_remap_var_names(varnames);
 
-  // This is broken because of the singleton issue in FleCSI state manager.
-  // Portage::make_mesh_cart2d(xmin, xmax, ymin, ymax, nx+1, ny+1, targetMesh);
-  // std::cout << "input mesh after target mesh creattion" << std::endl;
-  // for (auto v : inputMesh.vertices()) {
-  //   std::cout << "coords: " << v->coordinates() << std::endl;
+  // Declare interpolation order
+  d.set_interpolation_order(order);
+
+  // Do the remap
+  d.run();
+
+  // Get the new data and calculate the error
+  // use flecsi intrinsics - access_state is a macro in burton.h
+  auto outData = access_state(targetMesh, "celldata", real_t);
+
+  double toterr = 0.0;
+  for (auto c : targetMesh.cells()) {
+    auto cen = c->centroid();
+    double error = cen[0] + cen[1] - outData[c];
+    std::printf("Cell=% 4d Centroid = (% 5.3lf,% 5.3lf)", c.id(),
+                cen[0], cen[1]);
+    std::printf("  Value = % 10.6lf  Err = % lf\n",
+                outData[c], error);
+    toterr += error*error;
+  }
+  std::printf("\n\nL2 NORM OF ERROR = %lf\n\n", sqrt(toterr));
+
+  /// @TODO Dumping the targetMesh is still broken with FleCSI
+  // if (dump_output) {
+  //   flecsi::write_mesh("input.exo", inputMesh);
+  //   flecsi::write_mesh("output.exo", targetMesh);
   // }
 
   return 0;
