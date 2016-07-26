8128d9382b60c2c993fef18810c5448f07a8180c
Charles Ferenbaugh <cferenba@lanl.gov>
2016-07-26 12:27:38 -0600

    Move calculation of target wedge faces outside of loop

--- src/intersect/intersect_r3d.h
+++ src/intersect/intersect_r3d.h
@@ -43,6 +43,24 @@ public:
 
     std::vector<double> moments(4, 0);
 
+    std::vector<std::array<r3d_plane, 4>> target_faces_all;
+    target_faces_all.reserve(target_coords.size());
+    for (const auto &target_wedge : target_coords) {
+      std::array<r3d_plane, 4> faces;
+      r3d_rvec3 verts2[4];
+      for (int i=0; i<4; i++)
+        for (int j=0; j<3; j++)
+          verts2[i].xyz[j] = target_wedge[i][j];
+      // Only do this check in Debug mode. This test is important,
+      // otherwise R3D returns invalid results.
+#ifdef DEBUG
+      if (r3d_orient(verts2) < 0)
+        throw std::runtime_error("target_wedge has negative volume");
+#endif
+      r3d_tet_faces_from_verts(&faces[0], verts2);
+      target_faces_all.emplace_back(faces);
+    }
+
     for (const auto &source_wedge : source_coords) {
       r3d_rvec3 verts1[4];
       for (int i=0; i<4; i++)
@@ -55,7 +73,7 @@ public:
         throw std::runtime_error("source_wedge has negative volume");
 #endif
 
-      for (const auto &target_wedge : target_coords) {
+      for (auto &faces : target_faces_all) {
         r3d_poly poly;
         r3d_init_tet(&poly, verts1);
         // Only do this check in Debug mode:
@@ -64,22 +82,8 @@ public:
           throw std::runtime_error("source_wedge: invalid poly");
 #endif
 
-        r3d_plane faces[4];
-        r3d_rvec3 verts2[4];
-        for (int i=0; i<4; i++)
-          for (int j=0; j<3; j++)
-            verts2[i].xyz[j] = target_wedge[i][j];
-        // Only do this check in Debug mode. This test is important,
-        // otherwise R3D returns invalid results.
-#ifdef DEBUG
-        if (r3d_orient(verts2) < 0)
-          throw std::runtime_error("target_wedge has negative volume");
-#endif
-
-        r3d_tet_faces_from_verts(faces, verts2);
-
         // clip the first tet against the faces of the second
-        r3d_clip(&poly, faces, 4);
+        r3d_clip(&poly, &faces[0], 4);
 
         // find the moments (up to quadratic order) of the clipped poly
         const int POLY_ORDER=1;
