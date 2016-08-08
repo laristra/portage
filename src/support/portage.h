558d803bcfad20cab24be018ebe5e484aeba64da
Rao Garimella <rao@lanl.gov>
2016-08-08 15:38:54 -0600

    Revert (WILL DO SEPARATELY) "inline the functions in portage.h so that they are not multiply defined. Fixes LNK-361"
    This reverts commit 06d019dffed4203f7a0a3912f8fbee8559f6a6c8.

--- src/support/portage.h
+++ src/support/portage.h
@@ -96,21 +96,21 @@ template<typename T>
     using pointer = thrust::device_ptr<T>;
 
 typedef thrust::counting_iterator<int> counting_iterator;
-inline counting_iterator make_counting_iterator(int const i) {
+counting_iterator make_counting_iterator(int const i) {
   return thrust::make_counting_iterator(i);
 }
 
 template<typename InputIterator, typename OutputIterator,
     typename UnaryFunction>
-inline OutputIterator transform(InputIterator first, InputIterator last,
-                                OutputIterator result, UnaryFunction op) {
-  return thrust::transform(first, last, result, op);
-}
+    OutputIterator transform(InputIterator first, InputIterator last,
+                             OutputIterator result, UnaryFunction op) {
+      return thrust::transform(first, last, result, op);
+    }
 template<typename InputIterator1, typename InputIterator2,
     typename OutputIterator, typename BinaryFunction>
-inline OutputIterator transform(InputIterator1 first1, InputIterator1 last1,
-                                InputIterator2 first2, OutputIterator result,
-                                BinaryFunction op) {
+    OutputIterator transform(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, OutputIterator result,
+                             BinaryFunction op) {
   return thrust::transform(first1, last1, first2, result, op);
 }
 
@@ -123,21 +123,21 @@ template<typename T>
     using pointer = T*;
 
 typedef boost::counting_iterator<int> counting_iterator;
-inline counting_iterator make_counting_iterator(int const i) {
+counting_iterator make_counting_iterator(int const i) {
   return boost::make_counting_iterator<int>(i);
 }
 
 template<typename InputIterator, typename OutputIterator,
     typename UnaryFunction>
-inline OutputIterator transform(InputIterator first, InputIterator last,
-                                OutputIterator result, UnaryFunction op) {
+    OutputIterator transform(InputIterator first, InputIterator last,
+                             OutputIterator result, UnaryFunction op) {
   return std::transform(first, last, result, op);
 }
 template<typename InputIterator1, typename InputIterator2,
     typename OutputIterator, typename BinaryFunction>
-inline OutputIterator transform(InputIterator1 first1, InputIterator1 last1,
-                                InputIterator2 first2, OutputIterator result,
-                                BinaryFunction op) {
+    OutputIterator transform(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, OutputIterator result,
+                             BinaryFunction op) {
   return std::transform(first1, last1, first2, result, op);
 }
 
