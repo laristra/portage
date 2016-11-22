4a8aec4f74a939885a95f77ee5bc9b583fc0bbf7
Ondřej Čertík <ondrej.certik@gmail.com>
2016-11-21 18:34:07 -0700

    Add a portageapp_test3 test

---
+++ test/portageapp3/portageapp_test3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+# test a simple 2d remap, save a field
+mpirun -np 4 $APPDIR/portageapp 1 4 8 y
+
+# Compare the values for the field
+$APPDIR/apptest_cmp field_gold.txt field.txt 1e-12
