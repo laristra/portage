251dd388333751a7475851d9fc9fdf1263c771f0
Ondřej Čertík <ondrej.certik@gmail.com>
2016-11-30 08:02:38 -0700

    Relax the comparator eps tolerance

--- test/shotshellapp/shotshellapp_test1.sh
+++ test/shotshellapp/shotshellapp_test1.sh
@@ -11,4 +11,4 @@ DATA_DIR=$APPDIR/../test/wrappers
 mpirun -np 1 ${APPDIR}/shotshellapp 1 ${DATA_DIR}/shotshell.exo ${DATA_DIR}/shotshell-v.exo y
 
 # Compare the values for the field
-$APPDIR/apptest_cmp field_gold1.txt field1.txt 1e-12
+$APPDIR/apptest_cmp field_gold1.txt field1.txt 1e-9
