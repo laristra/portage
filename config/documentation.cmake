518f564100e10557f8995141337e4c1c95a8f364
Chris Malone <cmalone@lanl.gov>
2016-12-12 17:16:51 -0700

    start on docs changes

--- config/documentation.cmake
+++ config/documentation.cmake
@@ -9,27 +9,39 @@
 #------------------------------------------------------------------------------#
 
 configure_file(${CMAKE_SOURCE_DIR}/doc/ugheader.tex.in
-    ${CMAKE_BINARY_DIR}/doc/ugheader.tex)
+  ${CMAKE_BINARY_DIR}/doc/ugheader.tex)
 
 #------------------------------------------------------------------------------#
 # Pandoc options for user guide
 #------------------------------------------------------------------------------#
 
 set(ug_pandoc_options
-    "--toc"
-    "--include-in-header=${CMAKE_SOURCE_DIR}/cinch/tex/addtolength.tex"
-    "--include-in-header=${CMAKE_BINARY_DIR}/doc/ugheader.tex"
-    "--include-in-header=${CMAKE_SOURCE_DIR}/doc/ugtitle.tex"
-    "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/maketitle.tex"
-    "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/firstpageempty.tex"
-    "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/titlebreak.tex"
-)
+  "--toc"
+  "--include-in-header=${CMAKE_SOURCE_DIR}/cinch/tex/addtolength.tex"
+  "--include-in-header=${CMAKE_BINARY_DIR}/doc/ugheader.tex"
+  "--include-in-header=${CMAKE_SOURCE_DIR}/doc/ugtitle.tex"
+  "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/maketitle.tex"
+  "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/firstpageempty.tex"
+  "--include-before-body=${CMAKE_SOURCE_DIR}/cinch/tex/titlebreak.tex"
+  )
 
 #------------------------------------------------------------------------------#
 # Add user guide target
 #------------------------------------------------------------------------------#
 
 cinch_add_doc(user-guide portage_ug.py src
-    portage-user-guide-${${PROJECT_NAME}_VERSION}.pdf
-    PANDOC_OPTIONS ${ug_pandoc_options} IMAGE_GLOB "*.pdf")
+  portage-user-guide-${${PROJECT_NAME}_VERSION}.pdf
+  PANDOC_OPTIONS ${ug_pandoc_options} IMAGE_GLOB "*.pdf")
 
+
+
+#----------------------------------------------------------------------------#
+# Doxygen stuff
+#----------------------------------------------------------------------------#
+file(COPY
+  ${CMAKE_CURRENT_SOURCE_DIR}/doc/header.html
+  ${CMAKE_CURRENT_SOURCE_DIR}/doc/footer.html
+  ${CMAKE_CURRENT_SOURCE_DIR}/doc/stylesheet.css
+  DESTINATION
+  ${CMAKE_BINARY_DIR}/doc/doxygen/html
+  )
