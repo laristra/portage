0ea8dc2390945186ff77fbe8084893474cf36429
Gary Dilts <gad@lanl.gov>
2016-12-14 09:31:36 -0700

    Rewrite of Flat_State_Wrapper to accomodate a new add_data method. This uncovered a number of shortcomings in the existing implementation which were corrected. Some potential bugs in existing code were eliminated as well. Tests for normal operation and failure modes have been written.

--- src/wrappers/state/flat/flat_state_wrapper.h
+++ src/wrappers/state/flat/flat_state_wrapper.h
@@ -8,6 +8,10 @@
 
 #include <map>
 #include <memory>
+#include <vector>
+#include <stdexcept>
+#include <utility>
+#include <algorithm>
 
 #include "portage/support/portage.h"
 #include "portage/support/Point.h"
@@ -22,8 +26,6 @@ namespace Portage {
 /*!
   @class Flat_State_Wrapper "flat_state_wrapper.h"
   @brief Stores state data in a flat representation
-         
-         Currently all fields must be of the same type
 */
 template <class T=double>
 class Flat_State_Wrapper {
@@ -34,52 +36,203 @@ class Flat_State_Wrapper {
    */
   Flat_State_Wrapper() { };
 
+  /*!
+    @brief Assignment operator (disabled) - don't know how to implement (RVG)
+   */
+  Flat_State_Wrapper & operator=(Flat_State_Wrapper const &) = delete;
+
+  /*!
+    @brief Empty destructor
+   */
+  ~Flat_State_Wrapper() {};
+
+  using pair_t = std::pair<std::string, Entity_kind>;
+
+  void initialize(std::vector<std::string> names, std::vector<Entity_kind> entities,
+		  	      std::vector<std::shared_ptr<std::vector<T>>> data)
+  {
+    if (not (names.size() == entities.size() and names.size() == data.size() and data.size() == entities.size())) {
+        throw std::runtime_error("argument sizes do not agree");
+    }
+
+    size_t index;
+    for (size_t i=0; i<names.size(); i++) {
+    	add_data(entities[i], names[i], data[i]);
+    }
+  }
+
+  /*!
+   * @brief Initialize the state wrapper with another state wrapper and a list of names
+   * @param[in] input another state wrapper, which need not be for Flat_State.
+   * @param[in] var_names a list of state names to initialize
+   *
+   * Entities and sizes associated with the given name will be obtained from the input state wrapper.
+   *
+   * A name can be re-used with a different entity, but a name-entity combination
+   * must be unique.
+   *
+   * A name-entity combination must not introduce a new size for that entity if
+   * it has previously been encountered.
+   */
   template <class State_Wrapper>
   void initialize(State_Wrapper &input, std::vector<std::string> var_names) 
   {
-    for (unsigned int i=0; i<var_names.size(); i++)
+    for (size_t i=0; i<var_names.size(); i++)
     {
+      // get entity
       Entity_kind entity = input.get_entity(var_names[i]);
-      int dataSize = input.get_data_size(entity, var_names[i]);
+      auto newpair = pair_t(var_names[i], entity);
+
+      // check for duplicate name-entity combination, error if already in
+      auto isin = name_map_.find(newpair);
+      if (isin == name_map_.end()) {
+        throw std::runtime_error(std::string("variable ")+var_names[i]+" is already in this database for the requested entity");
+      }
+
+      // store entity type, possibly ambiguous
+      entity_map_[var_names[i]] = entity;
+
+      // store size of entity_type, error if changed from what we've seen before
+      size_t dataSize = input.get_data_size(entity, var_names[i]);
+      if (entity_size_map_.find(entity) != entity_size_map_.end()) {  // we have seen it
+    	size_t oldSize = entity_size_map_[entity];
+    	if (oldSize != dataSize) {
+    	  throw std::runtime_error(std::string("variable ")+var_names[i]+" has an invalid entity size");
+    	}
+      } else { // we haven't seen it
+	entity_size_map_[entity] = dataSize;
+      }
+
+      // store data for state
       T* data;
       input.get_data(entity, var_names[i], &data);
       std::shared_ptr<std::vector<T>> field = std::make_shared<std::vector<T>>();
       field->resize(dataSize);
       std::copy(data, data+dataSize, field->begin());
       state_.push_back(field);
-      name_map_[var_names[i]] = state_.size() - 1;
+      name_map_[newpair] = state_.size() - 1;
     }
   }
-  
-  /*!
-    @brief Assignment operator (disabled) - don't know how to implement (RVG)
-   */
-  Flat_State_Wrapper & operator=(Flat_State_Wrapper const &) = delete;
-  
-  /*!
-    @brief Empty destructor
-   */
-  ~Flat_State_Wrapper() {};
   
    
   /*!
     @brief Get pointer to scalar data
-    @param[in] on_what The entity type on which to get the data
+    @param[in] on_what The entity type on which the data is defined
     @param[in] var_name The string name of the data field
-    @param[in,out] data A pointer to an array of data
+    @param[in,out] data A pointer to an array of data. Null on output if data does not exist.
+    *
+    * Data is associated with the name-entity combination. Both values must be valid.
    */
-  template <class D>
-  void get_data(const Entity_kind on_what, const std::string var_name, D** const data) const {
-  
-    std::map<std::string, int>::const_iterator iter = name_map_.find(var_name);
-    if (iter != name_map_.end())
-      (*data) = (D*)(&((*(state_[iter->second]))[0]));
+  void get_data(const Entity_kind on_what, const std::string var_name, T** const data) const {
+    pair_t pr(var_name, on_what);
+    auto iter = name_map_.find(pr);
+    if (iter != name_map_.end()) {
+      (*data) = (T*)(&((*(state_[iter->second]))[0]));
+    } else {
+      (*data) = nullptr;
+    }
   }
+
+  /*!
+   @brief Add a scalar data field
+   @param[in] on_what The entity type on which the data is defined
+   @param[in] var_name The name of the data field
+   @param[in] value vector of data to add
+   *
+   * If data for name-entity combination already exists, then replace data.
+   * Size of data must match previous size recorded for requested entity.
+   * If entity has not been seen before, make that entity's size equal to that of data.
+   */
+  void add_data(const Entity_kind on_what, const std::string var_name, std::shared_ptr<std::vector<T>> data) {
+    // if we have seen this entity before - check size match, else store this size
+    auto sziter = entity_size_map_.find(on_what);
+    if (sziter != entity_size_map_.end()) {
+	if (sziter->second != data->size()) {
+	    throw std::runtime_error(std::string("variable ")+var_name+" has incompatible size on add");
+	}
+    } else {
+    	entity_size_map_[on_what] = data->size();
+    }
+
+    // store data and update internal book-keeping
+    pair_t pair(var_name, on_what);
+    auto iter = name_map_.find(pair);
+    if (iter == name_map_.end()) {  // have not seen this entity-name combo before, add data
+	state_.push_back(data);
+	name_map_[pair] = state_.size() - 1;
+	entity_map_[var_name] = on_what;
+	entity_size_map_[on_what] = data->size();
+    } else { // have seen the entity-name combo already, replace data. already checked size.
+    	std::copy(data->begin(), data->end(), state_[iter->second]->begin());
+    }
+  }
+
+  /*!
+   @brief Add a scalar data field with uniform values
+   @param[in] on_what The entity type on which the data is defined
+   @param[in] var_name The name of the data field
+   @param[in] value initialize with this value
+   *
+   * If data for name-entity combination already exists, then replace data with value.
+   */
+  void add_data(const Entity_kind on_what, const std::string var_name, T value) {
+    pair_t pair(var_name, on_what);
+    auto iter = name_map_.find(pair);
+    if (iter == name_map_.end()) { // have not seen this entity-name combo before
+	auto sziter = entity_size_map_.find(on_what);
+	// haven't seen this entity before - no size info - bail
+	if (sziter == entity_size_map_.end()) {
+	  throw std::runtime_error(std::string("variable ")+var_name+" has no size information available on add");
+	}
+	auto newptr = std::make_shared<std::vector<T>>(sziter->second, value);
+	state_.push_back(newptr);
+	name_map_[pair] = state_.size() - 1;
+	entity_map_[var_name] = on_what;
+    } else { // have seen this entity-name combo before
+	for (size_t i=0; i<sziter->second; i++) {
+	(*state_[iter->second])[i] = value;
+    }
+  }
+
+  /*!
+    @brief Get the entity type on which the given field is defined
+    @param[in] var_name The name of the data field
+    @return The Entity_kind enum for the entity type on which the field is defined
+   *
+   * If the name has previously been associated with more than one entity, then the
+   * entity that was most recently associated will be returned. To avoid this
+   * ambiguity, please provide entity hints in the field name for yourself.
+   *
+   * This function is provided to make the class compatible with other state wrappers.
+   */
+  Entity_kind get_entity(const std::string var_name) const {
+    return entity_map_[var_name];
+  }
+
+  /*!
+    @brief Get size for entity
+  */
+  size_t get_entity_size(Entity_kind ent) {
+    return entity_size_map_[ent];
+  }
+
+  /*!
+   * @brief Get index for entity and name
+   */
+  size_t get_vector_index(Entity_kind ent, std::string name) {
+    pair_t pair(name, ent);
+    return name_map_[pair];
+  }
+
+  /*!
+    @brief Get the number of data vectors
+  */
+  size_t get_num_vectors() { return state_.size(); }
 
   /*!
     @brief Get the data vector
   */
-  std::shared_ptr<std::vector<T>> get_vector(int index) 
+  std::shared_ptr<std::vector<T>> get_vector(size_t index)
   {
     return state_[index]; 
   }
@@ -87,15 +240,10 @@ class Flat_State_Wrapper {
   /*!
     @brief Get gradients
   */
-  std::shared_ptr<std::vector<Portage::Point3>> get_gradients(int index)
+  std::shared_ptr<std::vector<Portage::Point3>> get_gradients(size_t index)
   {
     return gradients_[index];
   }
-
-  /*! 
-    @brief Get the number of data vectors
-  */
-  int get_num_vectors() { return state_.size(); }
 
   /*!
     @brief Add a gradient field
@@ -107,9 +255,9 @@ class Flat_State_Wrapper {
   }
 
   /*! 
-    @brief Get field dimension
+    @brief Get field stride
   */
-  int get_field_dim(int index)
+  size_t get_field_stride(size_t index)
   {
     return 1;
   }
@@ -117,11 +265,13 @@ class Flat_State_Wrapper {
   /*!
     @brief Get the number of gradient vectors
   */
-  int get_num_gradients() { return gradients_.size(); }
+  size_t get_num_gradients() { return gradients_.size(); }
 
 private:
   std::vector<std::shared_ptr<std::vector<T>>> state_;
-  std::map<std::string, int> name_map_;
+  std::map<pair_t, size_t> name_map_;
+  std::map<std::string, Entity_kind> entity_map_;
+  std::map<Entity_kind, size_t> entity_size_map_;
   std::vector<std::shared_ptr<std::vector<Portage::Point3>>> gradients_;
 
 
--- src/wrappers/state/flat/test/test_flat_state_wrapper.cc
+++ src/wrappers/state/flat/test/test_flat_state_wrapper.cc
@@ -4,9 +4,15 @@
  *---------------------------------------------------------------------------~*/
 
 #include "portage/wrappers/state/jali/jali_state_wrapper.h"
+#include "portage/wrappers/mesh/jali/jali_mesh_wrapper.h"
+
 #include "portage/wrappers/state/flat/flat_state_wrapper.h"
 
+#include "portage/support/portage.h"
+
 #include <iostream>
+#include <exception>
+#include <string>
 
 #include "gtest/gtest.h"
 #include "mpi.h"
@@ -14,8 +20,173 @@
 #include "Mesh.hh"
 #include "MeshFactory.hh"
 
-#include "portage/wrappers/mesh/jali/jali_mesh_wrapper.h"
+TEST(Flat_State_Wrapper, VectorInit) {
+  std::vector<double> vertx={0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
+  std::vector<double> verty={0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  std::vector<double> nfield1(16,0.), nfield2(16,0.), cfield1(9,0.), cfield2(9,0.);
+  std::vector<std::string> names = {"nf1", "cf1", "cf2", "nf2"};
+  for (size_t i=0; i<16; i++) {
+      nfield1[i] = vertx[i]*vertx[i] + verty[i]*verty[i];
+      nfield2[i] = vertx[i]+verty[i];
+  }
+  for (size_t i=0; i<3; i++) for (size_t j=0; j<3; j++) {
+      double xc = .25*(vertx[i+j*4] + vertx[i+1+j*4] + vertx[i+j*4] + vertx[i+1+j*4]);
+      double yc = .5*(verty[i+(j+1)*4] + verty[i+j*4]);
+      cfield1[i+j*3] = xc*xc + yc*yc;
+      cfield2[i+j*3] = xc + yc;
+  }
 
+  std::shared_ptr<std::vector<double>> nf1(&nfield1),nf2(&nfield2),cf1(&cfield1),cf2(&cfield2);
+  std::vector<std::shared_ptr<std::vector<double>>> data ={nf1,cf1,cf2,nf2};
+
+  using namespace Portage;
+
+  std::vector<Entity_kind> entities = {NODE, CELL, CELL, NODE};
+
+  Flat_State_Wrapper<double> flatwrap;
+  flatwrap.initialize(names, entities, data);
+}
+
+TEST(Flat_State_Wrapper, VectorInitFail1) {
+  std::vector<double> vertx={0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
+  std::vector<double> verty={0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  std::vector<double> nfield1(16,0.), nfield2(16,0.), cfield1(9,0.), cfield2(9,0.), badfield(23,0.);
+  std::vector<std::string> names = {"nf1", "cf1", "cf2", "nf2", "bad"};
+  for (size_t i=0; i<16; i++) {
+      nfield1[i] = vertx[i]*vertx[i] + verty[i]*verty[i];
+      nfield2[i] = vertx[i]+verty[i];
+  }
+  for (size_t i=0; i<3; i++) for (size_t j=0; j<3; j++) {
+      double xc = .25*(vertx[i+j*4] + vertx[i+1+j*4] + vertx[i+j*4] + vertx[i+1+j*4]);
+      double yc = .5*(verty[i+(j+1)*4] + verty[i+j*4]);
+      cfield1[i+j*3] = xc*xc + yc*yc;
+      cfield2[i+j*3] = xc + yc;
+  }
+
+  std::shared_ptr<std::vector<double>> nf1(&nfield1),nf2(&nfield2),cf1(&cfield1),cf2(&cfield2),bf(&badfield);
+  std::vector<std::shared_ptr<std::vector<double>>> data ={nf1,cf1,cf2,nf2,bf};
+
+  using namespace Portage;
+
+  std::vector<Entity_kind> entities = {NODE, CELL, CELL, NODE, NODE};
+
+  Flat_State_Wrapper<double> flatwrap;
+  try {
+      flatwrap.initialize(names, entities, data);
+  } catch (std::exception& err) {
+      std::string var_name = "bad";
+      std::string msg=std::string("variable ")+var_name+" has incompatible size on add";
+      ASSERT_STREQ(err.what(), msg.c_str());
+  }
+}
+
+TEST(Flat_State_Wrapper, VectorInitFail2) {
+  std::vector<double> vertx={0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
+  std::vector<double> verty={0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  std::vector<double> nfield1(16,0.), nfield2(16,0.), cfield1(9,0.), cfield2(9,0.);
+  std::vector<std::string> names = {"nf1", "cf1", "cf2", "nf2"},
+                           badnames = {"nf1", "cf1", "cf2"};
+  for (size_t i=0; i<16; i++) {
+      nfield1[i] = vertx[i]*vertx[i] + verty[i]*verty[i];
+      nfield2[i] = vertx[i]+verty[i];
+  }
+  for (size_t i=0; i<3; i++) for (size_t j=0; j<3; j++) {
+      double xc = .25*(vertx[i+j*4] + vertx[i+1+j*4] + vertx[i+j*4] + vertx[i+1+j*4]);
+      double yc = .5*(verty[i+(j+1)*4] + verty[i+j*4]);
+      cfield1[i+j*3] = xc*xc + yc*yc;
+      cfield2[i+j*3] = xc + yc;
+  }
+
+  std::shared_ptr<std::vector<double>> nf1(&nfield1),nf2(&nfield2),cf1(&cfield1),cf2(&cfield2);
+  std::vector<std::shared_ptr<std::vector<double>>> data ={nf1,cf1,cf2,nf2},
+      baddata= {nf1,cf1,cf2};
+
+  using namespace Portage;
+
+  std::vector<Entity_kind> entities = {NODE, CELL, CELL, NODE}, badentities={NODE, CELL, CELL};
+
+  try {
+      Flat_State_Wrapper<double> flatwrap;
+      flatwrap.initialize(badnames, entities, data);
+  } catch (std::exception& err) {
+      std::string msg="argument sizes do not agree";
+      ASSERT_STREQ(err.what(), msg.c_str());
+  }
+
+  try {
+      Flat_State_Wrapper<double> flatwrap;
+      flatwrap.initialize(names, badentities, data);
+  } catch (std::exception& err) {
+      std::string msg="argument sizes do not agree";
+      ASSERT_STREQ(err.what(), msg.c_str());
+  }
+
+  try {
+      Flat_State_Wrapper<double> flatwrap;
+      flatwrap.initialize(names, entities, baddata);
+  } catch (std::exception& err) {
+      std::string msg="argument sizes do not agree";
+      ASSERT_STREQ(err.what(), msg.c_str());
+  }
+}
+
+TEST(Flat_State_Wrapper, AddData) {
+  std::vector<double> vertx={0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3};
+  std::vector<double> verty={0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  std::vector<double> nfield1(16,0.), nfield2(16,0.), nfield3(16,0.), cfield1(9,0.), cfield2(9,0.);
+  for (size_t i=0; i<16; i++) {
+      nfield1[i] = vertx[i]*vertx[i] + verty[i]*verty[i];
+      nfield2[i] = vertx[i]+verty[i];
+      nfield3[i] = vertx[i]*verty[i];
+  }
+  for (size_t i=0; i<3; i++) for (size_t j=0; j<3; j++) {
+      double xc = .25*(vertx[i+j*4] + vertx[i+1+j*4] + vertx[i+j*4] + vertx[i+1+j*4]);
+      double yc = .5*(verty[i+(j+1)*4] + verty[i+j*4]);
+      cfield1[i+j*3] = xc*xc + yc*yc;
+      cfield2[i+j*3] = xc + yc;
+  }
+
+  std::shared_ptr<std::vector<double>> nf1(&nfield1),nf2(&nfield2),nf3(&nfield3),cf1(&cfield1),cf2(&cfield2);
+
+  using namespace Portage;
+
+  {
+    Flat_State_Wrapper<double> flatwrap;
+    flatwrap.add_data(NODE, "nf1", nf1);
+    flatwrap.add_data(CELL, "cf1", cf1);
+    flatwrap.add_data(CELL, "cf2", cf2);
+    flatwrap.add_data(NODE, "nf2", nf2);
+
+    size_t sn=flatwrap.get_entity_size(NODE), sc=flatwrap.get_entity_size(CELL);
+    ASSERT_EQ(16, sn);
+    ASSERT_EQ(9, sc);
+
+    size_t index=flatwrap.get_vector_index(NODE, "nf2");
+    ASSERT_EQ(index, 3);
+    index=flatwrap.get_vector_index(CELL, "cf2");
+    ASSERT_EQ(index, 2);
+
+    double** nf2check;
+    flatwrap.get_data(NODE, "nf2", nf2check);
+    for (size_t i=0; i<16; i++) {
+	ASSERT_EQ((*nf2check)[i], nfield2[i]);
+    }
+
+    flatwrap.add_data(NODE, "nf2", nf3);
+    flatwrap.get_data(NODE, "nf2", nf2check);
+    for (size_t i=0; i<16; i++) {
+	ASSERT_EQ((*nf2check)[i], nfield3[i]);
+    }
+
+    try {
+	flatwrap.add_data(EDGE, "ef1", 1.7);
+    } catch (std::exception err) {
+	std::string var_name = "ef1";
+	std::string msg = std::string("variable ")+var_name+" has no size information available on add";
+	ASSERT_STREQ(err.what(), msg.c_str());
+    }
+  }
+}
 
 TEST(Flat_State_Wrapper, DataTypes2D) {
 
