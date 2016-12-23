6083c577c28e3b3734e91e43125325c26042f29b
Chris Malone <cmalone@lanl.gov>
2016-12-23 08:54:37 -0700

    add LICENSE to files

--- test/portageapp/portageapp_2d_node_f1_r2.sh
+++ test/portageapp/portageapp_2d_node_f1_r2.sh
@@ -52,24 +52,24 @@ set -x
 $APPDIR/portageapp_jali \
 --dim=2 --nsourcecells=5 --ntargetcells=7 \
 --conformal=y \
---entity_kind=cell --field_order=1 \
+--entity_kind=node --field_order=1 \
 --remap_order=2 \
 --output_results=y
 
 # Compare the values for the field
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2.txt field_2d_cell_f1_r2.txt 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f1_r2.txt field_2d_node_f1_r2.txt 1e-12
 
 # PARALLEL RUN
 
 mpirun -np 4 $APPDIR/portageapp_jali \
 --dim=2 --nsourcecells=5 --ntargetcells=7 \
 --conformal=n \
---entity_kind=cell --field_order=1 \
+--entity_kind=node --field_order=1 \
 --remap_order=2 \
 --output_results=y
 
 # Compare the values for the field
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.0 field_2d_cell_f1_r2_nc.txt.0 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.1 field_2d_cell_f1_r2_nc.txt.1 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.2 field_2d_cell_f1_r2_nc.txt.2 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.3 field_2d_cell_f1_r2_nc.txt.3 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f1_r2_nc.txt.0 field_2d_node_f1_r2_nc.txt.0 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f1_r2_nc.txt.1 field_2d_node_f1_r2_nc.txt.1 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f1_r2_nc.txt.2 field_2d_node_f1_r2_nc.txt.2 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f1_r2_nc.txt.3 field_2d_node_f1_r2_nc.txt.3 1e-12
--- test/portageapp/portageapp_3d_node_f1_r1.sh
+++ test/portageapp/portageapp_3d_node_f1_r1.sh
@@ -45,31 +45,31 @@ set -e
 # Echo each command
 set -x
 
-# 2D, 2nd order accurate remap of linear cell-centered function
+# 3D, 1st order accurate remap of linear node-centered function
 
 # SERIAL RUN
 
 $APPDIR/portageapp_jali \
---dim=2 --nsourcecells=5 --ntargetcells=7 \
+--dim=3 --nsourcecells=5 --ntargetcells=7 \
 --conformal=y \
---entity_kind=cell --field_order=1 \
---remap_order=2 \
+--entity_kind=node --field_order=1 \
+--remap_order=1 \
 --output_results=y
 
 # Compare the values for the field
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2.txt field_2d_cell_f1_r2.txt 1e-12
+$APPDIR/apptest_cmp GOLD-field_3d_node_f1_r1.txt field_3d_node_f1_r1.txt 1e-12
 
 # PARALLEL RUN
 
 mpirun -np 4 $APPDIR/portageapp_jali \
---dim=2 --nsourcecells=5 --ntargetcells=7 \
+--dim=3 --nsourcecells=5 --ntargetcells=7 \
 --conformal=n \
---entity_kind=cell --field_order=1 \
---remap_order=2 \
+--entity_kind=node --field_order=1 \
+--remap_order=1 \
 --output_results=y
 
 # Compare the values for the field
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.0 field_2d_cell_f1_r2_nc.txt.0 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.1 field_2d_cell_f1_r2_nc.txt.1 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.2 field_2d_cell_f1_r2_nc.txt.2 1e-12
-$APPDIR/apptest_cmp GOLD-field_2d_cell_f1_r2_nc.txt.3 field_2d_cell_f1_r2_nc.txt.3 1e-12
+$APPDIR/apptest_cmp GOLD-field_3d_node_f1_r1_nc.txt.0 field_3d_node_f1_r1_nc.txt.0 1e-12
+$APPDIR/apptest_cmp GOLD-field_3d_node_f1_r1_nc.txt.1 field_3d_node_f1_r1_nc.txt.1 1e-12
+$APPDIR/apptest_cmp GOLD-field_3d_node_f1_r1_nc.txt.2 field_3d_node_f1_r1_nc.txt.2 1e-12
+$APPDIR/apptest_cmp GOLD-field_3d_node_f1_r1_nc.txt.3 field_3d_node_f1_r1_nc.txt.3 1e-12
