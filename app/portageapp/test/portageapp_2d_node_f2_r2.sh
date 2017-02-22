c37a2a9fa87da0fbe693ad973b538e465a58817b
Charles Ferenbaugh <cferenba@lanl.gov>
2017-02-22 11:46:47 -0700

    Enable distributed case for rest of node-based tests

--- app/portageapp/test/portageapp_2d_node_f2_r2.sh
+++ app/portageapp/test/portageapp_2d_node_f2_r2.sh
@@ -68,8 +68,8 @@ mpirun -np 4 $APPDIR/portageapp_jali \
 --remap_order=2 \
 --output_results=y
 
-# # Compare the values for the field
-# $APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.0 field_2d_node_f2_r2_nc.txt.0 1e-12
-# $APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.1 field_2d_node_f2_r2_nc.txt.1 1e-12
-# $APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.2 field_2d_node_f2_r2_nc.txt.2 1e-12
-# $APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.3 field_2d_node_f2_r2_nc.txt.3 1e-12
+# Compare the values for the field
+$APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.0 field_2d_node_f2_r2_nc.txt.0 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.1 field_2d_node_f2_r2_nc.txt.1 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.2 field_2d_node_f2_r2_nc.txt.2 1e-12
+$APPDIR/apptest_cmp GOLD-field_2d_node_f2_r2_nc.txt.3 field_2d_node_f2_r2_nc.txt.3 1e-12
