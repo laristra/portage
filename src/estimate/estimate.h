c695763fb4933b697c8e23a5f4b4f1d784769f78
Gary Dilts <gad@lanl.gov>
2017-06-02 17:23:37 -0600

    final estimate class. tests are broken though.

--- src/estimate/estimate.h
+++ src/estimate/estimate.h
@@ -7,16 +7,9 @@
 #define ESTIMATE_H_INC_
 
 #include <vector>
-#include <memory>
 #include <string>
-#include <cmath>
-#include <array>
 #include <cassert>
 
-#include "portage/support/Point.h"
-#include "portage/support/weight.h"
-#include "portage/support/basis.h"
-#include "portage/swarm/swarm.h"
 #include "portage/swarm/swarm_state.h"
 
 namespace Portage {
@@ -24,63 +17,41 @@ namespace Meshfree {
 
 using std::string;
 using std::vector;
-using std::shared_ptr;
-using std::map;
 
-enum EstimateType {
-  Min,
-  Max,
-  KernelDensity,
-  LocalRegression
-};
-
-enum WeightCenter {
-  Gather,
-  Scatter
-};
-
-/*!
- * @brief
- */
 template<size_t dim>
 class Estimate {
  public:
-  Estimate(shared_ptr<Swarm<dim>> source, shared_ptr<Swarm<dim>> target,
-	   shared_ptr<SwarmState<dim>> source_state, shared_ptr<SwarmState<dim>> target_state,
-            EstimateType estimate, WeightCenter center,
-            shared_ptr<vector<Weight::Kernel>> kernels,
-            shared_ptr<vector<Weight::Geometry>> geometries,
-            shared_ptr<vector<vector<double>>> smoothing,
-            Basis::Type basis)
-      : source_(source),
-        target_(target),
-	source_state_(source_state),
-	target_state_(target_state),
-        estimate_(estimate),
-        center_(center),
-        kernels_(kernels),
-        geometries_(geometries),
-        smoothing_(smoothing),
-        basis_(basis)
+  Estimate(shared_ptr<SwarmState<dim>> source_state):
+	source_state_(source_state)
   {}
 
   double operator()(int const target_index,
-                    vector<vector<vector<double>>> const & moment_matrix_bit) const
-  {}
+                    vector<vector<double>> const & shape_vec,
+		    vector<size_t> const& source_particles)
+  {
+    assert(shape_vec.size() == source_particles.size());
+    assert(derivative_ < shape_vec[0].size());
 
-  void set_interpolation_variable(std::string const & interp_var_name) {}
+    typename SwarmState<dim>::DblVecPtr source_field_ptr;
+    source_state_->get_field(var_name_, source_field_ptr);
+    vector<double> &source_field(*source_field_ptr);
+
+    double result=0.;
+    for (size_t i=0; i<source_particles.size(); i++) {
+      result += source_field[source_particles[i]] * shape_vec[i][derivative_];
+    }
+    return result;
+  }
+
+  void set_interpolation_variable(std::string const & interp_var_name, size_t derivin=0) {
+    var_name_ = interp_var_name;
+    derivative_ = derivin;
+  }
 
  private:
-  shared_ptr<Swarm<dim>> source_;
-  shared_ptr<Swarm<dim>> target_;
   shared_ptr<SwarmState<dim>> source_state_;
-  shared_ptr<SwarmState<dim>> target_state_;
-  EstimateType estimate_;
-  WeightCenter center_;
-  shared_ptr<vector<Weight::Kernel>> kernels_;
-  shared_ptr<vector<Weight::Geometry>> geometries_;
-  shared_ptr<vector<vector<double>>> smoothing_;
-  Basis::Type basis_;
+  std::string var_name_;
+  size_t derivative_;
 };
 
 }
