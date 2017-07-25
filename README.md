44aa04ec950e8b69606279ee8669a75b64b3deff
Navamita Ray <nray@lanl.gov>
2017-07-25 17:26:44 -0600

    Added flecsi 3d tests

--- README.md
+++ README.md
@@ -131,20 +131,18 @@ verison of FleCSI on Varan.  An example is below:
 export MODULEPATH=""
 . /opt/local/packages/Modules/default/init/sh
 module load gcc/5.3.0 openmpi/1.10.3 cmake
-JALI_INSTALL_PREFIX=/usr/local/codes/ngc/private/jali/0.9.8-gcc-5.3.0-openmpi-1.10.3
-FLECSI_INSTALL_DIR=/usr/local/codes/ngc/private/flecsi-gcc
+FLECSI_INSTALL_PREFIX=/usr/local/codes/ngc/private/flecsi/gcc-5.3_openmpi10.3.0
+FLECSISP_INSTALL_PREFIX=/usr/local/codes/ngc/private/flecsi-sp/gcc-5.3_openmpi10.3.0
 mkdir build-flecsi
 cd build-flecsi
 cmake \
-    -D CMAKE_C_COMPILER=`which mpicc` \
-    -D CMAKE_CXX_COMPILER=`which mpiCC` \
+    -D CMAKE_C_COMPILER=`which gcc` \
+    -D CMAKE_CXX_COMPILER=`which g++` \
     -D CMAKE_BUILD_TYPE=Debug \
     -D ENABLE_UNIT_TESTS=True \
     -D ENABLE_APP_TESTS=True \
-    -D ENABLE_MPI=True \
-    -D ENABLE_MPI_CXX_BINDINGS=True \
-    -D Jali_DIR:FILEPATH=$JALI_INSTALL_PREFIX/lib \
-    -D FLECSI_INSTALL_DIR:FILEPATH=$FLECSI_INSTALL_DIR \
+    -D ENABLE_FleCSI=True \
+    -D CMAKE_PREFIX_PATH="$FLECSI_INSTALL_PREFIX;$FLECSISP_INSTALL_PREFIX" \
     ..
 make -j2
 ctest -j2 --output-on-failure
---
+++ src/wonton/mesh/flecsi/test/flecsi_3d.cc
@@ -0,0 +1,182 @@
+/*~-------------------------------------------------------------------------~~*
+ * Copyright (c) 2016 Los Alamos National Laboratory, LLC
+ * All rights reserved
+ *~-------------------------------------------------------------------------~~*/
+////////////////////////////////////////////////////////////////////////////////
+// \file
+// \brief Tests general features of the portage mesh wrapper.
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef HAVE_PORTAGE
+
+// user includes
+#include "flecsi_3d_test.h"
+
+// system includes
+#include <vector>
+
+namespace math = flecsi::sp::math;
+namespace mesh = flecsi::sp::burton;
+
+////////////////////////////////////////////////////////////////////////////////
+//! \brief dump the mesh to std out
+////////////////////////////////////////////////////////////////////////////////
+TEST_F(flecsi_3d, first_order) 
+{
+
+  // number of cells wide
+  constexpr size_t num_x = 4;
+  // number of cells high
+  constexpr size_t num_y = 4;
+  // number of cells deep
+  constexpr size_t num_z = 4;
+  // width of mesh
+  constexpr size_t len_x = 1.;
+  // length of mesh
+  constexpr size_t len_y = 1.;
+  // depth of mesh
+  constexpr size_t len_z = 1.;
+
+  auto mesh_a = mesh::box<mesh_t>( num_x, num_y, num_z, 0, 0, 0, len_x, len_y, len_z );
+  auto mesh_b = mesh::box<mesh_t>( num_x*2, num_y*2, num_z*2, 0, 0, 0, len_x, len_y, len_z );
+
+  flecsi_mesh_t mesh_wrapper_a( mesh_a );
+  flecsi_mesh_t mesh_wrapper_b( mesh_b );
+
+  flecsi_state_t state_wrapper_a( mesh_a );
+  flecsi_state_t state_wrapper_b( mesh_b );
+
+  // create the remapper object
+  portage_1st_order_driver_t remapper( 
+    mesh_wrapper_a, 
+    state_wrapper_a, 
+    mesh_wrapper_b, 
+    state_wrapper_b
+  );
+
+  // register data on the source mesh
+  flecsi_register_data(mesh_a, hydro, cell_data, real_t, dense, 1, cells);
+  flecsi_register_data(mesh_b, hydro, cell_data, real_t, dense, 1, cells);
+
+  auto a = flecsi_get_accessor(mesh_a, hydro, cell_data, real_t, dense, 0);
+  auto b = flecsi_get_accessor(mesh_b, hydro, cell_data, real_t, dense, 0);
+
+  a.attributes().set(persistent);
+  b.attributes().set(persistent);
+
+
+  // set some initial distribution in mesh A
+  for ( auto c : mesh_a.cells() ) a[c] = 1.0;
+  for ( auto c : mesh_b.cells() ) b[c] = 0.0;
+
+  // Declare which variables are remapped
+  std::vector<std::string> var_names(1, "cell_data");
+  remapper.set_remap_var_names(var_names);
+
+  // Do the remap
+  remapper.run(false);
+
+  // the result should the same
+  for ( auto c : mesh_b.cells() ) EXPECT_NEAR( b[c], 1.0, test_tolerance );
+
+  // now try something more complicated
+
+  // set some initial distribution in mesh A
+  real_t total_a = 0;
+  for ( auto c : mesh_a.cells() ) {
+    const auto & xc = c->centroid();
+    auto val = math::sqr(xc[0]) + math::sqr(xc[1]) + math::sqr(xc[2]);
+    a[c] = val;
+    total_a += c->volume() * val;
+  }
+  for ( auto c : mesh_b.cells() ) b[c] = 0.0;
+
+  // Do the remap
+  remapper.run(false);
+
+  // the result should the same
+  EXPECT_FALSE( mesh::write_mesh(output_prefix()+"_a.vtk", mesh_a, false) );
+  EXPECT_FALSE( mesh::write_mesh(output_prefix()+"_b.vtk", mesh_b, false) );
+
+  // Check the result
+  real_t total_b = 0;
+  for ( auto c : mesh_b.cells() ) 
+    total_b += c->volume() * b[c];
+ 
+  EXPECT_NEAR( total_a, total_b, test_tolerance );
+
+}
+
+#ifdef HAVE_EXODUS
+
+////////////////////////////////////////////////////////////////////////////////
+//! \brief Remap between a triangular mesh and a quad mesh
+////////////////////////////////////////////////////////////////////////////////
+TEST_F(flecsi_3d, unstruct) 
+{
+
+  mesh_t mesh_a, mesh_b;
+  mesh::read_mesh( "box-hex.g", mesh_a); 
+  mesh::read_mesh( "box-tet.g",  mesh_b); 
+
+  flecsi_mesh_t mesh_wrapper_a( mesh_a );
+  flecsi_mesh_t mesh_wrapper_b( mesh_b );
+
+  flecsi_state_t state_wrapper_a( mesh_a );
+  flecsi_state_t state_wrapper_b( mesh_b );
+
+  // create the remapper object
+  portage_1st_order_driver_t remapper( 
+    mesh_wrapper_a, 
+    state_wrapper_a, 
+    mesh_wrapper_b, 
+    state_wrapper_b
+  );
+
+  // register data on the source mesh
+  flecsi_register_data(mesh_a, hydro, cell_data, real_t, dense, 1, cells);
+  flecsi_register_data(mesh_b, hydro, cell_data, real_t, dense, 1, cells);
+
+  auto a = flecsi_get_accessor(mesh_a, hydro, cell_data, real_t, dense, 0);
+  auto b = flecsi_get_accessor(mesh_b, hydro, cell_data, real_t, dense, 0);
+
+  a.attributes().set(persistent);
+  b.attributes().set(persistent);
+
+
+  // set some initial distribution in mesh A
+  for ( auto c : mesh_a.cells() ) a[c] = 1.0;
+  for ( auto c : mesh_b.cells() ) b[c] = 0.0;
+
+  // Declare which variables are remapped
+  std::vector<std::string> var_names(1, "cell_data");
+  remapper.set_remap_var_names(var_names);
+
+  // Do the remap
+  remapper.run(false);
+
+  // the result should the same
+  for ( auto c : mesh_b.cells() ) EXPECT_NEAR( b[c], 1.0, test_tolerance );
+
+  // now try something more complicated
+
+  // set some initial distribution in mesh A
+  for ( auto c : mesh_a.cells() ) {
+    const auto & xc = c->centroid();
+    a[c] = math::sqr(xc[0]) + math::sqr(xc[1]) + math::sqr(xc[2]);
+  }
+  for ( auto c : mesh_b.cells() ) b[c] = 0.0;
+
+  // Do the remap
+  remapper.run(false);
+
+  // the result should the same
+  EXPECT_FALSE( mesh::write_mesh(output_prefix()+"_a.vtk", mesh_a, false) );
+  EXPECT_FALSE( mesh::write_mesh(output_prefix()+"_b.vtk", mesh_b, false) );
+
+}
+
+#endif // HAVE_EXODUS
+
+#endif // HAVE_PORTAGE
+
