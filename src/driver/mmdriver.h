db6ac6d628097784e963408169509c8b98a62477
Rao Garimella <rao@lanl.gov>
2017-12-19 16:41:52 -0700

    put back declaration of DummyInterfaceReconstructor that was removed by mistake

--- src/driver/mmdriver.h
+++ src/driver/mmdriver.h
@@ -47,6 +47,24 @@ namespace Portage {
 
 using namespace Wonton;
 
+
+// Dummy interface reconstruction class, if external interface
+// reconstruction methods are not found
+template<class Mesh_Wrapper, int Dim> class DummyInterfaceReconstructor {
+ public:
+  DummyInterfaceReconstructor(Mesh_Wrapper const & mesh) {};
+  void set_volume_fractions(std::vector<int> const& cell_num_mats,
+                            std::vector<int> const& cell_mat_ids,
+                            std::vector<double> const& cell_mat_volfracs,
+                            std::vector<Tangram::Point<Dim>> const& cell_mat_centroids) {}; 
+  void set_volume_fractions(std::vector<int> const& cell_num_mats,
+                            std::vector<int> const& cell_mat_ids,
+                            std::vector<double> const& cell_mat_volfracs) {};
+
+  void set_cell_indices_to_operate_on(std::vector<int> const& cellIDs_to_op_on) {}
+  std::shared_ptr<Tangram::CellMatPoly<Dim>> operator()(const int cell_op_ID) const {}
+};
+
 /*!
   @class MMDriver "mmdriver.h"
   @brief MMDriver provides the API to mapping multi-material data from one mesh to another.
