18d98b356538b67ea76e59d60f2218012a9bb271
Danny Shevitz <shevitz@lanl.gov>
2018-01-05 11:27:10 -0700

    both tests in test_gradient seem to be working

--- src/interpolate/test/test_gradient.cc
+++ src/interpolate/test/test_gradient.cc
@@ -27,6 +27,7 @@ TEST(Gradient, Fields_Cell_Ctr) {
   // Create a 4 cell mesh
   std::shared_ptr<Portage::Simple_Mesh> mesh1 =
       std::make_shared<Portage::Simple_Mesh>(0.0, 0.0, 1.0, 1.0, 4, 4);
+  ASSERT_TRUE(mesh1 != nullptr);
 
   // create the wrapper
   Wonton::Simple_Mesh_Wrapper meshwrapper(*mesh1);
@@ -52,7 +53,7 @@ TEST(Gradient, Fields_Cell_Ctr) {
 
   // set the data (x+2*y)
   for (int c = 0; c < nc1; c++) {
-    Portage::Point<3> ccen;
+    Portage::Point<2> ccen;
     meshwrapper.cell_centroid(c, &ccen);
     data2[c] = ccen[0] + 2 * ccen[1];
   }
@@ -136,129 +137,120 @@ TEST(Gradient, Fields_Cell_Ctr) {
   }
 }
 
-/// Test gradient computation with node centered fields
+// Test gradient computation with node centered fields
 
-// TEST(Gradient, Fields_Node_Ctr) {
-//   // Make a 3x3 mesh
+TEST(Gradient, Fields_Node_Ctr) {
+  // Make a 3x3 mesh
+  std::shared_ptr<Portage::Simple_Mesh> mesh1 =
+      std::make_shared<Portage::Simple_Mesh>(0.0, 0.0, 1.0, 1.0, 3, 3);
+  ASSERT_TRUE(mesh1 != nullptr);
 
-//   Portage::MeshFactory mf(MPI_COMM_WORLD);
+  // create the wrapper
+  Wonton::Simple_Mesh_Wrapper meshwrapper(*mesh1);
 
-//   if (Portage::framework_available(Portage::MSTK))
-//   mf.framework(Portage::MSTK);
-//   mf.included_entities({Portage::Entity_kind::EDGE,
-//   Portage::Entity_kind::FACE,
-//                         Portage::Entity_kind::WEDGE,
-//                         Portage::Entity_kind::CORNER});
+  // Create a state object
+  Portage::Simple_State mystate(mesh1);
 
-//   std::shared_ptr<Portage::Mesh> mesh1 = mf(0.0, 0.0, 1.0, 1.0, 3, 3);
-//   ASSERT_TRUE(mesh1 != nullptr);
+  // Create a state Wrapper
+  Wonton::Simple_State_Wrapper statewrapper(mystate);
 
-//   // Create a state object and add the first two vectors to it
+  // get the number of nodes
+  const int nn1 = meshwrapper.num_owned_nodes();
 
-//   Portage::State mystate(mesh1);
+  std::vector<double> data1(nn1, 1.5);
 
-//   // Define three state vectors, one with constant value, the other
-//   // with a linear function
+  // add the data vector to the state
+  mystate.add("nodevars1", Portage::Entity_kind::NODE, &(data1[0]));
 
-//   int nn1 = mesh1->num_entities(Portage::Entity_kind::NODE,
-//                                 Portage::Entity_type::PARALLEL_OWNED);
+  std::vector<double> data2(nn1);
 
-//   std::vector<double> data1(nn1, 1.5);
+  for (int n = 0; n < nn1; ++n) {
+    Portage::Point<2> nodexy;
+    meshwrapper.node_get_coordinates(n, &nodexy);
+    data2[n] = 3 * nodexy[0] + nodexy[1];
+  }
 
-//   Portage::StateVector<double> myvec1(
-//       "nodevars1", mesh1, Portage::Entity_kind::NODE,
-//       Portage::Entity_type::PARALLEL_OWNED, &(data1[0]));
-//   Portage::StateVector<double> &addvec1 = mystate.add(myvec1);
+  // add the data vector to the state
+  mystate.add("nodevars2", Portage::Entity_kind::NODE, &(data2[0]));
 
-//   std::vector<double> data2(nn1);
-//   for (int n = 0; n < nn1; ++n) {
-//     PortageGeometry::Point nodexy;
-//     mesh1->node_get_coordinates(n, &nodexy);
-//     data2[n] = 3 * nodexy[0] + nodexy[1];
-//   }
-//   Portage::StateVector<double> myvec2(
-//       "nodevars2", mesh1, Portage::Entity_kind::NODE,
-//       Portage::Entity_type::PARALLEL_OWNED, &(data2[0]));
-//   Portage::StateVector<double> &addvec2 = mystate.add(myvec2);
+  // Create Gradient calculater objects
 
-//   // Create Gradient calculater objects
+  Portage::Limited_Gradient<2, Portage::NODE, Wonton::Simple_Mesh_Wrapper,
+                            Wonton::Simple_State_Wrapper>
+      gradcalc1(meshwrapper, statewrapper, "nodevars1", Portage::NOLIMITER);
 
-//   Wonton::Portage_Mesh_Wrapper meshwrapper(*mesh1);
-//   Wonton::Portage_State_Wrapper statewrapper(mystate);
+  Portage::Limited_Gradient<2, Portage::NODE, Wonton::Simple_Mesh_Wrapper,
+                            Wonton::Simple_State_Wrapper>
+      gradcalc2(meshwrapper, statewrapper, "nodevars2", Portage::NOLIMITER);
 
-//   Portage::Limited_Gradient<2, Portage::NODE, Wonton::Portage_Mesh_Wrapper,
-//                             Wonton::Portage_State_Wrapper>
-//       gradcalc1(meshwrapper, statewrapper, "nodevars1", Portage::NOLIMITER);
-//   Portage::Limited_Gradient<2, Portage::NODE, Wonton::Portage_Mesh_Wrapper,
-//                             Wonton::Portage_State_Wrapper>
-//       gradcalc2(meshwrapper, statewrapper, "nodevars2", Portage::NOLIMITER);
-//   Portage::Limited_Gradient<2, Portage::NODE, Wonton::Portage_Mesh_Wrapper,
-//                             Wonton::Portage_State_Wrapper>
-//       gradcalc3(meshwrapper, statewrapper, "nodevars1",
-//                 Portage::BARTH_JESPERSEN);
-//   Portage::Limited_Gradient<2, Portage::NODE, Wonton::Portage_Mesh_Wrapper,
-//                             Wonton::Portage_State_Wrapper>
-//       gradcalc4(meshwrapper, statewrapper, "nodevars2",
-//                 Portage::BARTH_JESPERSEN);
+  Portage::Limited_Gradient<2, Portage::NODE, Wonton::Simple_Mesh_Wrapper,
+                            Wonton::Simple_State_Wrapper>
+      gradcalc3(meshwrapper, statewrapper, "nodevars1",
+                Portage::BARTH_JESPERSEN);
 
-//   // Make sure we retrieved the correct gradient value for each node
-//   // For field 1, it is a constant
-//   // For field 2, it is a linear function
+  Portage::Limited_Gradient<2, Portage::NODE, Wonton::Simple_Mesh_Wrapper,
+                            Wonton::Simple_State_Wrapper>
+      gradcalc4(meshwrapper, statewrapper, "nodevars2",
+                Portage::BARTH_JESPERSEN);
 
-//   Portage::Vector<2> grad;
+  // Make sure we retrieved the correct gradient value for each node
+  // For field 1, it is a constant
+  // For field 2, it is a linear function
 
-//   for (int n = 0; n < nn1; ++n) {
-//     // unlimited gradient of constant function
+  Portage::Vector<2> grad;
 
-//     grad = gradcalc1(n);
-//     ASSERT_NEAR(0.0, grad[0], 1.0e-10);
-//     ASSERT_NEAR(0.0, grad[1], 1.0e-10);
+  for (int n = 0; n < nn1; ++n) {
+    // unlimited gradient of constant function
 
-//     // unlimited gradient of linear function
+    grad = gradcalc1(n);
+    ASSERT_NEAR(0.0, grad[0], 1.0e-10);
+    ASSERT_NEAR(0.0, grad[1], 1.0e-10);
 
-//     grad = gradcalc2(n);
-//     ASSERT_NEAR(3.0, grad[0], 1.0e-10);
-//     ASSERT_NEAR(1.0, grad[1], 1.0e-10);
+    // unlimited gradient of linear function
 
-//     // limited gradient of constant function
+    grad = gradcalc2(n);
+    ASSERT_NEAR(3.0, grad[0], 1.0e-10);
+    ASSERT_NEAR(1.0, grad[1], 1.0e-10);
 
-//     grad = gradcalc3(n);
-//     ASSERT_NEAR(0.0, grad[0], 1.0e-10);
-//     ASSERT_NEAR(0.0, grad[1], 1.0e-10);
+    // limited gradient of constant function
 
-//     // unlimited gradient of linear function
-//     //
-//     // For now, the limiter does not know anything about boundary
-//     // conditions and therefore, it is a little unpredictable what the
-//     // limiting will be in boundary nodes/dual-cells (which are not
-//     // completely surrounded by other dual cells). So check only
-//     // interior nodes.
+    grad = gradcalc3(n);
+    ASSERT_NEAR(0.0, grad[0], 1.0e-10);
+    ASSERT_NEAR(0.0, grad[1], 1.0e-10);
 
-//     grad = gradcalc4(n);
+    // unlimited gradient of linear function
+    //
+    // For now, the limiter does not know anything about boundary
+    // conditions and therefore, it is a little unpredictable what the
+    // limiting will be in boundary nodes/dual-cells (which are not
+    // completely surrounded by other dual cells). So check only
+    // interior nodes.
 
-//     // use the primary mesh to check if the node (dual cell) is a
-//     // boundary node and don't check answer if its a boundary node
+    grad = gradcalc4(n);
 
-//     bool boundary_node = false;
-//     std::vector<int> nodecells;
-//     mesh1->node_get_cells(n, Portage::Entity_type::ALL, &nodecells);
-//     for (auto nc : nodecells) {
-//       std::vector<int> cfaces;
-//       mesh1->cell_get_faces(nc, &cfaces);
-//       for (auto f : cfaces) {
-//         std::vector<int> fcells;
-//         mesh1->face_get_cells(f, Portage::Entity_type::ALL, &fcells);
-//         if (fcells.size() == 1) {
-//           boundary_node = true;
-//           break;
-//         }
-//       }
-//       if (boundary_node) break;
-//     }
+    // use the primary mesh to check if the node (dual cell) is a
+    // boundary node and don't check answer if its a boundary node
 
-//     if (!boundary_node) {
-//       ASSERT_NEAR(3.0, grad[0], 1.0e-10);
-//       ASSERT_NEAR(1.0, grad[1], 1.0e-10);
-//     }
-//   }
-// }
+    bool boundary_node = false;
+    std::vector<int> nodecells;
+    meshwrapper.node_get_cells(n, Portage::Entity_type::ALL, &nodecells);
+    for (auto nc : nodecells) {
+      std::vector<int> cfaces, cellfaceDirs;
+      meshwrapper.cell_get_faces_and_dirs(nc, &cfaces, &cellfaceDirs);
+      for (auto f : cfaces) {
+        std::vector<int> fcells;
+        meshwrapper.face_get_cells(f, Portage::Entity_type::ALL, &fcells);
+        if (fcells.size() == 1) {
+          boundary_node = true;
+          break;
+        }
+      }
+      if (boundary_node) break;
+    }
+
+    if (!boundary_node) {
+      ASSERT_NEAR(3.0, grad[0], 1.0e-10);
+      ASSERT_NEAR(1.0, grad[1], 1.0e-10);
+    }
+  }
+}
