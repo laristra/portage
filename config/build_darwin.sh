393218b6ac96193417dab2e0c854fcc6de3e6432
Danny Shevitz <shevitz@lanl.gov>
2018-02-08 16:49:59 -0700

    added test_tangram_intersect.cc for testing matpoly intersections with target cells

--- config/build_darwin.sh
+++ config/build_darwin.sh
@@ -5,6 +5,7 @@ cd build
 cmake \
   -D CMAKE_BUILD_TYPE=Debug \
   -D ENABLE_MPI=True \
+  -D ENABLE_APP_TESTS=True \
   -D ENABLE_UNIT_TESTS=True \
   -D ENABLE_MPI_CXX_BINDINGS=True \
   -D Jali_DIR:FILEPATH=$JALI_INSTALL_PREFIX/lib \
