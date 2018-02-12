13ea6645080cbb71fad2ce2cd6169c616d825027
Eugene Kikinzon <kikinzon@lanl.gov>
2018-02-12 14:32:59 -0700

    Added function for error computation. Hausdorff distance between actual and reference material interfaces is calculated for every multi-material cell. The max distance over all cells is returned as the value of the error.

--- app/ir_linetest_app/ir_linetest_app.cc
+++ app/ir_linetest_app/ir_linetest_app.cc
@@ -33,8 +33,9 @@ const double mat_int_b = -0.25;
 const int mat_id_above = 0;
 const int mat_id_below = 1;
 
-const double deps = 1.0e-15;	//Distance tolerance
-const double seps = 1.0e-14;	//Size(area) tolerance
+const double deps = 1.0e-15;	 //Distance tolerance
+const double seps = 1.0e-14;	 //Size(area) tolerance
+const double denom_eps = 1.0e-6; //Denominator tolerance
 
 void R2DizeLine(const double line_a,
                 const double line_b,
@@ -47,10 +48,10 @@ void R2DizeCell(const Mesh_Wrapper& Mesh,
 
 template <class Mesh_Wrapper>
 int CellPosition(const Mesh_Wrapper& Mesh,
-                const int cell_id,
-                const double line_a,
-                const double line_b,
-                const double yeps = deps);
+                 const int cell_id,
+                 const double line_a,
+                 const double line_b,
+                 const double yeps = deps);
 
 template <class Mesh_Wrapper>
 void get_materials_data(const Mesh_Wrapper& Mesh,
@@ -61,10 +62,29 @@ void get_materials_data(const Mesh_Wrapper& Mesh,
                         std::vector<double>& cell_mat_volfracs,
                         std::vector<Tangram::Point2>& cell_mat_centroids);
 
+int PointPosition(const Portage::Point2& pt,
+                  const Portage::Vector2& line_nvec,
+                  const Portage::Point2& line_pt,
+                  const double eps = deps);
+
+Portage::Point2 LinesIntersect(std::vector< std::vector<Portage::Point2> > lines_pts,
+                               double den_eps,
+                               double eps = deps);
+
+double SegmentsDistance(const std::vector<std::vector<Portage::Point2>>& segments);
+
+template <class Mesh_Wrapper>
+double get_ir_error(const Mesh_Wrapper& Mesh,
+                    const double line_a,
+                    const double line_b,
+                    const std::vector<int>& cell_num_mats,
+                    const std::vector<std::shared_ptr<Tangram::CellMatPoly<2>>>&
+                      cellmatpoly_list,
+                    const double eps = deps);
+
 int main(int argc, char** argv) {
 #ifdef ENABLE_MPI
   MPI_Init(&argc, &argv);
-  MPI_Comm comm = MPI_COMM_WORLD;
 #endif
   if (argc != 3) {
     std::ostringstream os;
@@ -99,6 +119,11 @@ int main(int argc, char** argv) {
   
   const std::vector<std::shared_ptr<Tangram::CellMatPoly<2>>>&
     cellmatpoly_list = xmof_driver.cell_matpoly_ptrs();
+
+  double source_ir_rel_error = get_ir_error(source_mesh_wrapper, mat_int_a, mat_int_b,
+                                            cell_num_mats, cellmatpoly_list, deps);
+  std::cout << "Reconstruction error for the source mesh -> " << source_ir_rel_error <<
+               std::endl;
   
   Tangram::write_to_gmv(source_mesh_wrapper, 2, cell_num_mats, cell_mat_ids,
                         cellmatpoly_list, "source_mesh.gmv");
@@ -201,13 +226,13 @@ void get_materials_data(const Mesh_Wrapper& Mesh,
         double poly_above_vfrac = moments[0]/cell_size;
         cell_mat_volfracs.push_back(poly_above_vfrac);
         Tangram::Point2 poly_above_cen(
-                                       moments[1]/moments[0], moments[2]/moments[0]);
+          moments[1]/moments[0], moments[2]/moments[0]);
         cell_mat_centroids.push_back(poly_above_cen);
         
         cell_mat_ids.push_back(mat_id_below);
         cell_mat_volfracs.push_back(1.0 - poly_above_vfrac);
         Tangram::Point2 poly_below_cen(
-                                       (cell_cen - poly_above_vfrac*poly_above_cen)/(1.0 - poly_above_vfrac));
+          (cell_cen - poly_above_vfrac*poly_above_cen)/(1.0 - poly_above_vfrac));
         cell_mat_centroids.push_back(poly_below_cen);
       }
       else {
@@ -229,3 +254,181 @@ void get_materials_data(const Mesh_Wrapper& Mesh,
     }
   }
 }
+
+int PointPosition(const Portage::Point2& pt,
+                  const Portage::Vector2& line_nvec,
+                  const Portage::Point2& line_pt,
+                  const double eps) {
+  Portage::Vector2 vec2line = line_pt - pt;
+  double prj = dot(vec2line, line_nvec);
+  int pos;
+  if (std::fabs(prj) < eps) pos = 0;
+  else pos = signbit(prj) ? -1 : 1;
+  
+  return pos;
+}
+
+Portage::Point2 LinesIntersect(std::vector< std::vector<Portage::Point2> > lines_pts,
+                               double denom_eps,
+                               double eps) {
+  Portage::Point2 p_int;
+
+  double denom = (lines_pts[0][0][0] - lines_pts[0][1][0])*(lines_pts[1][0][1] - lines_pts[1][1][1]) -
+  (lines_pts[0][0][1] - lines_pts[0][1][1])*(lines_pts[1][0][0] - lines_pts[1][1][0]);
+  
+  if (std::fabs(denom) > denom_eps) {
+    p_int[0] = (lines_pts[0][0][0]*lines_pts[0][1][1] - lines_pts[0][0][1]*lines_pts[0][1][0])*
+    (lines_pts[1][0][0] - lines_pts[1][1][0]) -
+    (lines_pts[1][0][0]*lines_pts[1][1][1] - lines_pts[1][0][1]*lines_pts[1][1][0])*
+    (lines_pts[0][0][0] - lines_pts[0][1][0]);
+    p_int[1] = (lines_pts[0][0][0]*lines_pts[0][1][1] - lines_pts[0][0][1]*lines_pts[0][1][0])*
+    (lines_pts[1][0][1] - lines_pts[1][1][1]) -
+    (lines_pts[1][0][0]*lines_pts[1][1][1] - lines_pts[1][0][1]*lines_pts[1][1][0])*
+    (lines_pts[0][0][1] - lines_pts[0][1][1]);
+    
+    p_int /= denom;
+  }
+  else {
+    Portage::Vector2 line1_nvec(lines_pts[1][1][1] - lines_pts[1][0][1],
+                                lines_pts[1][0][0] - lines_pts[1][1][0]);
+    line1_nvec.normalize();
+    Portage::Point2 end_pts[2];
+    end_pts[0] = lines_pts[0][0]; end_pts[1] = lines_pts[0][1];
+    int end_pts_pos[2];
+    end_pts_pos[0] = PointPosition(end_pts[0], line1_nvec, lines_pts[1][0], eps);
+    end_pts_pos[1] = PointPosition(end_pts[1], line1_nvec, lines_pts[1][0], eps);
+    if (end_pts_pos[0] == end_pts_pos[1]) {
+      double d2line[2];
+      for (int ipt = 0; ipt < 2; ipt++) {
+        Portage::Vector2 vec2line = lines_pts[1][0] - end_pts[ipt];
+        d2line[ipt] = std::fabs(dot(vec2line, line1_nvec));
+      }
+      int inearest = (d2line[0] < d2line[1]) ? 0 : 1;
+      while (end_pts_pos[0] == end_pts_pos[1]) {
+        Portage::Vector2 dvec = end_pts[inearest] - end_pts[(inearest + 1)%2];
+        end_pts[inearest] += 2*dvec;
+        end_pts_pos[inearest] = PointPosition(end_pts[inearest], line1_nvec, lines_pts[1][0], eps);
+      }
+    }
+    int mid_pt_pos;
+    do {
+      Portage::Point2 mid_pt = 0.5*(end_pts[0] + end_pts[1]);
+      mid_pt_pos = PointPosition(mid_pt, line1_nvec, lines_pts[1][0], eps);
+      if (mid_pt_pos == 0)
+        p_int = mid_pt;
+      else if (mid_pt_pos == end_pts_pos[0])
+        end_pts[0] = mid_pt;
+      else
+        end_pts[1] = mid_pt;
+    } while (mid_pt_pos != 0);
+  }
+  
+  return p_int;
+}
+
+double SegmentsDistance(const std::vector<std::vector<Portage::Point2>>& segments) {
+  double max_dist = 0.0;
+  for (int iseg = 0; iseg < 2; iseg++) {
+    int iother = (iseg + 1)%2;
+    Portage::Vector2 other_dvec = segments[iother][1] - segments[iother][0];
+    double other_len = other_dvec.norm();
+    other_dvec /= other_len;
+    for (int ipt = 0; ipt < 2; ipt++) {
+      double shift = dot(segments[iseg][ipt] - segments[iother][0], other_dvec);
+      double cur_dist;
+      if (shift <= 0.0)
+        cur_dist = (segments[iseg][ipt] - segments[iother][0]).norm();
+      else if (shift >= other_len)
+        cur_dist = (segments[iseg][ipt] - segments[iother][1]).norm();
+      else {
+        Portage::Point2 prj_pt = segments[iother][0] + shift*other_dvec;
+        cur_dist = (segments[iseg][ipt] - prj_pt).norm();
+      }
+      if (cur_dist > max_dist) max_dist = cur_dist;
+    }
+  }
+  return max_dist;
+}
+
+template <class Mesh_Wrapper>
+double get_ir_error(const Mesh_Wrapper& Mesh,
+                    const double line_a,
+                    const double line_b,
+                    const std::vector<int>& cell_num_mats,
+                    const std::vector<std::shared_ptr<Tangram::CellMatPoly<2>>>&
+                      cellmatpoly_list,
+                    const double eps) {
+  int ncells = Mesh.num_owned_cells();
+  double max_hdist = 0.0;
+  int offset = 0;
+  for (int icell = 0; icell < ncells; icell++) {
+    if (cell_num_mats[icell] == 1) {
+      if (cellmatpoly_list[icell] != nullptr)
+        throw std::runtime_error("CellMatPoly was constructed for a single-material cell!");
+      continue;
+    }
+    if (cellmatpoly_list[icell] == nullptr)
+      throw std::runtime_error("CellMatPoly was NOT constructed for a multi-material cell!");
+
+    std::vector<int> ifaces, fdirs;
+    Mesh.cell_get_faces_and_dirs(icell, &ifaces, &fdirs);
+    int nsides = (int) ifaces.size();
+    std::vector<Portage::Point2> ref_int_pts;
+    for (int iside = 0; iside < nsides; iside++) {
+      std::vector<int> inodes;
+      Mesh.face_get_nodes(ifaces[iside], &inodes);
+      std::vector<Portage::Point2> side_pts(2);
+      Mesh.node_get_coordinates(inodes[0], &side_pts[0]);
+      Mesh.node_get_coordinates(inodes[1], &side_pts[1]);
+      int pts_pos[2];
+      for (int ipt = 0; ipt < 2; ipt++) {
+        double line_y = line_a*side_pts[ipt][0] + line_b;
+        if (side_pts[ipt][1] < line_y - eps)
+          pts_pos[ipt] = -1;
+        else if (side_pts[ipt][1] > line_y + eps)
+          pts_pos[ipt] = 1;
+        else
+          pts_pos[ipt] = 0;
+      }
+
+      if (!pts_pos[0] || !pts_pos[1]) {
+        Portage::Point2 ref_int = pts_pos[0] ? side_pts[1] : side_pts[0];
+        bool new_int_pt = true;
+        for (int iip = 0; iip < ref_int_pts.size(); iip++)
+          if (approxEq(ref_int, ref_int_pts[iip], eps)) {
+            new_int_pt = false;
+            break;
+          }
+        if (new_int_pt)
+          ref_int_pts.push_back(ref_int);
+      }
+      else if (pts_pos[0] != pts_pos[1]) {
+        std::vector<Portage::Point2> line_pts = {
+          Portage::Point2(0.0, line_b), Portage::Point2(1.0, line_a + line_b) };
+        Portage::Point2 ref_int = LinesIntersect({side_pts, line_pts}, denom_eps, eps);
+        ref_int_pts.push_back(ref_int);
+      }
+    }    
+    if (ref_int_pts.size() != 2)
+      throw std::runtime_error("Expected two intersection points!");
+
+    const Tangram::CellMatPoly<2>& cell_mat_poly = *cellmatpoly_list[icell];
+    int nfaces = cell_mat_poly.num_matfaces();
+    std::vector<int> iintfaces;
+    for (int iface = 0; iface < nfaces; iface++)
+      if (cell_mat_poly.matface_is_interface(iface))
+        iintfaces.push_back(iface);
+    if (iintfaces.size() != 1)
+      throw std::runtime_error("All CellMatPoly's are expected to have only one interior face!");
+    else {
+      std::vector<Tangram::Point2> int_face_pts =
+        cell_mat_poly.matface_points(iintfaces[0]);
+      std::vector<Portage::Point2> ir_int_pts = { Portage::Point2(int_face_pts[0]), 
+                                                  Portage::Point2(int_face_pts[1]) };
+      double cur_mat_int_dist = SegmentsDistance({ ir_int_pts, ref_int_pts });
+      if (cur_mat_int_dist > max_hdist)  max_hdist = cur_mat_int_dist;
+    }
+  }
+  return (max_hdist > eps) ? max_hdist : 0.0;
+}
+
