9bce9f0f53eda4960235caff748dde9b11c46a7a
Danny Shevitz <shevitz@lanl.gov>
2018-04-17 18:12:49 -0600

    added state directory

---
+++ portage/state/test/test_state_vector.cc
@@ -0,0 +1,16 @@
+/*
+This file is part of the Ristra portage project.
+Please see the license file at the root of this repository, or at:
+    https://github.com/laristra/portage/blob/master/LICENSE
+*/
+
+
+
+#include "portage/state/state_vector.h"
+
+#include "gtest/gtest.h"
+
+
+TEST(State, Basic) {
+  ASSERT_EQ(1.,0.);
+}
