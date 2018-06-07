48bb4d74d57beb01fc93f682808b091cad297cab
Danny Shevitz <shevitz@lanl.gov>
2018-06-07 10:51:01 -0600

    removed cached size for mesh number of cells

--- portage/state/state_manager.h
+++ portage/state/state_manager.h
@@ -9,7 +9,6 @@ Please see the license file at the root of this repository, or at:
 
 #include <string>
 #include <iostream>
-#include <map>
 #include <unordered_map>
 #include <unordered_set>
 #include <memory>
@@ -21,8 +20,6 @@ Please see the license file at the root of this repository, or at:
 #include "portage/state/state_vector_base.h"
 #include "portage/state/state_vector_uni.h"
 #include "portage/state/state_vector_multi.h"
-#include "portage/state/state_vector_uni_raw.h"
-#include "portage/state/state_vector_multi_raw.h"
 
 namespace Portage {  
 
@@ -30,14 +27,16 @@ template <class MeshWrapper>
 class StateManager {
 
 	public:
+
 	 		
  		StateManager (const MeshWrapper& mesh, 
  			std::unordered_map<std::string,int> names={},
  			std::unordered_map<int,std::vector<int>> material_cells={}
- 			):mesh_(mesh), num_cells_(mesh_.num_owned_cells()){
+ 			):mesh_(mesh){
  				add_material_names(names);
  				add_material_cells(material_cells);
  		}
+
  		
 		void add(std::shared_ptr<StateVectorBase> sv){
 		
@@ -52,6 +51,7 @@ class StateManager {
 			// copies the shared pointer
  			state_vectors_[key]=sv;
 		}
+
 		
 		// specialization for adding StateVectorUni's
 		// we need to do size checking and this is the easiest way
@@ -67,14 +67,15 @@ class StateManager {
 					" already exists in the state manager");
 					
 			// check that the size is correct
-			if (sv->get_data().size()!=num_cells_) throw std::runtime_error(
+			if (sv->get_data().size()!=mesh_.num_owned_cells()) throw std::runtime_error(
 				"The added data did not have the same number of elements (" + 
 				std::to_string(sv->get_data().size()) +") as the number of cells ("+
-				std::to_string(num_cells_)+")");
+				std::to_string(mesh_.num_owned_cells())+")");
 				
 			// copies the shared pointer
  			state_vectors_[key]=sv;
 		}
+
 		
 		// specialization for adding StateVectorMulti's
 		// we need to do size checking and this is the easiest way
@@ -98,6 +99,7 @@ class StateManager {
 			// copies the shared pointer
  			state_vectors_[key]=sv;
 		}
+
 		
 		// is the data the correct shape
 		template <class T=double>
@@ -122,6 +124,7 @@ class StateManager {
 			
 			return true;
 		}
+
 					
  		// get the data and do the cast internal to the function
  		// note this call always works even if the key doesn't exist. We can check
@@ -140,6 +143,7 @@ class StateManager {
 			return std::dynamic_pointer_cast<StateVectorType<T>>(state_vectors_[key]);
  			
 		}	
+
 		
  		// get the data and do the cast internal to the function
  		// note this call always works even if the key doesn't exist. We can check
@@ -157,44 +161,13 @@ class StateManager {
 			return state_vectors_[key];
  			
 		}	
-/**/		
- 		// convenience function for getting StateVectorMulti
- 		// note this call always works even if the key doesn't exist. We can check
- 		// the return against a nullptr
-/* 		template <class T=double>
-		std::shared_ptr<StateVectorMulti<T>> get(std::string name){
-		
-			// create the key
-			std::string key{name};
-			
-			if (state_vectors_.find(key)==state_vectors_.end()){
-				return nullptr;
-			}			
-			
-			// do the return all in one step
-			return std::dynamic_pointer_cast<StateVectorMulti<T>>(state_vectors_[key]);
- 			
-		}	
-*/
+
 		
 		////////////////////
 		// Const Versions
 		////////////////
-/* 		template <class T=double>
-		std::shared_ptr<StateVectorMulti<T>> get(std::string name) const{
+
 		
-			// create the key
-			std::string key{name};
-			
-			if (state_vectors_.find(key)==state_vectors_.end()){
-				return nullptr;
-			}			
-			
-			// do the return all in one step
-			return std::dynamic_pointer_cast<StateVectorMulti<T>>(state_vectors_.at(key));
- 			
-		}	
-*/		
  		template <class T, template<class> class StateVectorType>
 		std::shared_ptr<StateVectorType<T>> get(std::string name) const{
 		
@@ -209,12 +182,14 @@ class StateManager {
 			return std::dynamic_pointer_cast<StateVectorType<T>>(state_vectors_.at(key));
  			
 		}	
+
 				
 		// add the names of the materials
 		void add_material_names(std::unordered_map<std::string,int> names){
 			material_ids_=names;
 			for (auto& kv: names) material_names_[kv.second]=kv.first;				
 		}
+
 		
 		// add the material cells
 		void add_material_cells(std::unordered_map<int,std::vector<int>> cells){
@@ -244,38 +219,47 @@ class StateManager {
 			material_data_shape_=shape;
 		}
 
+
 		const std::unordered_map<int,int> get_material_shape() const {
 				return material_data_shape_;
 		}
+
 			
 		// get the number of materials
 		int get_num_materials() const {return material_names_.size();}
 		int num_materials() const {return material_names_.size();}
 
+
 		// get a material name from its id
 		std::string get_material_name(int id) {return material_names_[id];}
+
 		
 		// get the material id from its name
 		int get_material_id(std::string name) {return material_ids_[name];}
+
 		
 		// Get the number of cells in the material
   	int num_material_cells(int m) {return material_data_shape_[m];}
+
 
 		// Get the cells in the m'th material. If no materials have been defined
 		// return reference to a dummy vector thats empty
 		std::vector<int> const& get_material_cells(int m)  {
 			return material_cells_[m];
 		}
+
 		
 		// Get the number of materials in a cell
 		int num_cell_materials(int c) {    	
     	return cell_materials_[c].size();
   	}
+
   	
   	// Get the materials in a cell
   	std::unordered_set<int> get_cell_materials(int c) {
   		return cell_materials_[c];
   	}
+
   	
   	// Get the cell index in material
   	int cell_index_in_material(int c, int m) const{
@@ -290,6 +274,7 @@ class StateManager {
   		return std::distance(cells.begin(),it);
   		
   	}
+
   	
   	// Get the Entity_kind for a name
   	// NOTE: This may not be unique, a name can be reused, but for the moment...
@@ -308,6 +293,7 @@ class StateManager {
   		return state_vectors_.at(name)->type();
   	}
 
+
 		// Get the states in the states in state manager
 		std::vector<std::string> get_state_keys(){
 			std::vector<std::string> keys;
@@ -316,6 +302,7 @@ class StateManager {
 			} 
 			return keys;
 		}
+
 		
 		void mat_get_cells(int matid, std::vector<int> *matcells) const {
 			// the "this" is for a gcc compiler error about assert and templates
@@ -323,6 +310,7 @@ class StateManager {
 		  matcells->clear();
 		  *matcells = material_cells_.at(matid);
 		}
+
 		
 		template <class T>
 		void mat_get_celldata(std::string const& var_name, int matid,
@@ -330,31 +318,37 @@ class StateManager {
 		  *data= get<T,StateVectorMulti>(var_name)->get_data()[matid].data();                
 		}
 
+
   	int cell_get_num_mats(int cellid) const {
     	return cell_materials_.at(cellid).size();
   	}
+
 	
 		void cell_get_mats(int cellid, std::vector<int> *cellmats) const {
 		  cellmats->clear();
 		  auto& mats=cell_materials_.at(cellid);
 		  *cellmats = std::vector<int>{mats.begin(),mats.end()};
 		} 
+
 		
 		template <class T>
 		void mesh_get_data(Entity_kind on_what, std::string const& var_name,
 		                   T const **data) const {
 		  *data = get<T,StateVectorUni>(var_name)->get_data().data();		                 
 		}
+
 		
 		template <class T>
 		void mesh_get_data(Entity_kind on_what, std::string const& var_name,
 		                   T **data) {
 		  *data = get<T,StateVectorUni>(var_name)->get_data().data();		                 
 		}
+
 		
 		std::string material_name(int matid) const {
      return material_names_.at(matid);
 		} 
+
 		
 		void mat_add_cells(int matid, std::vector<int> const& newcells) {
 		
@@ -367,6 +361,7 @@ class StateManager {
 		  
 		}
 
+
 		// starts from scratch, add the material and cell id's
 		// I think this is problematic in a distributed sense, and I don't hit it
 		// in simple_mash app, so for now don't do anythin
@@ -374,6 +369,7 @@ class StateManager {
 			std::string const& matname, std::vector<int> const& matcells) {
 				std::cout<<"in add_material: "<<matname<<std::endl;
 			}
+
 
 		template <class T>
 		void mat_add_celldata(std::string const& var_name, int matid,
@@ -388,6 +384,7 @@ class StateManager {
 		  data.clear();
 		  for (int i=0;i<ncells;++i) data.push_back(*(values+i));
 		}
+
 	
 		// Discussion points:
 		// This function has a side effect. When this function is called, the 
@@ -421,25 +418,26 @@ class StateManager {
 		  
 		  *data= material_data.data();                
 		}
+
 		
 		std::unordered_map<int,std::vector<int>> const get_material_cells(){
 			return material_cells_;
 		}
 
+
 		int mat_get_num_cells(int matid) const {
 			return material_cells_.at(matid).size();
 		}
+
 		
 		int get_data_size(Entity_kind on_what, std::string const& var_name) const {
 			return 0; //fix if ever used
 		}
 
+
 	private:
 	
 		const MeshWrapper& mesh_;
-		
-		// the number of cells
-		int num_cells_;
 		
 		// I'm using a map instead of an unordered map, because there aren't
 		// that many state vectors, and also, to use an unordered_map we need
