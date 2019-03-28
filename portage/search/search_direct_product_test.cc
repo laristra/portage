207018cf441628a108fb94f0d2a6f5200378b3ed
Brendan K. Krueger <bkkrueger@lanl.gov>
2019-03-28 11:40:24 -0600

    Look at all the mistakes I made
    Don't worry -- more to come.

--- portage/search/search_direct_product_test.cc
+++ portage/search/search_direct_product_test.cc
@@ -12,6 +12,8 @@ Please see the license file at the root of this repository, or at:
 // wonton includes
 #include "wonton/mesh/direct_product/direct_product_mesh.h"
 #include "wonton/mesh/direct_product/direct_product_mesh_wrapper.h"
+#include "wonton/wonton/support/CellID.h"
+#include "wonton/wonton/support/IntPoint.h"
 #include "wonton/support/Point.h"
 
 TEST(search_direct_product, case1) {
@@ -48,31 +50,30 @@ TEST(search_direct_product, case1) {
   // Create meshes
   const std::vector<double> x_tgt = {0.0, 0.5, 1.0};
   const std::vector<double> y_tgt = {0.0, 0.5, 1.0};
-  Wonton::Direct_Product_Mesh tgt(x_tgt,y_tgt)
+  Wonton::Direct_Product_Mesh tgt(x_tgt,y_tgt);
   const std::vector<double> x_src = {0.00, 0.25, 0.75, 1.00};
   const std::vector<double> y_src = {0.00, 0.25, 0.75, 1.00};
-  Wonton::Direct_Product_Mesh src(x_src,y_src)
+  Wonton::Direct_Product_Mesh src(x_src,y_src);
 
   // Create wrappers
   const Wonton::Direct_Product_Mesh_Wrapper tgt_wrapper(tgt);
   const Wonton::Direct_Product_Mesh_Wrapper src_wrapper(src);
 
   // Declare search
-  Portage::SearchDirectProduct<Wonton::Direct_Product_Mesh_Wrapper,
+  Portage::SearchDirectProduct<D, Wonton::Direct_Product_Mesh_Wrapper,
     Wonton::Direct_Product_Mesh_Wrapper> search(src_wrapper, tgt_wrapper);
 
   // Verify overlaps
   for (int i = 0; i < x_tgt.size() - 1; ++i) {
     for (int j = 0; j < y_tgt.size() - 1; ++j) {
-      IntPoint<D> indices = {i,j};
-      CellID id = tgt_wrapper.indices_to_cellid<D>(indices);
-      std::vector<CellID> candidates;
-      search(id, &candidates);
-      n = 0;
+      Wonton::IntPoint<D> indices = {i,j};
+      Wonton::CellID id = tgt_wrapper.indices_to_cellid<D>(indices);
+      const std::vector<Wonton::CellID> candidates = search(id);
+      int n = 0;
       for (int i2 = i; i2 < i+2; ++i2) {
         for (int j2 = j; j2 < j+2; ++j2) {
-          IntPoint<D> candidate = {i2,j2};
-          CellID c_id = src_wrapper.indices_to_cellid<D>(candidate);
+          Wonton::IntPoint<D> candidate = {i2,j2};
+          Wonton::CellID c_id = src_wrapper.indices_to_cellid<D>(candidate);
           ASSERT_EQ(candidates[n], c_id);
           n++;
         }
