de9521567340f20d49f87523a080af472563809e
Brendan K. Krueger <bkkrueger@lanl.gov>
2019-05-10 16:56:13 -0600

    Modifying for new coordinate system representation.

--- portage/intersect/intersect_boxes.h
+++ portage/intersect/intersect_boxes.h
@@ -6,6 +6,8 @@
 #include <cmath>
 #include <vector>
 
+#include "wonton/support/CoordinateSystems.h"
+
 #include "portage/support/portage.h"
 #include "portage/support/Point.h"
 
@@ -13,10 +15,15 @@
 
 namespace Portage {
 
-///
-/// \class IntersectBoxes  Intersection algorithm for axis-aligned boxes
+//! \class IntersectBoxes  Intersection algorithm for axis-aligned boxes
+//!
+//! It is assumed that both boxes are axis-aligned and in the same geometry.
+//! For example, this will work for two axis-aligned cells in 2D axis-aligned
+//! cylindrical geometry, but it cannot intersect a 3D axis-aligned box in
+//! Cartesian coordinates with a 3D axis-aligned box in spherical coordinates.
 
-template <int D, typename SourceMeshType, typename TargetMeshType>
+template <int D, typename SourceMeshType, typename TargetMeshType,
+         class CoordSys = Wonton::DefaultCoordSys>
 class IntersectBoxes {
 
  public:
@@ -28,87 +35,101 @@ class IntersectBoxes {
   IntersectBoxes(const SourceMeshType& source_mesh,
                  const TargetMeshType& target_mesh)
       : sourceMeshWrapper(source_mesh),
-        targetMeshWrapper(target_mesh),
-        geometry(source_mesh.geometry()) {}
+        targetMeshWrapper(target_mesh) {}
 
-  /// Assignment operator (disabled)
+  //! Assignment operator (disabled)
   IntersectBoxes & operator = (const IntersectBoxes&) = delete;
 
-  /// \brief Intersect control volume of a target entity with control volumes of a set of source entities
-  /// \param[in] tgt_entity  Entity of target mesh to intersect
-  /// \param[in] src_entities Entities of source mesh to intersect against
-  /// \return vector of Weights_t structure containing moments of intersection
-  std::vector<Portage::Weights_t>
-  operator() (const int tgt_cell,
-              const std::vector<int>& src_cells) const;
+  //! \brief Intersect control volume of a target box with control volumes
+  //!        of a set of source boxes.
+  //! \param[in] tgt_cell  Entity of target mesh to intersect
+  //! \param[in] src_cells Entities of source mesh to intersect against
+  //! \return vector of Weights_t structure containing moments of intersection
+  std::vector<Portage::Weights_t> operator() (
+      const int tgt_cell, const std::vector<int>& src_cells) const;
 
  private:
+
+  //! Wrapper for the source mesh
   const SourceMeshType& sourceMeshWrapper;
+
+  //! Wrapper for the target mesh
   const TargetMeshType& targetMeshWrapper;
-  const Geometry geometry;
 
 };  // class IntersectBoxes
 
 
-template <int D, typename SourceMeshType, typename TargetMeshType>
+// ============================================================================
+// Callable operator
+template <int D, typename SourceMeshType, typename TargetMeshType,
+         class CoordSys>
 std::vector<Portage::Weights_t>
-IntersectBoxes<D, SourceMeshType, TargetMeshType>::operator() (
-    const int tgt_cell,
-    const std::vector<int>& src_cells) const {
+  IntersectBoxes<D, SourceMeshType, TargetMeshType, CoordSys>::operator() (
+  const int tgt_cell, const std::vector<int>& src_cells) const {
 
+  // Get bounding box for target box
   Portage::Point<D> tlo, thi;
   targetMeshWrapper.cell_get_bounds(tgt_cell, &tlo, &thi);
 
+  // Allocate storage for moments
   int nsrc = src_cells.size();
   std::vector<Portage::Weights_t> sources_and_weights(nsrc);
+
+  // Loop over source boxes and compute moments
   int ninserted = 0;
   for (int i = 0; i < nsrc; ++i) {
     int s = src_cells[i];
 
+    // Get source cell bounding box
     Portage::Point<D> slo, shi;
     sourceMeshWrapper.cell_get_bounds(s, &slo, &shi);
 
-    // compute intersection and volume
-    Portage::Point<D> ilo, ihi;
-    double vol = 1.;
+    // Compute intersection and volume
+    Portage::Point<D> ilo, ihi;   // bounding box of intersection
+    double vol0 = 1.;
     for (int d = 0; d < D; ++d) {
       ilo[d] = std::max(slo[d], tlo[d]);
       ihi[d] = std::min(shi[d], thi[d]);
       double delta = std::max(ihi[d] - ilo[d], 0.);
-      vol *= delta;
+      vol0 *= delta;
     }
 
-    if (vol <= 0.) continue;
+    // If the intersection volume is zero, don't bother computing the moments
+    // because they will also be zero, and don't bother inserting into the list
+    // of actually intersecting boxes.
+    if (vol0 <= 0.) continue;
 
+    // At this point the two boxes actually intersect and we need to compute
+    // moments and insert into the list.
+
+    // Save the source cell ID
     auto & this_wt = sources_and_weights[ninserted];
     this_wt.entityID = s;
 
-    // compute weights (volume + 1 moment for each dimension)
+    // Compute and save weights (volume + 1 moment for each dimension)
     auto & weights = this_wt.weights;
     weights.resize(1 + D);
     switch (geometry) {
-      case Geometry::CARTESIAN:
-        weights[0] = vol;
-        for (int d = 0; d < D; ++d) {
-          weights[1+d] = vol * 0.5 * (ilo[d] + ihi[d]);
-        }
-        break;
-      case Geometry::CYLINDRICAL:
-        double xsum = ilo[0] + ihi[0];
-        double volrz = vol * 0.5 * xsum;
-        weights[0] = volrz;
-        weights[1] = vol * (1./3.) * (xsum * xsum - ilo[0] * ihi[0]);
-        for (int d = 1; d < D; ++d) {
-          weights[1+d] = volrz * 0.5 * (ilo[d] + ihi[d]);
-        }
-      break;
+      weights[0] = CoordSys::modify_volume(vol0, ilo, ihi);
+      Wonton::Point<D> mom0;
+      for (int d = 0; d < D; ++d) {
+        const ibar = 0.5 * (ilo[d] + ihi[d]);
+        mom0[d] = ibar * vol0;
+      }
+      auto first_moments = CoordSys::compute_moments(mom0, ilo, ihi);
+      for (int d = 0; d < D; ++d) {
+        weights[1+d] = first_moments[d];
+      }
     }
 
+    // Increment the count, because we've now inserted a new entry
     ++ninserted;
 
   }  // for i
 
+  // Not all source cells provided necessary actually intersect.
   sources_and_weights.resize(ninserted);
+
   return sources_and_weights;
 
 }  // operator()
