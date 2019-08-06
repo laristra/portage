ce254898b8578be8540bc4493b9da26a7a0eee0b
Rao Garimella <rao@lanl.gov>
2019-08-06 17:13:18 -0600

    generic templated struct for uniform invocation of interpolators of different order

--- portage/interpolate/interpolate_nth_order.h
+++ portage/interpolate/interpolate_nth_order.h
@@ -5,14 +5,25 @@ Please see the license file at the root of this repository, or at:
 */
 
 
-#ifndef PORTAGE_INTERSECT_INTERSECT_RND_H_
-#define PORTAGE_INTERSECT_INTERSECT_RND_H_
+#ifndef PORTAGE_INTERPOLATE_INTERPOLATE_RND_H_
+#define PORTAGE_INTERPOLATE_INTERPOLATE_RND_H_
 
 #include "portage/support/portage.h"
-#include "portage/intersect/intersect_r2d.h"
-#include "portage/intersect/intersect_r3d.h"
+#include "portage/interpolate/interpolate_1st_order.h"
+#include "portage/interpolate/interpolate_2nd_order.h"
 
 namespace Portage {
+
+// A struct templated on interpolation order so that interpolate
+// classes can be referred to in a uniform manner
+//
+// Use as
+// Portage::Interpolate_NthOrder<O>::Interpolate<blah,blah...>
+// where O is a compile time parameter
+//
+// If Interpolate becomes a dependent name, one may have to prefix it
+// with the keyword 'template', like so
+// Portage::Interpolate_NthOrder<D>::template Interpolate
 
 template <int order> struct Interpolate_NthOrder;
 
@@ -22,14 +33,15 @@ template <> struct Interpolate_NthOrder<1> {
            class SourceMeshType,
            class TargetMeshType,
            class SourceStateType,
-           template<class, int, class, class> class InterfaceReconstructorType,
-           class MatPoly_Splitter,
-           class MatPoly_Clipper,
-           class CoordSys>
+           template<class, int, class, class> class InterfaceReconstructorType =
+           DummyInterfaceReconstructor,
+           class MatPoly_Splitter = void,
+           class MatPoly_Clipper = void,
+           class CoordSys = Wonton::DefaultCoordSys>
   using Interpolate = Portage::Interpolate_1stOrder<D, ONWHAT,
                                                     SourceMeshType,
-                                                    SourceStateType,
                                                     TargetMeshType,
+                                                    SourceStateType,
                                                     InterfaceReconstructorType,
                                                     MatPoly_Splitter,
                                                     MatPoly_Clipper,
@@ -42,11 +54,12 @@ template <> struct Interpolate_NthOrder<2> {
            class SourceMeshType,
            class TargetMeshType,
            class SourceStateType,
-           template<class, int, class, class> class InterfaceReconstructorType,
-           class MatPoly_Splitter,
-           class MatPoly_Clipper,
-           class CoordSys>
-  using Interpolate = Portage::Interpolate_2ndOrder<ONWHAT,
+           template<class, int, class, class> class InterfaceReconstructorType =
+           DummyInterfaceReconstructor,
+           class MatPoly_Splitter = void,
+           class MatPoly_Clipper = void,
+           class CoordSys = Wonton::DefaultCoordSys>
+  using Interpolate = Portage::Interpolate_2ndOrder<D, ONWHAT,
                                                     SourceMeshType,
                                                     TargetMeshType,
                                                     SourceStateType,
