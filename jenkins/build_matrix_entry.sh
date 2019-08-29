92de4f8ccd160e6d4f73bc967102cc994ed00a65
Eugene Kikinzon <kikinzon@lanl.gov>
2019-08-29 12:46:43 -0600

    Make Jenkins distinguish between Tangram with and without Thrust

--- jenkins/build_matrix_entry.sh
+++ jenkins/build_matrix_entry.sh
@@ -68,8 +68,13 @@ elif [[ $compiler == "gcc7" ]]; then
 fi
 
 jali_install_dir=$NGC/private/jali/${jali_version}-${compiler_type}-${compiler_version}-openmpi-${openmpi_version}
-tangram_install_dir=$NGC/private/tangram/${tangram_version}-${compiler_type}-${compiler_version}-openmpi-${openmpi_version}
-tangram_install_dir_nompi=$NGC/private/tangram/${tangram_version}-${compiler_type}-${gcc_version}-nompi
+if [[ $build_type == "thrust" ]]; then
+  tangram_install_dir=$NGC/private/tangram/${tangram_version}-thrust-${compiler_type}-${compiler_version}-openmpi-${openmpi_version}
+  tangram_install_dir_nompi=$NGC/private/tangram/${tangram_version}-thrust-${compiler_type}-${gcc_version}-nompi
+else
+  tangram_install_dir=$NGC/private/tangram/${tangram_version}-boost-${compiler_type}-${compiler_version}-openmpi-${openmpi_version}
+  tangram_install_dir_nompi=$NGC/private/tangram/${tangram_version}-boost-${compiler_type}-${gcc_version}-nompi
+fi
 xmof2d_install_dir=$NGC/private/xmof2d/${xmof2d_version}-${compiler_type}-${compiler_version}
 lapacke_dir=$NGC/private/lapack/${lapack_version}-patched-${compiler_type}-${compiler_version}
 
