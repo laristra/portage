d1b5b90bf6744396a700857d80e4b7f743f622f1
Hoby Rakotoarivelo <hoby@lanl.gov>
2019-10-30 12:28:19 -0600

    Add skeleton for tests

---
+++ portage/intersect/test/test_intersect_swept_faces.cc
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the Ristra portage project.
+ * Please see the license file at the root of this repository, or at:
+ * https://github.com/laristra/portage/blob/master/LICENSE
+ */
+
+#include "gtest/gtest.h"
+#include "portage/intersect/intersect_swept_2d.h"
+
+
