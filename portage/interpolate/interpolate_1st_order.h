8f74c95b4b43ff0c840152bf13e82e61e2afb007
Hoby Rakotoarivelo <hoby@lanl.gov>
2019-11-27 12:27:03 -0700

    Fix missing namespace

--- portage/interpolate/interpolate_1st_order.h
+++ portage/interpolate/interpolate_1st_order.h
@@ -173,7 +173,7 @@ class Interpolate_1stOrder {
   void set_interpolation_variable(std::string const & interp_var_name,
                                   Limiter_type limtype=NOLIMITER,
                                   Boundary_Limiter_type bnd_limtype=BND_NOLIMITER,
-                                  const Portage::vector<Vector<D>>* gradients = nullptr) {
+                                  const Portage::vector<Wonton::Vector<D>>* gradients = nullptr) {
     interp_var_name_ = interp_var_name;
     field_type_ = source_state_.field_type(Entity_kind::CELL, interp_var_name);
     if (field_type_ == Field_type::MESH_FIELD)
@@ -331,7 +331,7 @@ class Interpolate_1stOrder<
   void set_interpolation_variable(std::string const & interp_var_name,
                                   Limiter_type limtype = NOLIMITER,
                                   Boundary_Limiter_type bnd_limtype=BND_NOLIMITER,
-                                  const Portage::vector<Vector<D>>* gradients = nullptr) {
+                                  const Portage::vector<Wonton::Vector<D>>* gradients = nullptr) {
     interp_var_name_ = interp_var_name;
     field_type_ = source_state_.field_type(Entity_kind::CELL, interp_var_name);
     if (field_type_ == Field_type::MESH_FIELD)
@@ -528,7 +528,7 @@ class Interpolate_1stOrder<
   void set_interpolation_variable(std::string const & interp_var_name,
                                   Limiter_type limtype = NOLIMITER,
                                   Boundary_Limiter_type bnd_limtype=BND_NOLIMITER,
-                                  const Portage::vector<Vector<D>>* gradients = nullptr) {
+                                  const Portage::vector<Wonton::Vector<D>>* gradients = nullptr) {
     interp_var_name_ = interp_var_name;
     field_type_ = source_state_.field_type(Entity_kind::NODE, interp_var_name);
     if (field_type_ == Field_type::MESH_FIELD)
