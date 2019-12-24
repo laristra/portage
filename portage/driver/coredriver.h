1e7c2342153cdb7902a0a92a8874d96561f1ecbe
Rao Garimella <rao@lanl.gov>
2019-12-24 10:22:06 -0700

    account for cases where the material centroids are not provided because VOF is being used for interface reconstruction

--- portage/driver/coredriver.h
+++ portage/driver/coredriver.h
@@ -1058,6 +1058,7 @@ class CoreDriver : public CoreDriverBase<D,
     std::vector<double> cell_mat_volfracs_full(nsourcecells*nmats, 0.0);
     std::vector<Wonton::Point<D>> cell_mat_centroids_full(nsourcecells*nmats);
 
+    bool have_centroids = true;
     int nvals = 0;
     for (int m = 0; m < nmats; m++) {
       std::vector<int> cellids;
@@ -1077,8 +1078,11 @@ class CoreDriver : public CoreDriverBase<D,
 
       Wonton::Point<D> const *matcenvec;
       source_state_.mat_get_celldata("mat_centroids", m, &matcenvec);
-      for (int ic = 0; ic < cellids.size(); ic++)
-        cell_mat_centroids_full[cellids[ic]*nmats+m] = matcenvec[ic];
+      if (cellids.size() && !matcenvec)
+        have_centroids = false;  // VOF
+      else
+        for (int ic = 0; ic < cellids.size(); ic++)
+          cell_mat_centroids_full[cellids[ic]*nmats+m] = matcenvec[ic];
     }
 
     // At this point nvals contains the number of non-zero volume
@@ -1088,7 +1092,7 @@ class CoreDriver : public CoreDriverBase<D,
 
     cell_mat_ids.resize(nvals);
     cell_mat_volfracs.resize(nvals);
-    cell_mat_centroids.resize(nvals);
+    cell_mat_centroids.resize(nvals);  // dummy vals for VOF
 
     int idx = 0;
     for (int c = 0; c < nsourcecells; c++) {
@@ -1096,7 +1100,8 @@ class CoreDriver : public CoreDriverBase<D,
         int matid = cell_mat_ids_full[c*nmats+m];
         cell_mat_ids[idx] = matid;
         cell_mat_volfracs[idx] = cell_mat_volfracs_full[c*nmats+matid];
-        cell_mat_centroids[idx] = cell_mat_centroids_full[c*nmats+matid];
+        if (have_centroids)
+          cell_mat_centroids[idx] = cell_mat_centroids_full[c*nmats+matid];
         idx++;
       }
     }
--- portage/driver/mmdriver.h
+++ portage/driver/mmdriver.h
@@ -783,6 +783,7 @@ class MMDriver {
     std::vector<double> cell_mat_volfracs_full(nsourcecells*nmats, 0.0);
     std::vector<Tangram::Point<D>> cell_mat_centroids_full(nsourcecells*nmats);
 
+    int have_centroids = true;
     int nvals = 0;
     for (int m = 0; m < nmats; m++) {
       std::vector<int> cellids;
@@ -802,8 +803,11 @@ class MMDriver {
 
       Portage::Point<D> const *matcenvec;
       source_state.mat_get_celldata("mat_centroids", m, &matcenvec);
-      for (int ic = 0; ic < cellids.size(); ic++)
-        cell_mat_centroids_full[cellids[ic]*nmats+m] = matcenvec[ic];
+      if (cellids.size() && !matcenvec)
+        have_centroids = false;
+      else
+        for (int ic = 0; ic < cellids.size(); ic++)
+          cell_mat_centroids_full[cellids[ic]*nmats+m] = matcenvec[ic];
     }
 
     // At this point nvals contains the number of non-zero volume
@@ -813,7 +817,7 @@ class MMDriver {
 
     cell_mat_ids.resize(nvals);
     cell_mat_volfracs.resize(nvals);
-    cell_mat_centroids.resize(nvals);
+    cell_mat_centroids.resize(nvals);  // dummy vals for VOF
 
     int idx = 0;
     for (int c = 0; c < nsourcecells; c++) {
@@ -821,7 +825,8 @@ class MMDriver {
         int matid = cell_mat_ids_full[c*nmats+m];
         cell_mat_ids[idx] = matid;
         cell_mat_volfracs[idx] = cell_mat_volfracs_full[c*nmats+matid];
-        cell_mat_centroids[idx] = cell_mat_centroids_full[c*nmats+matid];
+        if (have_centroids)
+          cell_mat_centroids[idx] = cell_mat_centroids_full[c*nmats+matid];
         idx++;
       }
     }
