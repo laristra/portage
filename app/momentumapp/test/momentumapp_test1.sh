4620f8efc3e546072ae31083d0ff3cb83ed857d7
Konstantin Lipnikov <lipnikov@lanl.gov>
2019-12-24 14:45:16 -0700

    Forgot to update one test after switching to input of math expressions.

--- app/momentumapp/test/momentumapp_test1.sh
+++ app/momentumapp/test/momentumapp_test1.sh
@@ -12,7 +12,7 @@ set -e
 set -x
 
 # 2nd order momentum CCH remapwil limiters of quadratic density and velocity
-${RUN_COMMAND} $TESTAPPDIR/momentumapp 8 6 2 1 2 2
+${RUN_COMMAND} $TESTAPPDIR/momentumapp 8 6 2 1 "1 + x + x * y" "x * x" "2 * y * y"
 
 # Compare the values for the field
 $CMPAPPDIR/apptest_cmp errors_gold1.txt errors1.txt 1e-10 1e-4
