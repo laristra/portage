51696a5cdc4c15c9075bf8a5f49099d06f42219c
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-01-21 15:55:15 -0700

    Disable displacement check for now

--- portage/intersect/intersect_swept_face.h
+++ portage/intersect/intersect_swept_face.h
@@ -542,13 +542,14 @@ namespace Portage {
             }
             // sanity check: ensure that swept face centroid remains
             // inside the neighbor cell.
-            else if (not centroid_inside_cell(neigh, moments)) {
-              throw std::runtime_error("invalid target mesh for swept face");
-            }
-            // append to list as current neighbor moment.
-            else {
-              swept_moments.emplace_back(neigh, moments);
-            }
+//            else if (not centroid_inside_cell(neigh, moments)) {
+//              throw std::runtime_error("invalid target mesh for swept face");
+//            }
+//            // append to list as current neighbor moment.
+//            else {
+//              swept_moments.emplace_back(neigh, moments);
+//            }
+            swept_moments.emplace_back(neigh, moments);
           }
         } // end for each edge of current cell
 
@@ -987,10 +988,12 @@ namespace Portage {
           }
         } // end of for each face of current cell
 
-        if (valid_displacement(source_id, swept_moments)) {
-          return swept_moments;
-        } else
-          throw std::runtime_error("invalid displacement");
+        return swept_moments;
+
+//        if (valid_displacement(source_id, swept_moments)) {
+//          return swept_moments;
+//        } else
+//          throw std::runtime_error("invalid displacement");
 
 #ifdef HAVE_TANGRAM
       } else /* multi-material case */ {
