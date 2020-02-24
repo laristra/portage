433f156973ba8d7d0b84b2b1da1cd5c59c31bc9a
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-02-24 10:40:10 -0700

    Update scripts for distributed compare tests

--- app/swept_face/test/swept_face_demo.sh
+++ app/swept_face/test/swept_face_demo.sh
@@ -12,10 +12,10 @@ END
 set -x
 
 # This should probably be changed at some time
-# In particular $6 is a field expression and can contain non alphanumeric
+# In particular $4 is a field expression and can contain non alphanumeric
 # characters that can be a problem for filenames, but at the moment it works
 # and avoids file clobbers in a human readable way as opposed to hashes.
-FILENAME=field_$1_$2_$3_$4_$5.txt
+FILENAME=field_$1_$2_$3_$4_$5_$6.txt
 
 TOLERANCE_2D=5.e-11
 TOLERANCE_3D=5.e-9
@@ -23,20 +23,24 @@ TOLERANCE_3D=5.e-9
 rm -f ${FILENAME}*
 
 # SERIAL RUN
-mpirun -np 1 $TESTAPPDIR/swept_face_singlemat \
+mpirun -np 1 $TESTAPPDIR/swept_face_demo \
   --dim=$2 \
   --ncells=$3 \
-  --field_expression=$4 \
+  --field=$4 \
   --remap_order=$5 \
-  --field_filename=${FILENAME}
+  --field_filename=${FILENAME} \
+  --simple=$6 \
+  --ntimesteps=1
  
 # PARALLEL RUN
-mpirun -np $1 $TESTAPPDIR/swept_face_singlemat \
+mpirun -np $1 $TESTAPPDIR/swept_face_demo \
   --dim=$2 \
   --ncells=$3 \
   --field_expression=$4 \
   --remap_order=$5 \
-  --field_filename=${FILENAME}
+  --field_filename=${FILENAME} \
+  --simple=$6 \
+  --ntimesteps=1
 
 # COMPARE  
 if [[ "$2" -eq "2" ]]
