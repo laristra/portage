f3bb22ef98bb8a27f2b14a08dcb2846d744d293b
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-02-24 11:28:50 -0700

    Fix application argument in distributed test

--- app/swept_face/test/swept_face_demo.sh
+++ app/swept_face/test/swept_face_demo.sh
@@ -15,12 +15,11 @@ set -x
 # In particular $4 is a field expression and can contain non alphanumeric
 # characters that can be a problem for filenames, but at the moment it works
 # and avoids file clobbers in a human readable way as opposed to hashes.
-FILENAME=field_$1_$2_$3_$4_$5_$6.txt
-
+FILE="field_rank_$1_dim_$2_cells_$3_field_$4_order_$5"
 TOLERANCE_2D=5.e-11
 TOLERANCE_3D=5.e-9
 
-rm -f ${FILENAME}*
+rm -f ${FILE}*
 
 # SERIAL RUN
 mpirun -np 1 $TESTAPPDIR/swept_face_demo \
@@ -28,27 +27,32 @@ mpirun -np 1 $TESTAPPDIR/swept_face_demo \
   --ncells=$3 \
   --field=$4 \
   --remap_order=$5 \
-  --field_filename=${FILENAME} \
-  --simple=$6 \
-  --ntimesteps=1
- 
+  --ntimesteps=4 \
+  --scale_by=10 \
+  --output_meshes=false \
+  --result_file="${FILE}" \
+  --keep_source=false \
+  --simple=true
+
 # PARALLEL RUN
 mpirun -np $1 $TESTAPPDIR/swept_face_demo \
   --dim=$2 \
   --ncells=$3 \
-  --field_expression=$4 \
+  --field=$4 \
   --remap_order=$5 \
-  --field_filename=${FILENAME} \
-  --simple=$6 \
-  --ntimesteps=1
+  --ntimesteps=4 \
+  --scale_by=10 \
+  --output_meshes=false \
+  --result_file="${FILE}" \
+  --keep_source=false \
+  --simple=true
 
 # COMPARE  
-if [[ "$2" -eq "2" ]]
-then
+if [[ "$2" -eq "2" ]]; then
   TOLERANCE=${TOLERANCE_2D}
 else 
   TOLERANCE=${TOLERANCE_3D}
 fi
 
-$CMPAPPDIR/distributed_cmp ${FILENAME} ${TOLERANCE}
+$CMPAPPDIR/distributed_cmp ${FILE} ${TOLERANCE}
 
