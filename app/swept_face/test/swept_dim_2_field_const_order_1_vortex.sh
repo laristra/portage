b33cd3c463895248d0693025ba2ded3591e31bdb
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-02-24 11:28:07 -0700

    Add test bash scripts

---
+++ app/swept_face/test/swept_dim_2_field_const_order_1_vortex.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+: <<'END'
+This file is part of the Ristra portage project.
+Please see the license file at the root of this repository, or at:
+    https://github.com/laristra/portage/blob/master/LICENSE
+END
+
+set -e
+set -x
+
+${RUN_COMMAND} $TESTAPPDIR/swept_face_demo \
+  --dim=2 \
+  --ncells=10 \
+  --remap_order=1 \
+  --field="42" \
+  --ntimesteps=4 \
+  --scale_by=10 \
+  --output_meshes=false \
+  --result_file="swept_dim_2_field_const_order_1_vortex" \
+  --keep_source=false \
+  --simple=false
