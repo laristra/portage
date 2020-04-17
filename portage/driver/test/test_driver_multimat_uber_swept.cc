b4c6d3d35be93fa83ad3489d7fa433a1c0ecb603
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-04-17 09:00:37 -0600

    Fix last unused variable

--- portage/driver/test/test_driver_multimat_uber_swept.cc
+++ portage/driver/test/test_driver_multimat_uber_swept.cc
@@ -119,9 +119,6 @@ TEST(UberDriverSwept, ThreeMat2D_1stOrder) {
   double matrho[nmats] = {0.1, 10.0, 100.0};  // material density
   double matvol[nmats] = {0.5, 0.25, 0.25};
   double matmass[nmats] = {0.05, 2.5, 25.0};
-  Wonton::Point<2> matcen[nmats] = {Wonton::Point<2>(0.25,0.5),
-                                    Wonton::Point<2>(0.75,0.25),
-                                    Wonton::Point<2>(0.75,0.75)};
 
   std::vector<int> matcells_src[nmats];
   std::vector<double> matvf_src[nmats];
