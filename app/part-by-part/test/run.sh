662809d2d5be2742fa6e7b019ca44defb56dec23
Hoby Rakotoarivelo <hoby@lanl.gov>
2020-08-28 12:23:40 -0600

    Add generic script for all test instances

---
+++ app/part-by-part/test/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This file is part of the Ristra portage project.
+# Please see the license file at the root of this repository, or at:
+# https://github.com/laristra/portage/blob/master/LICENSE
+
+set -e
+set -x
+
+suffix=${1}
+epsilon=1.e-10
+
+# run test
+mpirun -np 4 ./part-remap "input_${suffix}.json"
+
+# compare values with related gold file
+${COMPARE} \
+  "gold_${suffix}_density.dat" \
+  "data_${suffix}_density.dat" \
+  ${epsilon}
+
+${COMPARE} \
+  "gold_${suffix}_temperature.dat" \
+  "data_${suffix}_temperature.dat" \
+  ${epsilon}
