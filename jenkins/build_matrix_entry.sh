e71f6e70cfe5f26bc1b752b2119ccc2f9cabd52a
Rao Garimella <rao@abyzou.lanl.gov>
2020-10-15 12:24:11 -0600

    rework to use new Jenkins job configurations

--- jenkins/build_matrix_entry.sh
+++ jenkins/build_matrix_entry.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 # This script is executed on Jenkins using
 #
-#     $WORKSPACE/jenkins/build_matrix_entry.sh <compiler> <build_type>
+#     $WORKSPACE/jenkins/build_matrix_entry_varan.sh BUILD_TYPE <VER> <WONTON_VER>
+#
+# BUILD_TYPE  -  pr, nightly, install
+#
+# if VER is abset, the HEAD of the master branch will be taken. If
+# WONTON_VER is absent, the HEAD of the master branch of wonton will
+# be taken. If BUILD_TYPE is 'install' and VER is specified, it will
+# install it to /install_prefix/tangram/$VER-blah-blah; if VER is not
+# specified, it will install to /install_prefix/wonton/dev-blah-blah
+#
+# Note that the following environment variables must be set (Jenkins
+# will do this automatically).
+#
+# WORKSPACE   -  where the code is checked out
+# CONFIG_TYPE -  base, debug, serial, readme, thrust, kokkos
+# COMPILER    -  intel, gcc6, gcc7
+# BRANCH_NAME -  master, kokkos
 #
 # The exit code determines if the test succeeded or failed.
-# Note that the environment variable WORKSPACE must be set (Jenkins
-# will do this automatically).
 
 # Exit on error
 set -e
@@ -15,11 +29,24 @@ set -x
 # set umask so installations will have group rwx permission
 umask 007
 
-compiler=$1
-build_type=$2
+BUILD_TYPE=$1
+version=$2
+if [[ $version == "" ]]; then
+    version=dev
+fi
+tangram_version=$3
+if [[ $tangram_version == "" ]]; then
+    tangram_version=dev
+fi
+wonton_version=$3
+if [[ $wonton_version == "" ]]; then
+    wonton_version=dev
+fi
+
+echo "inside build_matrix on PLATFORM=$PLATFORM with BUILD_TYPE=$BUILD_TYPE $CONFIG_TYPE=$CONFIG_TYPE COMPILER=$COMPILER"
 
 # special case for README builds
-if [[ $build_type == "readme" ]]; then
+if [[ $BUILD_TYPE != "install" && $CONFIG_TYPE == "readme" ]]; then
   # Put a couple of settings in place to generate test output even if
   # the README doesn't ask for it.
   export CTEST_OUTPUT_ON_FAILURE=1
@@ -34,16 +61,12 @@ fi
 
 # set modules and install paths
 
-wonton_version=1.2.6
-tangram_version=1.0.1
- 
-
 export NGC=/usr/local/codes/ngc
 ngc_include_dir=$NGC/private/include
-ngc_tpl_dir=$NGC/private
+
 
 # compiler-specific settings
-if [[ $compiler == "intel18" ]]; then
+if [[ $COMPILER =~ "intel" ]]; then
 
     compiler_version=18.0.1
     cxxmodule=intel/${compiler_version}
@@ -53,14 +76,14 @@ if [[ $compiler == "intel18" ]]; then
     mpi_module=openmpi/${openmpi_version}
     mpi_suffix="-openmpi-${openmpi_version}"
 
-elif [[ $compiler =~ "gcc" ]]; then
+elif [[ $COMPILER =~ "gcc" ]]; then
 
     openmpi_version=2.1.2
-    if [[ $compiler == "gcc6" ]]; then
+    if [[ $COMPILER == "gcc6" ]]; then
 	compiler_version=6.4.0
-    elif [[ $compiler == "gcc7" ]]; then
+    elif [[ $COMPILER == "gcc7" ]]; then
 	compiler_version=7.3.0
-    elif [[ $compiler == "gcc8" ]]; then
+    elif [[ $COMPILER == "gcc8" ]]; then
 	compiler_version=8.2.0
 	openmpi_version=3.1.3
     fi
@@ -74,46 +97,51 @@ elif [[ $compiler =~ "gcc" ]]; then
 fi
 
 
-mpi_flags="-D PORTAGE_ENABLE_MPI=True"
-if [[ $build_type == "serial" ]]; then
-    mpi_flags=""
-    mpi_suffix=""
-fi
+# Jali
+jali_flags="-D PORTAGE_ENABLE_Jali::BOOL=True"
 
-cmake_build_type=Release
-if [[ $build_type == "debug" ]]; then
-    cmake_build_type=Debug
-fi
+# FleCSI
+flecsi_flags="-D PORTAGE_ENABLE_FleCSI:BOOL=False"  # Not building with FleCSI for HPC builds
 
-thrust_flags=""
-thrust_suffix=""
-if [[ $build_type == "thrust" ]]; then
+# THRUST
+thrust_flags=
+thrust_suffix=
+if [[ $CONFIG_TYPE == "thrust" ]]; then
     thrust_flags="-D PORTAGE_ENABLE_THRUST=True"
     thrust_suffix="-thrust"
 fi
 
-cov_flags=""
-if [[ $build_type == "coverage" ]]; then
-    cov_flags="-D CMAKE_C_FLAGS='-coverage' -D CMAKE_CXX_FLAGS='-coverage'"
+# MPI or not
+mpi_flags="-D PORTAGE_ENABLE_MPI=True"
+if [[ $CONFIG_TYPE == "serial" ]]; then
+    mpi_flags="-D PORTAGE_ENABLE_MPI=False"
+    mpi_suffix=
+    jali_flags=
+    flecsi_flags=
+fi
+
+# Debug or Optimized build
+cmake_build_type=Release
+debug_suffix=
+if [[ $CONFIG_TYPE == "debug" ]]; then
     cmake_build_type=Debug
-    export PATH=$NGC/private/bin:${PATH}
+    debug_suffix="-debug"
 fi
 
+# WONTON
+wonton_install_dir=$NGC/private/wonton/${wonton_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+wonton_flags="-D WONTON_ROOT:FILEPATH=$wonton_install_dir"
 
-wonton_install_dir=${ngc_tpl_dir}/wonton/${wonton_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}
-wonton_flags="-D WONTON_ROOT:PATH=$wonton_install_dir"
+# TANGRAM
+tangram_install_dir=$NGC/private/tangram/${tangram_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+tangram_flags="-D PORTAGE_ENABLE_TANGRAM=True -D TANGRAM_ROOT:FILEPATH=$tangram_install_dir"
 
-tangram_flags=
-if [[ $build_type != singlemat ]]; then
-   tangram_install_dir=${ngc_tpl_dir}/tangram/${tangram_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}
-   tangram_flags="-D PORTAGE_ENABLE_TANGRAM=True -D TANGRAM_ROOT:PATH=$tangram_install_dir"
-fi
+# Build up an install dir name
+portage_install_dir=$NGC/private/portage/${version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+
 
 if [[ $compiler == "gcc6" && $build_type != "serial" ]]; then
   flecsi_flags="-D PORTAGE_ENABLE_FleCSI:BOOL=True" # FleCSI found through Wonton
-fi
-if [[ $build_type != "serial" ]]; then
-    jali_flags="-D PORTAGE_ENABLE_Jali:BOOL=True"  # Jali found through Wonton
 fi
 
 
@@ -151,7 +179,10 @@ cmake \
 make -j2
 ctest --output-on-failure
 
-if [[ $build_type == "coverage" ]]; then
+if [[ $CONFIG_TYPE == "coverage" ]]; then
   gcovr -r .. -x  >coverage.xml
 fi
 
+if [[ $BUILD_TYPE == "install" ]]; then
+    make install
+fi
--- jenkins/build_matrix_entry_hpc.sh
+++ jenkins/build_matrix_entry_hpc.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 # This script is executed on Jenkins using
 #
-#     $WORKSPACE/jenkins/build_matrix_entry_hpc.sh <compiler> <build_type>
+#     $WORKSPACE/jenkins/build_matrix_entry_hpc.sh BUILD_TYPE <VER> <WONTON_VER>
+#
+# BUILD_TYPE - pr, nightly, install
+#
+# if VER is abset, the HEAD of the master branch will be taken. If
+# WONTON_VER is absent, the HEAD of the master branch of wonton will
+# be taken. If BUILD_TYPE is 'install' and VER is specified, it will
+# install it to /install_prefix/tangram/$VER-blah-blah; if VER is not
+# specified, it will install to /install_prefix/wonton/dev-blah-blah
+#
+# WORKSPACE   -  where the code is checked out
+# CONFIG_TYPE -  base, debug, serial, readme, thrust, kokkos
+# COMPILER    -  intel, gcc6, gcc7
+# BRANCH_NAME -  master
 #
 # The exit code determines if the test succeeded or failed.
-# Note that the environment variable WORKSPACE must be set (Jenkins
-# will do this automatically).
+
 
 # Exit on error
 set -e
@@ -15,19 +27,29 @@ set -x
 # set umask so installations will have group rwx permission
 umask 007
 
-echo "--------------------------------------------------------------"
-echo "Running configuration $COMPILER $BUILD_TYPE on `hostname`"
-echo "--------------------------------------------------------------"
-
-compiler=$1
+BUILD_TYPE=$1
 build_type=$2
+version=$2
+if [[ $version == "" ]]; then
+    version=dev
+fi
+tangram_version=$3
+if [[ $tangram_version == "" ]]; then
+    tangram_version=dev
+fi
+wonton_version=$3
+if [[ $wonton_version == "" ]]; then
+    wonton_version=dev
+fi
+
+echo "inside build_matrix on PLATFORM=$PLATFORM with BUILD_TYPE=$BUILD_TYPE $CONFIG_TYPE=$CONFIG_TYPE COMPILER=$COMPILER"
 
 # special case for README builds
-if [[ $build_type == "readme" ]]; then
+if [[ $BUILD_TYPE != "install" && $CONFIG_TYPE == "readme" ]]; then
 
     # Put a couple of settings in place to generate test output even if
     # the README doesn't ask for it.
-    export CTEST_OUTPUT_ON_FAILURE=1
+    export CTEST_OUTPUT_ON_FAILURE=1 
     CACHE_OPTIONS="-D ENABLE_JENKINS_OUTPUT=True"
     sed "s/^ *cmake/& $CACHE_OPTIONS/g" $WORKSPACE/README.md >$WORKSPACE/README.md.1
     python2 $WORKSPACE/jenkins/parseREADME.py \
@@ -40,15 +62,12 @@ fi
 
 # set modules and install paths
 
-wonton_version=1.2.6
-tangram_version=1.0.1
-
 export NGC=/usr/projects/ngc
 ngc_include_dir=$NGC/private/include
-ngc_tpl_dir=$NGC/private
+
 
 # compiler-specific settings
-if [[ $compiler == "intel18" ]]; then
+if [[ $COMPILER =~ "intel" ]]; then
 
     compiler_version=18.0.5
     cxxmodule=intel/${compiler_version}
@@ -58,12 +77,12 @@ if [[ $compiler == "intel18" ]]; then
     mpi_module=openmpi/${openmpi_version}
     mpi_suffix="-openmpi-${openmpi_version}"
     
-elif [[ $compiler =~ "gcc" ]]; then
+elif [[ $COMPILER =~ "gcc" ]]; then
 
     openmpi_version=2.1.2
-    if [[ $compiler == "gcc6" ]]; then
+    if [[ $COMPILER == "gcc6" ]]; then
 	compiler_version=6.4.0
-    elif [[ $compiler == "gcc7" ]]; then
+    elif [[ $COMPILER == "gcc7" ]]; then
 	compiler_version=7.4.0
     fi
     
@@ -75,24 +94,50 @@ elif [[ $compiler =~ "gcc" ]]; then
 
 fi
 
-mpi_flags="-D PORTAGE_ENABLE_MPI=True"
-if [[ $build_type == "serial" ]]; then
-    mpi_flags=
-    mpi_suffix=
-fi
+# Jali
+jali_flags="-D PORTAGE_ENABLE_Jali::BOOL=True"
 
-cmake_build_type=Release
-if [[ $build_type == "debug" ]]; then
-    cmake_build_type=Debug
-fi
+# FleCSI
+flecsi_flags="-D PORTAGE_ENABLE_FleCSI:BOOL=False"  # Not building with FleCSI for HPC builds
 
+# THRUST
 thrust_flags=
 thrust_suffix=
-if [[ $build_type == "thrust" ]]; then
+if [[ $CONFIG_TYPE == "thrust" ]]; then
     thrust_flags="-D PORTAGE_ENABLE_THRUST=True"
     thrust_suffix="-thrust"
 fi
 
+# MPI or not
+mpi_flags="-D PORTAGE_ENABLE_MPI=True"
+if [[ $CONFIG_TYPE == "serial" ]]; then
+    mpi_flags="-D PORTAGE_ENABLE_MPI=False"
+    mpi_suffix=
+    jali_flags=
+    flecsi_flags=
+fi
+
+# Debug or Optimized build
+cmake_build_type=Release
+debug_suffix=
+if [[ $CONFIG_TYPE == "debug" ]]; then
+    cmake_build_type=Debug
+    debug_suffix="-debug"
+fi
+
+# WONTON
+wonton_install_dir=$NGC/private/wonton/${wonton_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+wonton_flags="-D WONTON_ROOT:FILEPATH=$wonton_install_dir"
+
+# TANGRAM
+tangram_install_dir=$NGC/private/tangram/${tangram_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+tangram_flags="-D PORTAGE_ENABLE_TANGRAM=True -D TANGRAM_ROOT:FILEPATH=$tangram_install_dir"
+
+# Build up an install dir name
+portage_install_dir=$NGC/private/portage/${version}${compiler_suffix}${mpi_suffix}${thrust_suffix}${kokkos_suffix}${debug_suffix}
+
+
+# Coverage
 cov_flags=
 if [[ $build_type == "coverage" ]]; then
     cov_flags="-D CMAKE_C_FLAGS='-coverage' -D CMAKE_CXX_FLAGS='-coverage' -D CMAKE_EXE_LINKER_FLAGS=-coverage"
@@ -101,32 +146,17 @@ if [[ $build_type == "coverage" ]]; then
 fi
 
 
-wonton_install_dir=${ngc_tpl_dir}/wonton/${wonton_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}
-wonton_flags="-D WONTON_ROOT:PATH=$wonton_install_dir"
-
-tangram_install_dir=${ngc_tpl_dir}/tangram/${tangram_version}${compiler_suffix}${mpi_suffix}${thrust_suffix}
-tangram_flags="-D PORTAGE_ENABLE_TANGRAM=True -D TANGRAM_ROOT:PATH=$tangram_install_dir"
-
-flecsi_flags="-D PORTAGE_ENABLE_FleCSI:BOOL=False"  # Not building with FleCSI for HPC builds
-
-if [[ $build_type != "serial" ]]; then
-    jali_flags="-D PORTAGE_ENABLE_Jali:BOOL=True"  # Jali found through Wonton
+#Rely on default user environment to load modules; these scripts can be found in /etc/profile.d/ as of 8/25/20 the scripts that set up modules on snow are  /etc/profile.d/z00_lmod.sh; /etc/profile.d/00-modulepath.sh; /etc/profile.d/z01-modules.lanl.sh;
+module load $cxxmodule
+if [[ -n "$mpi_flags" ]]; then
+    module load ${mpi_module}
 fi
+module load cmake/3.14.0 # 3.13 or higher is required
 
-
-export SHELL=/bin/sh
-
-. /usr/share/lmod/lmod/init/sh
-module load ${cxxmodule}
-module load cmake/3.14.6 # 3.13 or higher is required
-
-if [[ -n "$mpi_flags" ]] ; then
-  module load openmpi/${openmpi_version}
-fi
-
-echo $WORKSPACE
+echo "JENKINS WORKSPACE = $WORKSPACE"
 cd $WORKSPACE
 
+rm -fr build
 mkdir build
 cd build
 
@@ -144,12 +174,19 @@ cmake \
   $flecsi_flags \
   $cov_flags \
   ..
-make -j8
+
+make -j36
 ctest -j36 --output-on-failure  && true #keep going if tests fail so that we get coverage report 
 status=$?
-if [[ $build_type == "coverage" ]]; then                     
+
+if [[ $CONFIG_TYPE == "coverage" ]]; then                     
     echo 'building coverage reports'
     export PYTHONPATH=/usr/projects/ngc/private/gcovr/var/lib/perceus/vnfs/asc-fe/rootfs/usr/lib/python2.7/site-packages
     /usr/projects/ngc/private/gcovr/var/lib/perceus/vnfs/asc-fe/rootfs/usr/bin/gcovr -f "$(readlink -f ..)"  -e '.*googletest' -e '.*exprtk.hpp' -e '.*json.h' -e '.*CMakeFiles' -x >coverage.xml
 fi
+
+if [[ $BUILD_TYPE == "config" ]]; then
+    make install
+fi
+
 exit $status #return the status of the ctest build so that jenkins knows whether tests past or fail
